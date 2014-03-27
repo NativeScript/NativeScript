1d46ada0df89910d069fbd3d51474a8a831dcc83
Vladimir Enchev <vladimir.enchev@telerik.com>
2014-03-27 11:04:24 +0200

    index files added to all modules

--- WebClient/web_client.d.ts
+++ WebClient/web_client.d.ts
@@ -3,7 +3,7 @@
   */
 import image_module = require("Image/image");
 
-declare class Client {
+export declare class Client {
     private static get(url: string, successCallback: (result: any) => void, errorCallback?: (e: Error) => void)
     getString(url: string, successCallback: (result: string) => void, errorCallback?: (e: Error) => void)
     getJSON(url: string, successCallback: (result: Object) => void, errorCallback?: (e: Error) => void)
