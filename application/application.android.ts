5d6cf14bea6716e2c2a35c1d5a45deeeb4f6efba
Vladimir Enchev <vladimir.enchev@telerik.com>
2014-05-15 10:31:52 +0300

    application common naming changed to use "-"

--- application/application.android.ts
+++ application/application.android.ts
@@ -1,4 +1,4 @@
-﻿import appModule = require("application/application_common");
+﻿import appModule = require("application/application-common");
 
 // merge the exports of the application_common file with the exports of this file
 declare var exports;
--- application/application.ios.ts
+++ application/application.ios.ts
@@ -20,7 +20,7 @@ log("JavaScript loading ended.");
 
 */
 
-import appModule = require("application/application_common");
+import appModule = require("application/application-common");
 
 // merge the exports of the application_common file with the exports of this file
 declare var exports;
