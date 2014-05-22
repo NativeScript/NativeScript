5c82a6d67ae37f3fefd24c8c3e573594cbaa1186
Stanimir Karoserov <stanimir.karoserov@telerik.com>
2014-05-22 11:27:27 +0300

    updated console logs for iOS

--- console/console-native.ios.ts
+++ console/console-native.ios.ts
@@ -1,20 +1,25 @@
 ﻿// TODO: we should use Foundation.NSLog() but it currently does not work
 // TODO: Is there a better way to implement the info/warn/error
 
+function nslog(prefix: string, message: string)
+{
+    (<any>Foundation).NSLog("%@: %@", [{ type: PrimitiveType.POINTER, value: prefix }, { type: PrimitiveType.POINTER, value: message}]);
+}
+
 export var helper_log = function (message: string) {
-    log('log: ' + message);
+    nslog('log', message);
 }
 
 export var info = function (message: string) {
-    log('info: ' + message);
+    nslog('info', message);
 }
 
 export var error = function (message: string) {
-    log('error: ' + message);
+    nslog('error', message);
 }
 
 export var warn = function (message: string) {
-    log('warning: ' + message);
+    nslog('warning', message);
 }
 
 export var timeMillis = function (): number {
