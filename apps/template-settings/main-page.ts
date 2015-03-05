9056482ddb0ce1c3011443cd808113d6ff388304
vakrilov <alexander.vakrilov@telerik.com>
2015-03-05 17:37:09 +0200

    Settings app update

--- apps/template-settings/main-page.ts
+++ apps/template-settings/main-page.ts
@@ -1,6 +1,5 @@
 import observable = require("data/observable");
 import pages = require("ui/page");
-import dialogs = require("ui/dialogs");
 import vmModule = require("./view-model");
 
 var viewModel = vmModule.settingsViewModel;
@@ -9,12 +8,4 @@ export function pageLoaded(args: observable.EventData) {
     // The page has loaded.
     var page = <pages.Page>args.object;
     page.bindingContext = viewModel;
-}
-
-export function promptName(args: observable.EventData) {
-    dialogs.prompt("Enter your name:", viewModel.name).then((promptResult) => {
-        if (promptResult.result) {
-            viewModel.set("name", promptResult.text);
-        }
-    });
 }
