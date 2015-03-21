ba6715412954340d2cf8db8d4b09a7bfab19e5f3
vakrilov <alexander.vakrilov@telerik.com>
2015-03-21 15:50:18 +0200

    Dates Fix

--- apps/TelerikNEXT/app-view-model.ts
+++ apps/TelerikNEXT/app-view-model.ts
@@ -145,9 +145,11 @@ export class SessionModel extends observable.Observable implements Session {
     get range(): string {
         var startMinutes = this.start.getMinutes() + "";
         var endMinutes = this.end.getMinutes() + "";
+        var startAM = this.start.getHours() < 12 ? "am" : "pm";
+        var endAM = this.end.getHours() < 12 ? "am" : "pm";
 
-        return this.start.getHours() + ':' + (startMinutes.length === 1 ? '0' + startMinutes : startMinutes) +
-            ' - ' + this.end.getHours() + ':' + (endMinutes.length === 1 ? '0' + endMinutes : endMinutes);
+        return this.start.getHours() + ':' + (startMinutes.length === 1 ? '0' + startMinutes : startMinutes) + startAM + 
+            ' - ' + this.end.getHours() + ':' + (endMinutes.length === 1 ? '0' + endMinutes : endMinutes) + endAM;
     }
 
     get canBeFavorited(): boolean {
@@ -208,26 +210,26 @@ var sessions: Array<SessionModel> = [
     }),
     new SessionModel({
         title: "NativeScript Deep Dive",
-        start: new Date(2015, 5, 3, 1, 30),
-        end: new Date(2015, 5, 3, 4, 30),
+        start: new Date(2015, 5, 3, 13, 30),
+        end: new Date(2015, 5, 3, 16, 30),
         room: "Workshop Room 1"
     }),
     new SessionModel({
         title: "Smart Design for Smartphones",
-        start: new Date(2015, 5, 3, 1, 30),
-        end: new Date(2015, 5, 3, 4, 30),
+        start: new Date(2015, 5, 3, 13, 30),
+        end: new Date(2015, 5, 3, 16, 30),
         room: "Workshop Room 2"
     }),
     new SessionModel({
         title: "Responsive Apps with Telerik DevCraft",
-        start: new Date(2015, 5, 3, 1, 30),
-        end: new Date(2015, 5, 3, 4, 30),
+        start: new Date(2015, 5, 3, 13, 30),
+        end: new Date(2015, 5, 3, 16, 30),
         room: "Workshop Room 3"
     }),
     new SessionModel({
         title: "ASP .NET MVC Development in Telerik Sitefinity",
-        start: new Date(2015, 5, 3, 1, 30),
-        end: new Date(2015, 5, 3, 4, 30),
+        start: new Date(2015, 5, 3, 13, 30),
+        end: new Date(2015, 5, 3, 16, 30),
         room: "Workshop Room 4"
     }), new SessionModel({
         title: "Registration",
@@ -282,97 +284,97 @@ var sessions: Array<SessionModel> = [
     }), new SessionModel({
         title: "Lunch",
         start: new Date(2015, 5, 4, 12, 30),
-        end: new Date(2015, 5, 4, 1, 30),
+        end: new Date(2015, 5, 4, 13, 30),
         room: ""
     }), new SessionModel({
         title: "Hybrid vs Native vs Web: Which is Right for Me?",
-        start: new Date(2015, 5, 4, 1, 30),
-        end: new Date(2015, 5, 4, 2, 15),
+        start: new Date(2015, 5, 4, 13, 30),
+        end: new Date(2015, 5, 4,  14, 15),
         room: "Conference Room 1"
     }), new SessionModel({
         title: "AngularJS Directives For Kendo UI",
-        start: new Date(2015, 5, 4, 1, 30),
-        end: new Date(2015, 5, 4, 2, 15),
+        start: new Date(2015, 5, 4, 13, 30),
+        end: new Date(2015, 5, 4,  14, 15),
         room: "Conference Room 2"
     }), new SessionModel({
         title: "Using Kendo UI in SharePoint/Office 365",
-        start: new Date(2015, 5, 4, 1, 30),
-        end: new Date(2015, 5, 4, 2, 15),
+        start: new Date(2015, 5, 4, 13, 30),
+        end: new Date(2015, 5, 4,  14, 15),
         room: "Conference Room 3"
     }), new SessionModel({
         title: "Develop the Next Generation of Content-Driven Mobile Apps",
-        start: new Date(2015, 5, 4, 1, 30),
-        end: new Date(2015, 5, 4, 2, 15),
+        start: new Date(2015, 5, 4, 13, 30),
+        end: new Date(2015, 5, 4,  14, 15),
         room: "Conference Room 3"
     }), new SessionModel({
         title: "PM Break",
-        start: new Date(2015, 5, 4, 2, 15),
-        end: new Date(2015, 5, 4, 2, 30),
+        start: new Date(2015, 5, 4,  14, 15),
+        end: new Date(2015, 5, 4, 14, 30),
         room: ""
     }), new SessionModel({
         title: "AppBuilder in 45 Minutes",
-        start: new Date(2015, 5, 4, 2, 30),
-        end: new Date(2015, 5, 4, 3, 15),
+        start: new Date(2015, 5, 4, 14, 30),
+        end: new Date(2015, 5, 16, 15, 15),
         room: "Conference Room 1"
     }), new SessionModel({
         title: "Mastering JavaScript",
-        start: new Date(2015, 5, 4, 2, 30),
-        end: new Date(2015, 5, 4, 3, 15),
+        start: new Date(2015, 5, 4, 14, 30),
+        end: new Date(2015, 5, 16, 15, 15),
         room: "Conference Room 2"
     }), new SessionModel({
         title: "Building Mobile Apps with Visual Studio",
-        start: new Date(2015, 5, 4, 2, 30),
-        end: new Date(2015, 5, 4, 3, 15),
+        start: new Date(2015, 5, 4, 14, 30),
+        end: new Date(2015, 5, 16, 15, 15),
         room: "Conference Room 3"
     }), new SessionModel({
         title: "Building a CRM Portal in 60 Minutes",
-        start: new Date(2015, 5, 4, 2, 30),
-        end: new Date(2015, 5, 4, 3, 15),
+        start: new Date(2015, 5, 4, 14, 30),
+        end: new Date(2015, 5, 16, 15, 15),
         room: "Conference Room 4"
     }), new SessionModel({
         title: "NativeScript Extensibility",
-        start: new Date(2015, 5, 4, 3, 30),
-        end: new Date(2015, 5, 4, 4, 15),
+        start: new Date(2015, 5, 4, 15, 30),
+        end: new Date(2015, 5, 4, 16, 15),
         room: "Conference Room 1"
     }), new SessionModel({
         title: "There's a Cordova Plugin for that!",
-        start: new Date(2015, 5, 4, 3, 30),
-        end: new Date(2015, 5, 4, 4, 15),
+        start: new Date(2015, 5, 4, 15, 30),
+        end: new Date(2015, 5, 4, 16, 15),
         room: "Conference Room 2"
     }), new SessionModel({
         title: "AngularJS and Kendo UI",
-        start: new Date(2015, 5, 4, 3, 30),
-        end: new Date(2015, 5, 4, 4, 15),
+        start: new Date(2015, 5, 4, 15, 30),
+        end: new Date(2015, 5, 4, 16, 15),
         room: "Conference Room 3"
     }), new SessionModel({
         title: "Continuous Delivery and Telerik Sitefinity",
-        start: new Date(2015, 5, 4, 3, 30),
-        end: new Date(2015, 5, 4, 4, 15),
+        start: new Date(2015, 5, 4, 15, 30),
+        end: new Date(2015, 5, 4, 16, 15),
         room: "Conference Room 4"
     }), new SessionModel({
         title: "Telerik Leadership Panel - Q&A",
-        start: new Date(2015, 5, 4, 4, 30),
-        end: new Date(2015, 5, 4, 5, 15),
+        start: new Date(2015, 5, 4, 16, 30),
+        end: new Date(2015, 5, 4, 17, 15),
         room: "Conference Room 1"
     }), new SessionModel({
         title: "Accelerate your Agile Adoption",
-        start: new Date(2015, 5, 4, 4, 30),
-        end: new Date(2015, 5, 4, 5, 15),
+        start: new Date(2015, 5, 4, 16, 30),
+        end: new Date(2015, 5, 4, 17, 15),
         room: "Conference Room 2"
     }), new SessionModel({
         title: "No Kidding, Real World Tester/Developer Collaboration",
-        start: new Date(2015, 5, 4, 4, 30),
-        end: new Date(2015, 5, 4, 5, 15),
+        start: new Date(2015, 5, 4, 16, 30),
+        end: new Date(2015, 5, 4, 17, 15),
         room: "Conference Room 3"
     }), new SessionModel({
         title: "Sitefinity",
-        start: new Date(2015, 5, 4, 4, 30),
-        end: new Date(2015, 5, 4, 5, 15),
+        start: new Date(2015, 5, 4, 16, 30),
+        end: new Date(2015, 5, 4, 17, 15),
         room: "Conference Room 4"
     }), new SessionModel({
         title: "Attendee Appreciation Party",
-        start: new Date(2015, 5, 4, 7, 0),
-        end: new Date(2015, 5, 4, 10, 30),
+        start: new Date(2015, 5, 4, 19, 0),
+        end: new Date(2015, 5, 4, 22, 30),
         room: ""
     }), new SessionModel({
         title: "Registration",
@@ -427,77 +429,77 @@ var sessions: Array<SessionModel> = [
     }), new SessionModel({
         title: "Lunch",
         start: new Date(2015, 5, 5, 12, 30),
-        end: new Date(2015, 5, 5, 1, 30),
+        end: new Date(2015, 5, 5, 13, 30),
         room: ""
     }), new SessionModel({
         title: "Performance Tuning Your Mobile Web Apps",
-        start: new Date(2015, 5, 5, 1, 30),
-        end: new Date(2015, 5, 5, 2, 15),
+        start: new Date(2015, 5, 5, 13, 30),
+        end: new Date(2015, 5, 5,  14, 15),
         room: "Conference Room 1"
     }), new SessionModel({
         title: "Improving Applications with Telerik Analytics",
-        start: new Date(2015, 5, 5, 1, 30),
-        end: new Date(2015, 5, 5, 2, 15),
+        start: new Date(2015, 5, 5, 13, 30),
+        end: new Date(2015, 5, 5,  14, 15),
         room: "Conference Room 2"
     }), new SessionModel({
         title: "Reporting vs Dashboards vs UI Data Apps",
-        start: new Date(2015, 5, 5, 1, 30),
-        end: new Date(2015, 5, 5, 2, 15),
+        start: new Date(2015, 5, 5, 13, 30),
+        end: new Date(2015, 5, 5,  14, 15),
         room: "Conference Room 3"
     }), new SessionModel({
         title: "Modern MVC and Front-End Development with Telerik Sitefinity",
-        start: new Date(2015, 5, 5, 1, 30),
-        end: new Date(2015, 5, 5, 2, 15),
+        start: new Date(2015, 5, 5, 13, 30),
+        end: new Date(2015, 5, 5,  14, 15),
         room: "Conference Room 4"
     }), new SessionModel({
         title: "PM Break",
-        start: new Date(2015, 5, 5, 2, 15),
-        end: new Date(2015, 5, 5, 2, 30),
+        start: new Date(2015, 5, 5,  14, 15),
+        end: new Date(2015, 5, 5, 14, 30),
         room: ""
     }), new SessionModel({
         title: "Telerik Native Mobile UI for iOS and Android",
-        start: new Date(2015, 5, 5, 2, 30),
-        end: new Date(2015, 5, 5, 3, 15),
+        start: new Date(2015, 5, 5, 14, 30),
+        end: new Date(2015, 5, 17, 15, 15),
         room: "Conference Room 1"
     }), new SessionModel({
         title: "IoT and the Telerik Platform",
-        start: new Date(2015, 5, 5, 2, 30),
-        end: new Date(2015, 5, 5, 3, 15),
+        start: new Date(2015, 5, 5, 14, 30),
+        end: new Date(2015, 5, 17, 15, 15),
         room: "Conference Room 2"
     }), new SessionModel({
         title: "Debugging with Fiddler",
-        start: new Date(2015, 5, 5, 2, 30),
-        end: new Date(2015, 5, 5, 3, 15),
+        start: new Date(2015, 5, 5, 14, 30),
+        end: new Date(2015, 5, 17, 15, 15),
         room: "Conference Room 3"
     }), new SessionModel({
         title: "Anticipating & Planning of Peak Online Traffic for Professional Football's Biggest Games",
-        start: new Date(2015, 5, 5, 2, 30),
-        end: new Date(2015, 5, 5, 3, 15),
+        start: new Date(2015, 5, 5, 14, 30),
+        end: new Date(2015, 5, 17, 15, 15),
         room: "Conference Room 4"
     }), new SessionModel({
         title: "Building a Mobile App API using MongoDB and Node.js",
-        start: new Date(2015, 5, 5, 3, 30),
-        end: new Date(2015, 5, 5, 4, 15),
+        start: new Date(2015, 5, 5, 15, 30),
+        end: new Date(2015, 5, 5, 16, 15),
         room: "Conference Room 1"
     }), new SessionModel({
         title: "Advanced Kendo UI",
-        start: new Date(2015, 5, 5, 3, 30),
-        end: new Date(2015, 5, 5, 4, 15),
+        start: new Date(2015, 5, 5, 15, 30),
+        end: new Date(2015, 5, 5, 16, 15),
         room: "Conference Room 2"
     }), new SessionModel({
         title: "Building Touch Apps with UI for WPF",
-        start: new Date(2015, 5, 5, 3, 30),
-        end: new Date(2015, 5, 5, 4, 15),
+        start: new Date(2015, 5, 5, 15, 30),
+        end: new Date(2015, 5, 5, 16, 15),
         room: "Conference Room 3"
     }), new SessionModel({
         title: "Making the Most Out of Sitefinity Personalization",
-        start: new Date(2015, 5, 5, 3, 30),
-        end: new Date(2015, 5, 5, 4, 15),
+        start: new Date(2015, 5, 5, 15, 30),
+        end: new Date(2015, 5, 5, 16, 15),
         room: "Conference Room 4"
     }), new SessionModel({
         title: "Closing Keynote",
-        start: new Date(2015, 5, 5, 4, 30),
-        end: new Date(2015, 5, 5, 5, 15),
+        start: new Date(2015, 5, 5, 16, 30),
+        end: new Date(2015, 5, 5, 17, 15),
         room: "General Session"
     })];
 
