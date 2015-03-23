9be733a7fa8c37ae364c2ea4154c2ba3548cecc7
Erjan Gavalji <erjan.gavalji@gmail.com>
2015-03-23 17:36:37 +0200

    Do not use starting slash when setting the main module

--- apps/cuteness.io/app.ts
+++ apps/cuteness.io/app.ts
@@ -1,7 +1,7 @@
 ﻿import application = require("application");
 
 // Set the start module for the application
-application.mainModule = "/main-page";
+application.mainModule = "main-page";
 
 // Start the application
 application.start();
