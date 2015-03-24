29d87714bde64b022e97668d2736571a6d6893cb
Erjan Gavalji <erjan.gavalji@gmail.com>
2015-03-24 14:15:19 +0200

    Fix the template-settings app paths

--- apps/template-settings/app.ts
+++ apps/template-settings/app.ts
@@ -1,7 +1,7 @@
 ﻿import application = require("application");
 
 // Remove this in the AppBuilder templates
-application.cssFile = "app/template-settings/app.css"
+application.cssFile = "template-settings/app.css"
 
-application.mainModule = "app/main-page";
+application.mainModule = "main-page";
 application.start();
