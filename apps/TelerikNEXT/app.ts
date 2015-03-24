21c1bcb7e27c9a9772cbb4307751fd53ef30c92b
Erjan Gavalji <erjan.gavalji@gmail.com>
2015-03-24 11:31:22 +0200

    Attempt to fix the TelerikNext (to be tested)

--- apps/TelerikNEXT/app.ts
+++ apps/TelerikNEXT/app.ts
@@ -1,7 +1,7 @@
 ﻿import application = require("application");
 
 // Set the start module for the application
-application.mainModule = "app/main-page";
+application.mainModule = "main-page";
 
 // TODO: This is only neede because of the deply script.
 application.cssFile = "app/TelerikNEXT/app.css";
