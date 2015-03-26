8ea1c5c74350f6a89133593e6548539af2380ad1
Vladimir Enchev <vladimir.enchev@telerik.com>
2015-03-26 11:49:25 +0200

    missing TS defs added

---
+++ ui/styling/css-selector.d.ts
@@ -0,0 +1,56 @@
+﻿declare module "ui/styling/css-selector" {
+    import view = require("ui/core/view");
+    import cssParser = require("js-libs/reworkcss");
+    import styleProperty = require("ui/styling/style-property");
+
+    export class CssSelector {
+        constructor(expression: string, declarations: cssParser.Declaration[]);
+
+        expression: string;
+
+        declarations(): Array<{ property: string; value: any }>;
+
+        specificity: number;
+
+        matches(view: view.View): boolean;
+
+        apply(view: view.View);
+
+        eachSetter(callback: (property: styleProperty.Property, resolvedValue: any) => void);
+    }
+
+    class CssTypeSelector extends CssSelector {
+        specificity: number;
+        matches(view: view.View): boolean;
+    }
+
+    class CssIdSelector extends CssSelector {
+        specificity: number;
+        matches(view: view.View): boolean;
+    }
+
+    class CssClassSelector extends CssSelector {
+        specificity: number;
+        matches(view: view.View): boolean;
+    }
+
+    export class CssVisualStateSelector extends CssSelector {
+        specificity: number;
+
+        key: string;
+
+        state: string;
+
+        constructor(expression: string, declarations: cssParser.Declaration[]);
+        matches(view: view.View): boolean;
+    }
+
+    export function createSelector(expression: string, declarations: cssParser.Declaration[]): CssSelector;
+
+    class InlineStyleSelector extends CssSelector {
+        constructor(declarations: cssParser.Declaration[]);
+        apply(view: view.View);
+    }
+
+    export function applyInlineSyle(view: view.View, declarations: cssParser.Declaration[]);
+}
