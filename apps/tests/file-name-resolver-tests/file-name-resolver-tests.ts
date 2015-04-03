cbbd28be6bc4de66cf78e89844ef5de1c55afdf4
vakrilov <alexander.vakrilov@telerik.com>
2015-04-03 10:56:17 +0300

    More tests fixed

--- apps/tests/file-name-resolver-tests/file-name-resolver-tests.ts
+++ apps/tests/file-name-resolver-tests/file-name-resolver-tests.ts
@@ -217,25 +217,25 @@ var testFilePath = "~/file-name-resolver-tests/files/test".replace("~", fs.known
 export function test_file_resolver_with_andorid_phone_portratit() {
     var fileResolver = new resolver.FileNameResolver(androidPhonePortraitContext);
     var result = fileResolver.resolveFileName(testFilePath, "xml");
-    TKUnit.assertEqual(result, testFilePath + ".android.xml", "File path");
+    TKUnit.assertEqual(result, testFilePath + ".xml", "File path");
 }
 
 export function test_file_resolver_with_andorid_phone_landscape() {
     var fileResolver = new resolver.FileNameResolver(androidPhoneLandsacpeContext);
     var result = fileResolver.resolveFileName(testFilePath, "xml");
-    TKUnit.assertEqual(result, testFilePath + ".android.xml", "File path");
+    TKUnit.assertEqual(result, testFilePath + ".land.xml", "File path");
 }
 
 export function test_file_resolver_with_andorid_tablet_portrait() {
     var fileResolver = new resolver.FileNameResolver(androidTabletPortraitContext);
     var result = fileResolver.resolveFileName(testFilePath, "xml");
-    TKUnit.assertEqual(result, testFilePath + ".android.minWH600.xml", "File path");
+    TKUnit.assertEqual(result, testFilePath + ".minWH600.xml", "File path");
 }
 
 export function test_file_resolver_with_ios_phone_landscape() {
     var fileResolver = new resolver.FileNameResolver(iPhoneLandscapeContext);
     var result = fileResolver.resolveFileName(testFilePath, "xml");
-    TKUnit.assertEqual(result, testFilePath + ".ios.land.xml", "File path");
+    TKUnit.assertEqual(result, testFilePath + ".land.xml", "File path");
 }
 
 export function test_file_resolver_with_ios_phone_portrait() {
