075e2ae6739e61ccf50ec85711bfadc0af2760e1
Nedyalko Nikolov <nedyalko.nikolov@telerik.com>
2015-04-17 16:29:40 +0300

    Removed test date.

--- camera/camera.android.ts
+++ camera/camera.android.ts
@@ -80,7 +80,7 @@ var calculateInSampleSize = function (imageWidth, imageHeight, reqWidth, reqHeig
 
 var createDateTimeStamp = function() {
     var result = "";
-    var date = new Date(2015, 3, 3);
+    var date = new Date();
     result = (date.getDate() < 10 ? "0" + date.getDate().toString() : date.getDate().toString())+ 
              ((date.getMonth() + 1) < 10 ? "0" + (date.getMonth() + 1).toString() : (date.getMonth() + 1).toString()) + 
              date.getFullYear().toString() + 
