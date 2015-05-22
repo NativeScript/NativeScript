3b8e27ef35066aa2734304237a6d6a99dadc8233
vakrilov <alexander.vakrilov@telerik.com>
2015-05-22 18:07:47 +0300

    Performance test pages

---
+++ apps/tests/pages/performance-test/start.ts
@@ -0,0 +1,12 @@
+﻿import frame = require("ui/frame");
+import observable = require("data/observable");
+
+declare function __startCPUProfiler(name: string);
+
+export function navigate(args: observable.EventData) {
+    var tag = "" + args.object.get("tag");
+    __startCPUProfiler("xml-performance-" + tag);
+    frame.topmost().navigate({
+        moduleName: tag,
+    });
+}
