542b948e8bfc56bb5d315e02c082a93130bf1b4b
Nedyalko Nikolov <nedyalko.nikolov@telerik.com>
2015-05-29 13:48:45 +0300

    Reverted some test files.

--- apps/tests/app/testPage.ts
+++ apps/tests/app/testPage.ts
@@ -1,22 +1,5 @@
 ﻿import frame = require("ui/frame");
-import pageModule = require("ui/page");
-import textViewModule = require("ui/text-view");
 
 export function buttonTap(args) { 
-    console.log("tap");
-}
-
-export function doubleTap(args) {
-    console.log("doubleTap");
-}
-
-export function checkRecognizers(args) {
-    var testTextView = <textViewModule.TextView>(page.getViewById("testTextView"));
-    console.log("testTextView: " + testTextView.ios.gestureRecognizers);
-}
-
-var page: pageModule.Page;
-
-export function pageLoaded(args) {
-    page = <pageModule.Page>args.object;
+    frame.topmost().goBack();
 }
