927fdcc6f1e6bc7775876bb608f0cf43359999ca
Vasil Chimev <Vasil.Chimev@telerik.com>
2015-06-02 15:34:03 +0300

    Add "console" module page to ui-tests-app.

---
+++ apps/ui-tests-app/pages/console.ts
@@ -0,0 +1,29 @@
+﻿export function pageLoaded() {
+    console.log("### TEST START ###.");
+    onTapTrue();
+    onTapFalse();
+    onTapText();
+    onTapNull();
+    onTapUndefined();
+    console.log("### TEST END ###.");
+}
+
+export function onTapTrue() {
+    console.log(true);
+}
+
+export function onTapFalse() {
+    console.log(false);
+}
+
+export function onTapText() {
+    console.log("text");
+}
+
+export function onTapNull() {
+    console.log(null);
+}
+
+export function onTapUndefined() {
+    console.log(undefined);
+}
