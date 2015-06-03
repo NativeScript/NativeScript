aff562c9d3bfbb6378990fa697e3434082453c4e
Vasil Chimev <Vasil.Chimev@telerik.com>
2015-06-03 17:53:04 +0300

    Update apps\ui-tests-app\app.ts

--- apps/ui-tests-app/app.ts
+++ apps/ui-tests-app/app.ts
@@ -11,56 +11,56 @@ application.onUncaughtError = function (error: application.NativeScriptError) {
 application.on(application.launchEvent, function (args: application.ApplicationEventData) {
     if (args.android) {
         // For Android applications, args.android is an android.content.Intent class.
-        console.log("Launched Android application with the following intent: " + args.android + ".");
+        console.log("Launched application with: " + args.android + ".");
     } else if (args.ios !== undefined) {
         // For iOS applications, args.ios is NSDictionary (launchOptions).
-        console.log("Launched iOS application with options: " + args.ios);
+        console.log("Launched application with: " + args.ios);
     }
 });
 
 application.on(application.suspendEvent, function (args: application.ApplicationEventData) {
     if (args.android) {
         // For Android applications, args.android is an android activity class.
-        console.log("Activity: " + args.android);
+        console.log("SuspendEvent Activity: " + args.android);
     } else if (args.ios) {
         // For iOS applications, args.ios is UIApplication.
-        console.log("UIApplication: " + args.ios);
+        console.log("SuspendEvent UIApplication: " + args.ios);
     }
 });
 
 application.on(application.resumeEvent, function (args: application.ApplicationEventData) {
     if (args.android) {
         // For Android applications, args.android is an android activity class.
-        console.log("Activity: " + args.android);
+        console.log("ResumeEvent Activity: " + args.android);
     } else if (args.ios) {
         // For iOS applications, args.ios is UIApplication.
-        console.log("UIApplication: " + args.ios);
+        console.log("ResumeEvent UIApplication: " + args.ios);
     }
 });
 
 application.on(application.exitEvent, function (args: application.ApplicationEventData) {
     if (args.android) {
         // For Android applications, args.android is an android activity class.
-        console.log("Activity: " + args.android);
+        console.log("ExitEvent Activity: " + args.android);
     } else if (args.ios) {
         // For iOS applications, args.ios is UIApplication.
-        console.log("UIApplication: " + args.ios);
+        console.log("ExitEvent UIApplication: " + args.ios);
     }
 });
 
 application.on(application.lowMemoryEvent, function (args: application.ApplicationEventData) {
     if (args.android) {
         // For Android applications, args.android is an android activity class.
-        console.log("Activity: " + args.android);
+        console.log("LowMemoryEvent Activity: " + args.android);
     } else if (args.ios) {
         // For iOS applications, args.ios is UIApplication.
-        console.log("UIApplication: " + args.ios);
+        console.log("LowMemoryEvent UIApplication: " + args.ios);
     }
 });
 
 application.on(application.uncaughtErrorEvent, function (args: application.ApplicationEventData) {
     if (args.android) {
-        // For Android applications, args.android is an NativeScriptError.
+        // For Android applications, args.android is NativeScriptError.
         console.log("NativeScriptError: " + args.android);
     } else if (args.ios) {
         // For iOS applications, args.ios is NativeScriptError.
