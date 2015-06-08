d335fae12372c71d6eb365175b957b9fb0ea86d2
Nedyalko Nikolov <nedyalko.nikolov@telerik.com>
2015-06-08 10:25:56 +0300

    Android console improved to support circular objects.

--- apps/perf-tests/LargeObjectArrayMemoryTest/mainPage.ts
+++ apps/perf-tests/LargeObjectArrayMemoryTest/mainPage.ts
@@ -2,7 +2,6 @@ import pages = require("ui/page");
 import labelModule = require("ui/label");
 import common = require("../common");
 import trace = require("trace");
-import cn = require("console/console-native");
 
 export function createPage() {
     var count = 2000000;
@@ -13,12 +12,12 @@ export function createPage() {
     page.content = label;
     
     trace.write("Creating " + count + " objects.", trace.categories.Test, trace.messageType.info);
-    var startTime = cn.timeMillis();
+    console.time("creatingObjects");
     for (var i = 0; i < count; i++) {
         people[i] = new common.Person("John Doe", 33, 1234.56);
     }
-    var elapsedTime = Math.round(cn.timeMillis() - startTime);
-    var message = "Created " + people.length + " objects in " + elapsedTime + " ms.";
+    console.timeEnd("creatingObjects");
+    var message = "Created " + people.length + " objects";
     trace.write(message, trace.categories.Test, trace.messageType.info);
     label.text = message;
     return page;
