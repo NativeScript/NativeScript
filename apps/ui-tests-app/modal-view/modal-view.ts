df265794e8eb443c967bb0f1fff02b638f59a399
Vasil Chimev <Vasil.Chimev@telerik.com>
2015-06-15 15:09:56 +0300

    Add modal-view demo to ui-tests-app.

---
+++ apps/ui-tests-app/modal-view/modal-view.ts
@@ -0,0 +1,19 @@
+import pages = require("ui/page");
+import labelModule = require("ui/label");
+import observable = require("data/observable");
+
+var page: pages.Page;
+var label: labelModule.Label;
+
+export function pageLoaded(args: observable.EventData) {
+    page = <pages.Page>args.object;
+    label = page.getViewById<labelModule.Label>("label");
+}
+
+export function onTap(args: observable.EventData) {
+    var fullscreen = (<any>args.object).text.indexOf("(full-screen)") !== -1;
+    page.showModal("modal-view/login-page", "context", function (username: string, password: string) {
+        console.log(username + "/" + password);
+        label.text = username + "/" + password;
+    }, fullscreen);
+}
