95b616edb320b2a46968efdd16668a42484dbc5e
Vasil Chimev <Vasil.Chimev@telerik.com>
2015-07-06 11:37:46 +0300

    Add fps-meter to cuteness.unoptimized.

--- apps/cuteness.unoptimized/app.ts
+++ apps/cuteness.unoptimized/app.ts
@@ -1,4 +1,9 @@
 ﻿import application = require("application");
+import fps = require("fps-meter");
+fps.addCallback(function (fps, minFps) {
+    console.info("fps=" + fps + " minFps=" + minFps);
+});
+fps.start();
 
 // Set the start module for the application
 application.mainModule = "main-page";
