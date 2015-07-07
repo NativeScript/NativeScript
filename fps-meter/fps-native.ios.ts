1d39b9906693881aacc6e98cd34367e820a23c89
Panayot Cankov <Panayot.Cankov@telerik.com>
2015-07-07 18:01:43 +0300

    FPS module will now correctly count frames while scrolling in iOS

--- fps-meter/fps-native.ios.ts
+++ fps-meter/fps-native.ios.ts
@@ -35,6 +35,9 @@ export class FPSCallback implements definition.FPSCallback {
         this.displayLink = CADisplayLink.displayLinkWithTargetSelector(this.impl, "handleFrame");
         this.displayLink.paused = true;
         this.displayLink.addToRunLoopForMode(NSRunLoop.currentRunLoop(), NSDefaultRunLoopMode);
+        // UIScrollView (including in UIITableView) will run a loop in UITrackingRunLoopMode during scrolling.
+        // If we do not add the CADisplayLink in this mode, it would appear paused during scrolling.
+        this.displayLink.addToRunLoopForMode(NSRunLoop.currentRunLoop(), UITrackingRunLoopMode);
     }
 
     public start() {
