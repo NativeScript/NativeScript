6f1b36763877ffc9592a18cf1615c5b7115a3fd1
vakrilov <alexander.vakrilov@telerik.com>
2015-09-03 15:06:52 +0300

    Tslint

--- apps/tests/pages/tab-view.ts
+++ apps/tests/pages/tab-view.ts
@@ -12,10 +12,10 @@ var i = 0;
 function onTap() {
     i++;
     vm.set("firstTitle", "changed " + i);
-    if (i == 3) {
+    if (i === 3) {
         vm.set("firstIcon", "res://ic_action");
     }
-    if (i == 4) {
+    if (i === 4) {
         vm.set("firstIcon", "");
     }
 }
