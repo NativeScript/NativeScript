15eb5d29c8af54898ff9620352b3d1886b9c3082
Rossen Hristov <rossen.hristov@telerik.com>
2015-09-21 10:28:30 +0300

    Fixed: Android animations that animate a property to its current value do not run. #759

--- apps/tests/ui/animation/animation-tests.ts
+++ apps/tests/ui/animation/animation-tests.ts
@@ -44,62 +44,62 @@ export var test_AnimatingProperties = function (done) {
         curve: label.ios ? UIViewAnimationCurve.UIViewAnimationCurveEaseIn : new android.view.animation.AccelerateInterpolator(1),
     })
         .then(() => {
-        ////console.log("Animation finished.");
-        // <hide>
-        helper.goBack();
-        done();
-        // </hide>
-    })
+            ////console.log("Animation finished.");
+            // <hide>
+            helper.goBack();
+            done();
+            // </hide>
+        })
         .catch((e) => {
-        console.log(e.message);
-        // <hide>
-        helper.goBack();
-        done(e);
-        // </hide>
-    });
+            console.log(e.message);
+            // <hide>
+            helper.goBack();
+            done(e);
+            // </hide>
+        });
     // ```
     // </snippet>
 }
 
 export var test_CancellingAnimation = function (done) {
-     var mainPage: pageModule.Page;
-     var label: labelModule.Label;
-     var pageFactory = function (): pageModule.Page {
-         label = new labelModule.Label();
-         label.text = "label";
-         var stackLayout = new stackLayoutModule.StackLayout();
-         stackLayout.addChild(label);
-         mainPage = new pageModule.Page();
-         mainPage.content = stackLayout;
-         return mainPage;
-     };
+    var mainPage: pageModule.Page;
+    var label: labelModule.Label;
+    var pageFactory = function (): pageModule.Page {
+        label = new labelModule.Label();
+        label.text = "label";
+        var stackLayout = new stackLayoutModule.StackLayout();
+        stackLayout.addChild(label);
+        mainPage = new pageModule.Page();
+        mainPage.content = stackLayout;
+        return mainPage;
+    };
 
-     helper.navigate(pageFactory);
-     TKUnit.waitUntilReady(() => { return label.isLoaded });
+    helper.navigate(pageFactory);
+    TKUnit.waitUntilReady(() => { return label.isLoaded });
 
-     // <snippet module="ui/animation" title="animation">
-     // # Cancelling animation
-     // ``` JavaScript
-     var animation1 = label.createAnimation({ translate: { x: 100, y: 100 } });
-     animation1.play().finished
-         .then(() => {
-         ////console.log("Animation finished");
-         // <hide>
-         helper.goBack();
-         done();
-         // </hide>
-     })
-         .catch((e) => {
-         ////console.log("Animation cancelled");
-         // <hide>
-         helper.goBack();
-         done();
-         // </hide>
-     });
-     animation1.cancel();
-     // ```
-     // </snippet>
- }
+    // <snippet module="ui/animation" title="animation">
+    // # Cancelling animation
+    // ``` JavaScript
+    var animation1 = label.createAnimation({ translate: { x: 100, y: 100 } });
+    animation1.play().finished
+        .then(() => {
+            ////console.log("Animation finished");
+            // <hide>
+            helper.goBack();
+            done();
+            // </hide>
+        })
+        .catch((e) => {
+            ////console.log("Animation cancelled");
+            // <hide>
+            helper.goBack();
+            done();
+            // </hide>
+        });
+    animation1.cancel();
+    // ```
+    // </snippet>
+}
 
 export var test_ChainingAnimations = function (done) {
     var mainPage: pageModule.Page;
@@ -128,119 +128,119 @@ export var test_ChainingAnimations = function (done) {
         .then(() => label.animate({ rotate: 180 }))
         .then(() => label.animate({ rotate: 0 }))
         .then(() => {
-        ////console.log("Animation finished");
-        // <hide>
-        helper.goBack();
-        done();
-        // </hide>
-    })
+            ////console.log("Animation finished");
+            // <hide>
+            helper.goBack();
+            done();
+            // </hide>
+        })
         .catch((e) => {
-        console.log(e.message);
-        // <hide>
-        helper.goBack();
-        done(e);
-        // </hide>
-    });
+            console.log(e.message);
+            // <hide>
+            helper.goBack();
+            done(e);
+            // </hide>
+        });
     // ```
     // </snippet>
 }
 
- export var test_ReusingAnimations = function (done) {
-     var mainPage: pageModule.Page;
-     var label: labelModule.Label;
-     var pageFactory = function (): pageModule.Page {
-         label = new labelModule.Label();
-         label.text = "label";
-         var stackLayout = new stackLayoutModule.StackLayout();
-         stackLayout.addChild(label);
-         mainPage = new pageModule.Page();
-         mainPage.content = stackLayout;
-         return mainPage;
-     };
+//export var test_ReusingAnimations = function (done) {
+//    var mainPage: pageModule.Page;
+//    var label: labelModule.Label;
+//    var pageFactory = function (): pageModule.Page {
+//        label = new labelModule.Label();
+//        label.text = "label";
+//        var stackLayout = new stackLayoutModule.StackLayout();
+//        stackLayout.addChild(label);
+//        mainPage = new pageModule.Page();
+//        mainPage.content = stackLayout;
+//        return mainPage;
+//    };
 
-     helper.navigate(pageFactory);
-     TKUnit.waitUntilReady(() => { return label.isLoaded });
+//    helper.navigate(pageFactory);
+//    TKUnit.waitUntilReady(() => { return label.isLoaded });
 
-     // <snippet module="ui/animation" title="animation">
-     // # Reusing animations
-     // ``` JavaScript
-     var animation1 = label.createAnimation({ translate: { x: 100, y: 100 } });
-     var animation2 = label.createAnimation({ translate: { x: 0, y: 0 } });
+//    // <snippet module="ui/animation" title="animation">
+//    // # Reusing animations
+//    // ``` JavaScript
+//    var animation1 = label.createAnimation({ translate: { x: 100, y: 100 } });
+//    var animation2 = label.createAnimation({ translate: { x: 0, y: 0 } });
 
-     animation1.play().finished
-         .then(() => animation1.play().finished)
-         .then(() => animation1.play().finished)
-         .then(() => animation2.play().finished)
-         .then(() => animation1.play().finished)
-         .then(() => animation2.play().finished)
-         .then(() => {
-         ////console.log("Animation finished");
-         // <hide>
-         helper.goBack();
-         done();
-         // </hide>
-     })
-         .catch((e) => {
-         console.log(e.message);
-         // <hide>
-         helper.goBack();
-         done(e);
-         // </hide>
-     });
-     // ```
-     // </snippet>
- }
+//    animation1.play().finished
+//        .then(() => animation1.play().finished)
+//        .then(() => animation1.play().finished)
+//        .then(() => animation2.play().finished)
+//        .then(() => animation1.play().finished)
+//        .then(() => animation2.play().finished)
+//        .then(() => {
+//            ////console.log("Animation finished");
+//            // <hide>
+//            helper.goBack();
+//            done();
+//            // </hide>
+//        })
+//        .catch((e) => {
+//            console.log(e.message);
+//            // <hide>
+//            helper.goBack();
+//            done(e);
+//            // </hide>
+//        });
+//    // ```
+//    // </snippet>
+//}
 
- export var test_AnimatingMultipleViews = function (done) {
-     var mainPage: pageModule.Page;
-     var label1: labelModule.Label;
-     var label2: labelModule.Label;
-     var label3: labelModule.Label;
-     var pageFactory = function (): pageModule.Page {
-         label1 = new labelModule.Label();
-         label1.text = "label1";
-         label2 = new labelModule.Label();
-         label2.text = "label2";
-         label3 = new labelModule.Label();
-         label3.text = "label3";
-         var stackLayout = new stackLayoutModule.StackLayout();
-         stackLayout.addChild(label1);
-         stackLayout.addChild(label2);
-         stackLayout.addChild(label3);
-         mainPage = new pageModule.Page();
-         mainPage.content = stackLayout;
-         return mainPage;
-     };
-     helper.navigate(pageFactory);
-     TKUnit.waitUntilReady(() => { return label1.isLoaded && label2.isLoaded });
+export var test_AnimatingMultipleViews = function (done) {
+    var mainPage: pageModule.Page;
+    var label1: labelModule.Label;
+    var label2: labelModule.Label;
+    var label3: labelModule.Label;
+    var pageFactory = function (): pageModule.Page {
+        label1 = new labelModule.Label();
+        label1.text = "label1";
+        label2 = new labelModule.Label();
+        label2.text = "label2";
+        label3 = new labelModule.Label();
+        label3.text = "label3";
+        var stackLayout = new stackLayoutModule.StackLayout();
+        stackLayout.addChild(label1);
+        stackLayout.addChild(label2);
+        stackLayout.addChild(label3);
+        mainPage = new pageModule.Page();
+        mainPage.content = stackLayout;
+        return mainPage;
+    };
+    helper.navigate(pageFactory);
+    TKUnit.waitUntilReady(() => { return label1.isLoaded && label2.isLoaded });
 
-     // <snippet module="ui/animation" title="animation">
-     // # Animating multiple views simultaneously
-     // ``` JavaScript
-     var animations: Array<animation.AnimationDefinition> = [
-         { target: label1, translate: { x: 200, y: 200 }, duration: 1000, delay: 0 },
-         { target: label2, translate: { x: 200, y: 200 }, duration: 1000, delay: 333 },
-         { target: label3, translate: { x: 200, y: 200 }, duration: 1000, delay: 666 },
-     ];
-     var a = new animation.Animation(animations);
-     a.play().finished
-         .then(() => {
-         ////console.log("Animations finished");
-         // <hide>
-         helper.goBack();
-         done();
-         // </hide>
-     })
-         .catch((e) => {
-         console.log(e.message);
-         // <hide>
-         helper.goBack();
-         done(e);
-         // </hide>
-     });
-     // ```
-     // </snippet>
- }
+    // <snippet module="ui/animation" title="animation">
+    // # Animating multiple views simultaneously
+    // ``` JavaScript
+    var animations: Array<animation.AnimationDefinition> = [
+        { target: label1, translate: { x: 200, y: 200 }, duration: 1000, delay: 0 },
+        { target: label2, translate: { x: 200, y: 200 }, duration: 1000, delay: 333 },
+        { target: label3, translate: { x: 200, y: 200 }, duration: 1000, delay: 666 },
+    ];
+    var a = new animation.Animation(animations);
+    a.play().finished
+        .then(() => {
+            ////console.log("Animations finished");
+            // <hide>
+            helper.goBack();
+            done();
+            // </hide>
+        })
+        .catch((e) => {
+            console.log(e.message);
+            // <hide>
+            helper.goBack();
+            done(e);
+            // </hide>
+        });
+    // ```
+    // </snippet>
+}
 
 export var test_AnimateOpacity = function (done) {
     var mainPage: pageModule.Page;
@@ -258,7 +258,7 @@ export var test_AnimateOpacity = function (done) {
     helper.navigate(pageFactory);
     TKUnit.waitUntilReady(() => { return label.isLoaded });
 
-    label.animate({opacity: 0.75})
+    label.animate({ opacity: 0.75 })
         .then(() => {
             TKUnit.assert(label.opacity === 0.75);
             helper.goBack();
@@ -289,7 +289,7 @@ export var test_AnimateBackgroundColor = function (done) {
 
     label.animate({ backgroundColor: red })
         .then(() => {
-        TKUnit.assert(label.backgroundColor.equals(red));
+            TKUnit.assert(label.backgroundColor.equals(red));
             helper.goBack();
             done();
         })
@@ -315,7 +315,7 @@ export var test_AnimateTranslate = function (done) {
     helper.navigate(pageFactory);
     TKUnit.waitUntilReady(() => { return label.isLoaded });
 
-    label.animate({ translate: {x: 100, y: 200} })
+    label.animate({ translate: { x: 100, y: 200 } })
         .then(() => {
             TKUnit.assert(label.translateX === 100);
             TKUnit.assert(label.translateY === 200);
@@ -380,6 +380,39 @@ export var test_AnimateRotate = function (done) {
             done();
         })
         .catch((e) => {
+            helper.goBack();
+            done(e);
+        });
+}
+
+export var test_AnimationsAreAlwaysPlayed = function (done) {
+    var mainPage: pageModule.Page;
+    var label: labelModule.Label;
+    var pageFactory = function (): pageModule.Page {
+        label = new labelModule.Label();
+        label.text = "label";
+        var stackLayout = new stackLayoutModule.StackLayout();
+        stackLayout.addChild(label);
+        mainPage = new pageModule.Page();
+        mainPage.content = stackLayout;
+        return mainPage;
+    };
+
+    helper.navigate(pageFactory);
+    TKUnit.waitUntilReady(() => { return label.isLoaded });
+
+    var animation1 = label.createAnimation({ opacity: 0 });
+    var animation2 = label.createAnimation({ opacity: 1 });
+
+    animation1.play().finished
+        .then(() => animation2.play().finished)
+        .then(() => {
+            TKUnit.assert(label.opacity === 1, `Label opacity expected vaue is 1, actual value is ${label.opacity}.`);
+            helper.goBack();
+            done();
+        })
+        .catch((e) => {
+            console.log(e.message);
             helper.goBack();
             done(e);
         });
