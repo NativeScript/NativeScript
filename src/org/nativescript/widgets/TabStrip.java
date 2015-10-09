c3f28ba42d5cdda457aa4cd39dc7016049194dc9
hshristov <hristo.hristov@telerik.com>
2015-10-09 10:47:02 +0300

    GridLayout desired size is based on the children desired size when alignment is not Stretch. Identation is now spaces instead of tabs.

--- src/org/nativescript/widgets/TabStrip.java
+++ src/org/nativescript/widgets/TabStrip.java
@@ -27,7 +27,7 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 class TabStrip extends LinearLayout {
-
+    
     private static final int DEFAULT_BOTTOM_BORDER_THICKNESS_DIPS = 0;
     private static final byte DEFAULT_BOTTOM_BORDER_COLOR_ALPHA = 0x26;
     private static final int SELECTED_INDICATOR_THICKNESS_DIPS = 3;
