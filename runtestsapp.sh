2c42783907126c36a6180fc0392a49ecb11bfbd3
Erjan Gavalji <erjan.gavalji@gmail.com>
2015-10-15 10:37:18 +0300

    Reduce the timeout of the test run. Start in no-window

--- runtestsapp.sh
+++ runtestsapp.sh
@@ -4,52 +4,48 @@ rm -rd $workingdir
 mkdir $workingdir
 cd $workingdir
 
+# Creating the emulator with:
 #android create avd -t "android-21" -n Api21 -b "default/x86"
 
-#if [ 1 == 2 ] ; then
-    echo "------------------------------------------------"
-    echo "Killing the emulator..."
-    time pkill ".*emulator64-x86" && true
+echo "------------------------------------------------"
+echo "Killing the emulator..."
+time pkill ".*emulator64-x86" && true
 
-    echo "------------------------------------------------"
-    echo "Starting the emulator..."
-    #time emulator -avd Api19 -no-skin -no-audio &  # -gpu on
-    time emulator -avd Api19 -no-audio &  # -gpu on
-    #emulator -avd Api19 -no-skin -no-audio -no-window &
+echo "------------------------------------------------"
+echo "Starting the emulator..."
+time emulator -avd Api19 -no-audio --no-window &
 
-    echo "------------------------------------------------"
-    echo "Creating the app..."
-    time tns create TestsApp
-    cd TestsApp
+echo "------------------------------------------------"
+echo "Creating the app..."
+time tns create TestsApp
+cd TestsApp
 
-    echo "------------------------------------------------"
-    echo "Removing the original template files..."
-    time rm app/*.*
+echo "------------------------------------------------"
+echo "Removing the original template files..."
+time rm app/*.*
 
-    echo "------------------------------------------------"
-    echo "Copying the test app files..."
-    time cp -r ../../bin/dist/apps/tests/* ./app/
+echo "------------------------------------------------"
+echo "Copying the test app files..."
+time cp -r ../../bin/dist/apps/tests/* ./app/
 
-    echo "------------------------------------------------"
-    echo "Adding the android platform..."
-    time tns platform add android #--frameworkPath=/Users/erjan/tns-android.tgz
+echo "------------------------------------------------"
+echo "Adding the android platform..."
+time tns platform add android #--frameworkPath=/Users/erjan/tns-android.tgz
 
-    # #DO THE REPLACEMENTS IN THE Info.plist and AndroidManifest.xml
-    cp /Users/erjan/work/spikes/__DEL__/AndroidManifest.xml platforms/android/src/main/
+# #DO THE REPLACEMENTS IN THE Info.plist and AndroidManifest.xml
+cp /Users/erjan/work/spikes/__DEL__/AndroidManifest.xml platforms/android/src/main/
 
-    echo "------------------------------------------------"
-    echo "Building the application..."
-    time tns build android
+echo "------------------------------------------------"
+echo "Building the application..."
+time tns build android
 
-    echo "------------------------------------------------"
-    echo "Killing the adb server..."
-    time adb kill-server
+echo "------------------------------------------------"
+echo "Killing the adb server..."
+time adb kill-server
 
-    echo "------------------------------------------------"
-    echo "Starting the adb server..."
-    time adb start-server
-
-#fi
+echo "------------------------------------------------"
+echo "Starting the adb server..."
+time adb start-server
 
 echo "------------------------------------------------"
 echo "Uninstalling the app..."
@@ -68,3 +64,5 @@ cd $startdir
 echo "------------------------------------------------"
 echo "Waiting for the tests to finish executing..."
 time ./expect.exp
+
+#pkill ".*emulator64-x86" && true
