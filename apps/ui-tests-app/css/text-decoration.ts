abe4614735da5248461b3deae930638fdbf4fe1a
Vladimir Enchev <vladimir.enchev@telerik.com>
2015-11-06 16:21:29 +0200

    example updated

---
+++ apps/ui-tests-app/css/text-decoration.ts
@@ -0,0 +1,17 @@
+﻿var obj;
+
+export function loaded(args) {
+    obj = args.object;
+}
+
+export function butonTap(args) {
+    if (obj.style.textDecoration === "underline") {
+        obj.style.textDecoration = "line-through";
+    } else if (obj.style.textDecoration === "line-through") {
+        obj.style.textDecoration = "line-through underline";
+    } else if (obj.style.textDecoration === "line-through underline") {
+        obj.style.textDecoration = "none";
+    } else if (obj.style.textDecoration === "none") {
+        obj.style.textDecoration = "underline";
+    }
+}
