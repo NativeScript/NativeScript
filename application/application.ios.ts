3c5415864590154774d53ada50ebcedb4c9a16e6
hshristov <hristo.hristov@telerik.com>
2015-11-09 11:17:29 +0200

    Implemented liveSync for iOS

--- application/application.ios.ts
+++ application/application.ios.ts
@@ -217,6 +217,10 @@ global.__onUncaughtError = function (error: Error) {
     definition.notify({ eventName: definition.uncaughtErrorEvent, object: <any>definition.ios, ios: error });
 }
 
+global.__onLiveSync = function () {
+    frame.reloadPage();
+}
+
 var started: boolean = false;
 exports.start = function () {
     if (!started) {
