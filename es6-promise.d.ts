57b3d9412fde21b5ce671e4113d75d3d02f0b0b5
Hristo Deshev <hristo@deshev.com>
2015-11-23 16:32:12 +0200

    Fix a compile error in Promise def.

--- es6-promise.d.ts
+++ es6-promise.d.ts
@@ -103,7 +103,7 @@ interface PromiseConstructor {
      * a resolve callback used resolve the promise with a value or the result of another promise,
      * and a reject callback used to reject the promise with a provided reason or error.
      */
-    new <T>(executor: (resolve: (value?: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void): Promise<T>;
+    new <T>(executor: (resolve: (value?: T | Thenable<T>) => void, reject: (reason?: any) => void) => void): Promise<T>;
 
     /**
      * Returns promise (only if promise.constructor == Promise)
