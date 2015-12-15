7ee9be6236c7d4be413fe9d535db1dd3c7ce830f
Hristo Hristov <hristo.hristov@telerik.com>
2015-12-15 16:22:34 +0200

    deleted old project

--- widgets/src/main/java/org/nativescript/widgets/TabItemSpec.java
+++
@@ -1,9 +0,0 @@
-package org.nativescript.widgets;
-
-import android.graphics.drawable.Drawable;
-
-public class TabItemSpec {
-    public String title;
-    public int iconId;
-    public Drawable iconDrawable;
-}
