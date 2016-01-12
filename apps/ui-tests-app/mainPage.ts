8df0b1088e41d16a17b5ae598cacadd7c62eafff
Vasil Chimev <Vasil.Chimev@telerik.com>
2016-01-12 14:57:35 +0200

    Update ui-tests-app

--- apps/ui-tests-app/mainPage.ts
+++ apps/ui-tests-app/mainPage.ts
@@ -32,15 +32,10 @@ export function selectExample() {
     }
 }
 
-examples.set("actColor", "action-bar/color");
-examples.set("actBG", "action-bar/background");
-examples.set("actStyle", "action-bar/all");
-examples.set("actSystemIcons", "action-bar/system-icons");
-
 examples.set("basics", "bindings/basics");
 examples.set("xmlbasics", "bindings/xmlbasics");
 
-examples.set("background", "css/background");
+
 examples.set("radius", "css/radius");
 examples.set("styles", "css/styles");
 
@@ -51,21 +46,7 @@ examples.set("fontlbl", "font/label");
 examples.set("fontfield", "font/text-field");
 examples.set("fontview", "font/text-view");
 
-examples.set("htmlview", "html-view/html-view");
 
-examples.set("roundbtn", "image-view/rounded-buttons");
-examples.set("roundimg", "image-view/rounded-images");
-
-examples.set("absolute", "layouts/absolute");
-examples.set("dock", "layouts/dock");
-examples.set("grid", "layouts/grid");
-examples.set("myview", "layouts/myview");
-examples.set("stack", "layouts/stack");
-examples.set("wrap", "layouts/wrap");
-
-examples.set("modalview", "modal-view/modal-view");
-
-examples.set("nordic", "nordic/nordic");
 
 examples.set("console", "pages/console");
 examples.set("gestures", "pages/gestures");
@@ -77,24 +58,23 @@ examples.set("listview_binding", "pages/listview_binding");
 
 examples.set("switch", "pages/switchandprogress"); // TODO: Update this test page.
 
-examples.set("segStyle", "segmented-bar/all");
-
-examples.set("tabColor", "tab-view/color");
-examples.set("tabBG", "tab-view/background");
-examples.set("tabTabsBG", "tab-view/tabsBackground");
-examples.set("tabSelected", "tab-view/selected");
-examples.set("tabStyle", "tab-view/all");
-
 examples.set("textfield", "text-field/text-field");
-
-examples.set("webview", "web-view/web-view");
-examples.set("webtest", "web-view/web-view-test");
 
 examples.set("decoration", "css/text-decoration");
 examples.set("whitespace", "css/white-space");
 examples.set("transform", "css/text-transform");
 
-// Layouts
+// ---
+
+examples.set("actColor", "action-bar/color");
+examples.set("actBG", "action-bar/background");
+examples.set("actStyle", "action-bar/all");
+examples.set("actIcons", "action-bar/system-icons");
+
+examples.set("background", "css/background");
+
+examples.set("htmlview", "html-view/html-view");
+
 examples.set("absolute", "layouts/absolute");
 examples.set("dock", "layouts/dock");
 examples.set("grid", "layouts/grid");
@@ -108,3 +88,20 @@ examples.set("pgrid", "layouts-percent/grid");
 examples.set("pmyview", "layouts-percent/myview");
 examples.set("pstack", "layouts-percent/stack");
 examples.set("pwrap", "layouts-percent/wrap");
+
+examples.set("modalview", "modal-view/modal-view");
+examples.set("nordic", "nordic/nordic");
+
+examples.set("roundbtn", "image-view/rounded-buttons");
+examples.set("roundimg", "image-view/rounded-images");
+
+examples.set("segStyle", "segmented-bar/all");
+
+examples.set("tabColor", "tab-view/color");
+examples.set("tabBG", "tab-view/background");
+examples.set("tabTabsBG", "tab-view/tabsBackground");
+examples.set("tabSelected", "tab-view/selected");
+examples.set("tabStyle", "tab-view/all");
+
+examples.set("webview", "web-view/web-view");
+examples.set("webtest", "web-view/web-view-test");
