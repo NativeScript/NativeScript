39cc657154711767c1fdd852a1b7b1b84f170d8f
Vladimir Enchev <vladimir.enchev@telerik.com>
2016-03-16 15:41:15 +0200

    rgb() and rgba() support added

--- apps/tests/color-tests.ts
+++ apps/tests/color-tests.ts
@@ -16,12 +16,12 @@ export var test_Hex_Color = function () {
     var color = new Color("#FF0000");
     // ```
     // </snippet>
-    TKUnit.assert(color.a === 255, "Color.a not properly parsed");
-    TKUnit.assert(color.r === 255, "Color.r not properly parsed");
-    TKUnit.assert(color.g === 0, "Color.g not properly parsed");
-    TKUnit.assert(color.b === 0, "Color.b not properly parsed");
-    TKUnit.assert(color.hex === "#FF0000", "Color.hex not properly parsed");
-    TKUnit.assert(color.argb === _parseArgb(255, 255, 0, 0), "Color.argb not properly parsed");
+    TKUnit.assertEqual(color.a, 255, "Color.a not properly parsed");
+    TKUnit.assertEqual(color.r, 255, "Color.r not properly parsed");
+    TKUnit.assertEqual(color.g, 0, "Color.g not properly parsed");
+    TKUnit.assertEqual(color.b, 0, "Color.b not properly parsed");
+    TKUnit.assertEqual(color.hex, "#FF0000", "Color.hex not properly parsed");
+    TKUnit.assertEqual(color.argb, _parseArgb(255, 255, 0, 0), "Color.argb not properly parsed");
 }
 
 export var test_ShortHex_Color = function () {
@@ -32,12 +32,12 @@ export var test_ShortHex_Color = function () {
     var color = new Color("#F80");
     // ```
     // </snippet>
-    TKUnit.assert(color.a === 255, "Color.a not properly parsed");
-    TKUnit.assert(color.r === 255, "Color.r not properly parsed");
-    TKUnit.assert(color.g === 136, "Color.g not properly parsed"); // 0x88 == 136
-    TKUnit.assert(color.b === 0, "Color.b not properly parsed");
-    TKUnit.assert(color.hex === "#FF8800", "Color.hex not properly parsed");
-    TKUnit.assert(color.argb === _parseArgb(255, 255, 136, 0), "Color.argb not properly parsed");
+    TKUnit.assertEqual(color.a, 255, "Color.a not properly parsed");
+    TKUnit.assertEqual(color.r, 255, "Color.r not properly parsed");
+    TKUnit.assertEqual(color.g, 136, "Color.g not properly parsed"); // 0x88 == 136
+    TKUnit.assertEqual(color.b, 0, "Color.b not properly parsed");
+    TKUnit.assertEqual(color.hex, "#FF8800", "Color.hex not properly parsed");
+    TKUnit.assertEqual(color.argb, _parseArgb(255, 255, 136, 0), "Color.argb not properly parsed");
 }
 
 export var test_Argb_Color = function () {
@@ -48,12 +48,12 @@ export var test_Argb_Color = function () {
     var color = new Color(100, 255, 100, 100);
     // ```
     // </snippet>
-    TKUnit.assert(color.a === 100, "Color.a not properly parsed");
-    TKUnit.assert(color.r === 255, "Color.r not properly parsed");
-    TKUnit.assert(color.g === 100, "Color.g not properly parsed");
-    TKUnit.assert(color.b === 100, "Color.b not properly parsed");
-    TKUnit.assert(color.hex === _buildHex(100, 255, 100, 100), "Color.hex not properly parsed");
-    TKUnit.assert(color.argb === _parseArgb(100, 255, 100, 100), "Color.argb not properly parsed");
+    TKUnit.assertEqual(color.a, 100, "Color.a not properly parsed");
+    TKUnit.assertEqual(color.r, 255, "Color.r not properly parsed");
+    TKUnit.assertEqual(color.g, 100, "Color.g not properly parsed");
+    TKUnit.assertEqual(color.b, 100, "Color.b not properly parsed");
+    TKUnit.assertEqual(color.hex, _buildHex(100, 255, 100, 100), "Color.hex not properly parsed");
+    TKUnit.assertEqual(color.argb, _parseArgb(100, 255, 100, 100), "Color.argb not properly parsed");
 }
 
 export var test_ArgbInt_Color = function () {
@@ -65,12 +65,46 @@ export var test_ArgbInt_Color = function () {
     var color = new Color(argb);
     // ```
     // </snippet>
-    TKUnit.assert(color.a === 100, "Color.a not properly parsed");
-    TKUnit.assert(color.r === 100, "Color.r not properly parsed");
-    TKUnit.assert(color.g === 100, "Color.g not properly parsed");
-    TKUnit.assert(color.b === 100, "Color.b not properly parsed");
-    TKUnit.assert(color.hex === _buildHex(100, 100, 100, 100), "Color.hex not properly parsed");
-    TKUnit.assert(color.argb === _parseArgb(100, 100, 100, 100), "Color.argb not properly parsed");
+    TKUnit.assertEqual(color.a, 100, "Color.a not properly parsed");
+    TKUnit.assertEqual(color.r, 100, "Color.r not properly parsed");
+    TKUnit.assertEqual(color.g, 100, "Color.g not properly parsed");
+    TKUnit.assertEqual(color.b, 100, "Color.b not properly parsed");
+    TKUnit.assertEqual(color.hex, _buildHex(100, 100, 100, 100), "Color.hex not properly parsed");
+    TKUnit.assertEqual(color.argb, _parseArgb(100, 100, 100, 100), "Color.argb not properly parsed");
+}
+
+export var test_rgb_Color_CSS = function () {
+    // <snippet module="color" title="color">
+    // ### Creating a Color from four RGB values
+    // ``` JavaScript
+    //// Creates the color with 255 red, 100 green, 100 blue
+    var color = new Color("rgb(255, 100, 100)");
+    // ```
+    // </snippet>
+    TKUnit.assertEqual(color.a, 255, "Color.a not properly parsed");
+    TKUnit.assertEqual(color.r, 255, "Color.r not properly parsed");
+    TKUnit.assertEqual(color.g, 100, "Color.g not properly parsed");
+    TKUnit.assertEqual(color.b, 100, "Color.b not properly parsed");
+    TKUnit.assertEqual(color.hex, _buildHex(255, 255, 100, 100), "Color.hex not properly parsed");
+    TKUnit.assertEqual(color.argb, _parseArgb(255, 255, 100, 100), "Color.argb not properly parsed");
+}
+
+export var test_rgba_Color_CSS = function () {
+    var alpha = 0.5;
+    var expected = Math.round(alpha * 255);
+    // <snippet module="color" title="color">
+    // ### Creating a Color from four RGB values
+    // ``` JavaScript
+    //// Creates the color with 255 red, 100 green, 100 blue and 0 alpha
+    var color = new Color(`rgba(255, 100, 100, ${alpha})`);
+    // ```
+    // </snippet>
+    TKUnit.assertEqual(color.a, expected, "Color.a not properly parsed");
+    TKUnit.assertEqual(color.r, 255, "Color.r not properly parsed");
+    TKUnit.assertEqual(color.g, 100, "Color.g not properly parsed");
+    TKUnit.assertEqual(color.b, 100, "Color.b not properly parsed");
+    TKUnit.assertEqual(color.hex, _buildHex(expected, 255, 100, 100), "Color.hex not properly parsed");
+    TKUnit.assertEqual(color.argb, _parseArgb(expected, 255, 100, 100), "Color.argb not properly parsed");
 }
 
 var _buildHex = function (a: number, r: number, g: number, b: number): string {
