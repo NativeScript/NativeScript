3ef45c19841c32f5073c46ee2ef8f8b431974b94
Nathan Walker <walkerrunpdx@gmail.com>
2016-03-24 09:02:20 -0700

    fix(es6): objectcontstructor assign

--- es6.d.ts
+++ es6.d.ts
@@ -1,1 +1,9 @@
 declare var Symbol: any;
+
+interface ObjectConstructor {
+    assign(target: any, ...sources: any[]): any;
+    is(value1: any, value2: any): boolean;
+    setPrototypeOf(o: any, proto: any): any;
+}
+
+declare var Object: ObjectConstructor;
