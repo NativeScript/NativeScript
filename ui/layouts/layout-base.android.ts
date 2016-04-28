54c65e94d7ed58e45eaf46f110384b268cb3ac41
Rossen Hristov <rossen.hristov@telerik.com>
2016-04-28 17:35:06 +0300

    Split layout-base to iOS and Android files
    clipToBounds for a single view can't be implemented in Android in a straightforward manner
    
    Resolves #1829

---
+++ ui/layouts/layout-base.android.ts
@@ -0,0 +1,16 @@
+﻿import common = require("./layout-base-common");
+
+export class LayoutBase extends common.LayoutBase {
+    public _onClipToBoundsChanged(oldValue: boolean, newValue: boolean) {
+        // We can't implement this without calling setClipChildren(false) on every ancestor up in the visual tree, 
+        // which will kill performance. It will also lead to unwanted side effects such as other totally unrelated 
+        // views being affected by setting the parents' setClipChildren to false. 
+        // The problem in Android is that a ViewGroup either clips ALL of its children or it does not. Unlike iOS, the clipping 
+        // cannot be controlled on a per view basis. So clipToBounds=false will have to be somehow achieved with stacking different
+        // views on top of one another in an AbsoluteLayout or GridLayout. There is always a workaround when playing with layouts.
+        //
+        // The following article explains this in detail:
+        // http://stackoverflow.com/questions/25044085/when-drawing-outside-the-view-clip-bounds-with-android-how-do-i-prevent-underli
+        console.warn(`clipToBounds with value false is not supported on Android. You can use this.android.getParent().setClipChildren(false) as an alternative`);
+    }
+}
