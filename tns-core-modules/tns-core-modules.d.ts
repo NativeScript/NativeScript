58b4a6eec626cd08addbded0de80fb4f3a467fb0
Panayot Cankov <Panayot.Cankov@telerik.com>
2016-05-30 09:24:33 +0300

    Add 'npm test' for mac

---
+++ tns-core-modules/tns-core-modules.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="module.d.ts" />
+/// <reference path="es-collections.d.ts" />
+/// <reference path="es6-promise.d.ts" />
+/// <reference path="es6.d.ts" />
+/// <reference path="weakmap.d.ts" />
+/// <reference path="tns-core-modules.base.d.ts" />
--- tns-core-modules/ui/styling/background.android.ts
+++ tns-core-modules/ui/styling/background.android.ts
@@ -6,6 +6,19 @@ import types = require("utils/types");
 import * as styleModule from "./style";
 import * as buttonModule from "ui/button";
 
+//@private
+declare module "ui/styling/background" {
+    // We are using "ad" here to avoid namespace collision with the global android object
+    export module ad {
+        export class BorderDrawable extends android.graphics.drawable.ColorDrawable {
+            borderWidth: number;
+            cornerRadius: number;
+            borderColor: number;
+            background: Background;
+        }
+    }
+}
+
 var button: typeof buttonModule;
 var style: typeof styleModule;
 
