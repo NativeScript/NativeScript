149c0f44f735d11f0790ab56fecbe73d9e4699c7
Hristo Hristov <Hristo.Hristov@telerik.com>
2016-06-20 13:36:42 +0300

    Add css-perf app.

---
+++ apps/css-perf-test/main-page.ts
@@ -0,0 +1,9 @@
+﻿import {EventData as ObservableEventData} from "data/observable";
+import {Page} from "ui/page";
+
+export function navigatedTo(args: ObservableEventData) {
+    setTimeout(() => {
+        __stopCPUProfiler("alabala");
+        console.log(`Time: ${global.time() - global.startTime} ms`);
+    });
+}
---
+++ apps/css-perf-test/root.ts
@@ -0,0 +1,8 @@
+﻿import {Page} from "ui/page";
+
+export function onTap(args: any) {
+    global.startTime = global.time();
+    let page = <Page>args.object.page;
+    __startCPUProfiler("alabala");
+    page.frame.navigate("css-perf-test/main-page");
+}
