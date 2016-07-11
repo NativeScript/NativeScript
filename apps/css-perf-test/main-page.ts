1e96af27542392aa876d31d87fbb9fd3e0fb5043
Rossen Hristov <rossen.hristov@telerik.com>
2016-07-11 15:35:40 +0300

    Delete css-perftest

--- apps/css-perf-test/main-page.ts
+++
@@ -1,7 +0,0 @@
-﻿import {EventData as ObservableEventData} from "data/observable";
-
-export function navigatedTo(args: ObservableEventData) {
-    setTimeout(() => {
-        console.log(`Time: ${global.time() - global.startTime} ms`);
-    });
-}
