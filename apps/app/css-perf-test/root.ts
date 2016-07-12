525d0c43e351dca57d6c40de6adc9e68d641f440
Rossen Hristov <rossen.hristov@telerik.com>
2016-07-12 10:00:32 +0300

    Fix ui-tests-app paths and add css-perf-test app

---
+++ apps/app/css-perf-test/root.ts
@@ -0,0 +1,7 @@
+﻿import {Page} from "ui/page";
+
+export function onTap(args: any) {
+    global.startTime = global.time();
+    let page = <Page>args.object.page;
+    page.frame.navigate("css-perf-test/main-page");
+}
