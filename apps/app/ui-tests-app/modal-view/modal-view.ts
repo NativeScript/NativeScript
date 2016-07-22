16b5214013668763b6746d3c72ac99aca91a6bce
Rossen Hristov <rossen.hristov@telerik.com>
2016-07-22 11:18:47 +0300

    Fix two ui tests

--- apps/app/ui-tests-app/modal-view/modal-view.ts
+++ apps/app/ui-tests-app/modal-view/modal-view.ts
@@ -12,7 +12,7 @@ export function pageLoaded(args: observable.EventData) {
 
 export function onTap(args: observable.EventData) {
     var fullscreen = (<any>args.object).text.indexOf("(full-screen)") !== -1;
-    page.showModal("modal-view/login-page", "context", function (username: string, password: string) {
+    page.showModal("ui-tests-app/modal-view/login-page", "context", function (username: string, password: string) {
         console.log(username + "/" + password);
         label.text = username + "/" + password;
     }, fullscreen);
