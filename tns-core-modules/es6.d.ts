e0a19d2f48646332e634e0f4e1d330a5e1843a3a
Hristo Hristov <Hristo.Hristov@telerik.com>
2016-08-15 13:45:07 +0300

    Removed all comments from es6.d.ts (TSLint hell)

--- tns-core-modules/es6.d.ts
+++ tns-core-modules/es6.d.ts
@@ -1,21 +1,10 @@
 interface Symbol {
-    /** Returns a string representation of an object. */
     toString(): string;
-
-    /** Returns the primitive value of the specified object. */
     valueOf(): Object;
 }
 
 interface SymbolConstructor {
-    /** 
-      * A reference to the prototype. 
-      */
     prototype: Symbol;
-
-    /**
-      * Returns a new unique Symbol value.
-      * @param  description Description of the new Symbol object.
-      */
     (description?: string | number): symbol;
 }
 
@@ -25,10 +14,6 @@ interface ObjectConstructor {
     assign(target: any, ...sources: any[]): any;
     is(value1: any, value2: any): boolean;
     setPrototypeOf(o: any, proto: any): any;
-    /**
-      * Returns an array of all symbol properties found directly on object o.
-      * @param o Object to retrieve the symbols from.
-      */
     getOwnPropertySymbols(o: any): symbol[];
 }
 
