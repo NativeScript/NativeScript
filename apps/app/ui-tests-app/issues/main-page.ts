83e0ae4c85299c8481b8039cf00490221d11ca58
Rossen Hristov <rossen.hristov@telerik.com>
2016-10-26 15:36:37 +0300

    Fix: CSS scale breaks background properties on iOS
    Resolves #2911

---
+++ apps/app/ui-tests-app/issues/main-page.ts
@@ -0,0 +1,24 @@
+import { EventData } from "data/observable";
+import { MianPageViewModel } from "../mainPage";
+import { WrapLayout } from "ui/layouts/wrap-layout";
+import { Page } from "ui/page";
+
+export function pageLoaded(args: EventData) {
+    let page = <Page>args.object;
+    let view = require("ui/core/view");
+
+    let wrapLayout = view.getViewById(page, "wrapLayoutWithExamples");
+
+    let examples: Map<string, string> = new Map<string, string>();
+    
+    examples.set("2911", "issues/issue-2911");
+
+    let viewModel = new SubMianPageViewModel(wrapLayout, examples);
+    page.bindingContext = viewModel;
+}
+
+export class SubMianPageViewModel extends MianPageViewModel {
+    constructor(container: WrapLayout, examples: Map<string, string>) {
+        super(container, examples);
+    }
+}
