fe3454529c57816ccb208ce004c51e69d9762fad
vB Results, LLC <vbresults@users.noreply.github.com>
2016-10-30 12:29:28 +0530

    Library/Gallery as camera source.

--- tns-core-modules/camera/camera-common.ts
+++ tns-core-modules/camera/camera-common.ts
@@ -1,11 +1,39 @@
-﻿export function getAspectSafeDimensions(sourceWidth, sourceHeight, reqWidth, reqHeight) {
-    let widthCoef = sourceWidth / reqWidth;
-    let heightCoef = sourceHeight / reqHeight;
-
-    let aspectCoef = widthCoef > heightCoef ? widthCoef : heightCoef;
-
-    return {
-        width: Math.floor(sourceWidth / aspectCoef),
-        height: Math.floor(sourceHeight / aspectCoef)
+﻿export class Camera {
+    static modes = {
+        photo: "photo",
+        video: "video"
     };
+
+    static sources = {
+        device: "device",
+        library: "library",
+        roll: "roll"
+    };
+
+    static getAspectSafeDimensions(sourceWidth, sourceHeight, reqWidth, reqHeight) {
+        let widthCoef = sourceWidth / reqWidth;
+        let heightCoef = sourceHeight / reqHeight;
+        let aspectCoef = widthCoef > heightCoef ? widthCoef : heightCoef;
+
+        return {
+            width: Math.floor(sourceWidth / aspectCoef),
+            height: Math.floor(sourceHeight / aspectCoef)
+        };
+    }
+
+    static getMode(mode) {
+        if (!mode || typeof mode != "string" || Object.keys(Camera.modes).indexOf(mode) === -1) {
+            return Camera.modes.photo;
+        }
+
+        return mode;
+    }
+
+    static getSource(source) {
+        if (!source || typeof source != "string" || Object.keys(Camera.sources).indexOf(source) === -1) {
+            return Camera.sources.device;
+        }
+
+        return source;
+    }
 }
