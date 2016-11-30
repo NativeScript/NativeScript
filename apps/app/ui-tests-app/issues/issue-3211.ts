35c48b30e6354987e0cbe0d81a5198bbbc6e4cdc
zh-m <maya.zhecheva@gmail.com>
2016-11-30 15:25:09 +0200

    UI test for setting bckg and color of buttons inside dialogs

---
+++ apps/app/ui-tests-app/issues/issue-3211.ts
@@ -0,0 +1,12 @@
+import { alert } from "ui/dialogs";
+
+export function onTap() {
+     var options = {
+            title: "Race selection",
+            message: "Race chosen: Unicorn",
+            okButtonText: "OK"
+        };
+        alert(options).then(() => {
+            console.log("Race chosen!");
+        });
+}
