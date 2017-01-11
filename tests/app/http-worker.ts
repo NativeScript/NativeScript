c5fabe8594ad3aabac25aa8cf3d36b2bddaeb899
Vladimir Enchev <vladimir.enchev@gmail.com>
2017-01-11 15:56:03 +0200

    file deleted

--- tests/app/http-worker.ts
+++
@@ -1,8 +0,0 @@
-import http = require("http");
-
-declare var postMessage : any;
-http.getString("https://httpbin.org/get").then(function (r) {
-    postMessage(r);
-}, function (e) {
-    postMessage(e);
-});
