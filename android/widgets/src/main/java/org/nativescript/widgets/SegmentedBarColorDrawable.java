aa14ce8b80a01f10a60695527822730fc4d138da
Hristo Hristov <hristo.hristov@telerik.com>
2017-03-09 16:23:16 +0200

    Add construtor that accepts color. (#88)

--- android/widgets/src/main/java/org/nativescript/widgets/SegmentedBarColorDrawable.java
+++ android/widgets/src/main/java/org/nativescript/widgets/SegmentedBarColorDrawable.java
@@ -2,12 +2,17 @@ package org.nativescript.widgets;
 
 import android.graphics.Paint;
 import android.graphics.drawable.ColorDrawable;
+import android.support.annotation.ColorInt;
 
 /**
  * Created by hhristov on 2/23/17.
  */
 
 public class SegmentedBarColorDrawable extends ColorDrawable {
+    public SegmentedBarColorDrawable(@ColorInt int color) {
+        super(color);
+    }
+
     public void draw(android.graphics.Canvas canvas) {
         Paint p = new Paint();
         p.setColor(this.getColor());
