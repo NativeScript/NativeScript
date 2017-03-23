89a01107d3e507367cb1e33e304f692ce42af1c5
Panayot Cankov <panayot.cankov@telerik.com>
2017-03-23 17:21:03 +0200

    Added perf category in the tests suite and the synthetic properties perf app from HHristov

---
+++ apps/app/ui-tests-app/perf/main-page.ts
@@ -0,0 +1,23 @@
+import { EventData } from "tns-core-modules/data/observable";
+import { MainPageViewModel } from "../mainPage";
+import { WrapLayout } from "tns-core-modules/ui/layouts/wrap-layout";
+import { Page } from "tns-core-modules/ui/page";
+
+export function pageLoaded(args: EventData) {
+    let page = <Page>args.object;
+    let view = require("ui/core/view");
+
+    let wrapLayout = view.getViewById(page, "wrapLayoutWithExamples");
+
+    let examples: Map<string, string> = new Map<string, string>();
+    examples.set("properties", "perf/properties/main-page");
+
+    let viewModel = new SubMainPageViewModel(wrapLayout, examples);
+    page.bindingContext = viewModel;
+}
+
+export class SubMainPageViewModel extends MainPageViewModel {
+    constructor(container: WrapLayout, examples: Map<string, string>) {
+        super(container, examples);
+    }
+}
