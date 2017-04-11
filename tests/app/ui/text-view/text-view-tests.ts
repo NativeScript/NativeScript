6c72c07f3a94711d7f986019f6e64bdd2517e444
NickIliev <Nikolay.Iliev@telerik.com>
2017-04-11 11:50:43 +0300

    uncommented code snippet

--- tests/app/ui/text-view/text-view-tests.ts
+++ tests/app/ui/text-view/text-view-tests.ts
@@ -26,13 +26,13 @@ import * as observable from "tns-core-modules/data/observable";
 // </Page>
 // << text-view-xml
 // >> observable-declare
-// export function pageLoaded(args) {
-//   let page = args.object;
-//   let obj = new observable.Observable();
-//   obj.set("someProperty", "Please change this text!");
-//   page.bindingContext = obj;
-// }
-// exports.pageLoaded = pageLoaded;
+export function pageLoaded(args) {
+  let page = args.object;
+  let obj = new observable.Observable();
+  obj.set("someProperty", "Please change this text!");
+  page.bindingContext = obj;
+}
+exports.pageLoaded = pageLoaded;
 // << observable-declare
 
 var _createTextViewFunc = function (): textViewModule.TextView {
