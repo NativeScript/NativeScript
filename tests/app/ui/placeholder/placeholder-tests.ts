502a37d27c4e0a5d44d82bd08800ebfbc9f1aad2
Hristo Hristov <hristo.hristov@telerik.com>
2017-04-26 14:27:24 +0300

    Fix ios placeholder implementation (#4067)
    * Fix https://github.com/NativeScript/NativeScript/issues/4043
    
    * Merge ios & android implementation

--- tests/app/ui/placeholder/placeholder-tests.ts
+++ tests/app/ui/placeholder/placeholder-tests.ts
@@ -1,7 +1,6 @@
 ﻿import * as TKUnit from "../../TKUnit";
 // >> article-creating-view
-import * as platform from "tns-core-modules/platform";
-//var utils = require("utils/utils");
+import { isIOS, isAndroid } from "tns-core-modules/platform";
 import * as utils from "tns-core-modules/utils/utils";
 import * as helper from "../helper";
 import * as viewModule from "tns-core-modules/ui/core/view";
@@ -11,53 +10,54 @@ import * as placeholderModule from "tns-core-modules/ui/placeholder";
 // << article-require-placeholder-module
 
 function creatingView(args) {
-   var nativeView;
-   if (platform.device.os === platform.platformNames.ios) {
-       nativeView = UITextView.new();
-       nativeView.text = "Native";
-   } else if (platform.device.os === platform.platformNames.android) {
-       nativeView = new android.widget.TextView(utils.ad.getApplicationContext());
-       nativeView.setText("Native");
-   }
+    let nativeView;
+    if (isIOS) {
+        nativeView = UITextView.new();
+        nativeView.text = "Native";
+    } else if (isAndroid) {
+        nativeView = new android.widget.TextView(utils.ad.getApplicationContext());
+        nativeView.setText("Native");
+    }
 
-   args.view = nativeView;
+    args.view = nativeView;
 }
 
 exports.creatingView = creatingView;
 // << article-creating-view
 
 export function test_placeholder_creatingView() {
-    var nativeView;
-
-    var p = new placeholderModule.Placeholder();
-    p.id = "test";
+    const p = new placeholderModule.Placeholder();
     p.on(placeholderModule.Placeholder.creatingViewEvent, (args: placeholderModule.CreateViewEventData) => {
-        if (platform.device.os === platform.platformNames.ios) {
-        nativeView = UITextView.new();
+        let nativeView;
+        if (isIOS) {
+            nativeView = UITextView.new();
             nativeView.text = "Native";
-        } else if (platform.device.os === platform.platformNames.android) {
+        } else if (isAndroid) {
             nativeView = new android.widget.TextView(utils.ad.getApplicationContext());
             nativeView.setText("Native");
         }
-    
+
         args.view = nativeView;
     });
 
-    if (platform.device.os === platform.platformNames.ios) {
-       TKUnit.assert(p.ios instanceof UITextView, "ios property should be UITextView. Current value: " + p.ios);
-    } else if (platform.device.os === platform.platformNames.android) {
-       p._emit("creatingView");
-       TKUnit.assert(nativeView instanceof android.widget.TextView, "Native view should be android.widget.TextView. Current value: " + nativeView);
-    }
+    function testAction(views: Array<viewModule.View>) {
+        if (isIOS) {
+            TKUnit.assert(p.nativeView instanceof UITextView, "nativeView property should be UITextView. Current value: " + p.nativeView);
+        } else if (isAndroid) {
+            TKUnit.assert(p.nativeView instanceof android.widget.TextView, "Native view should be android.widget.TextView. Current value: " + p.nativeView);
+        }
+    };
+
+    helper.buildUIAndRunTest(p, testAction);
 }
 
 export function test_placeholder_will_not_crash_wihout_creatingView() {
-    var p = new placeholderModule.Placeholder();   
+   const p = new placeholderModule.Placeholder();
 
     function testAction(views: Array<viewModule.View>) {
-        if (platform.device.os === platform.platformNames.ios) {
+        if (isIOS) {
             TKUnit.assert(p.ios === undefined, "ios property should be undefined. Current value: " + p.ios);
-        } else if (platform.device.os === platform.platformNames.android) {
+        } else if (isAndroid) {
             TKUnit.assert(p.android === undefined, "android view should be undefined. Current value: " + p.android);
         }
     };
