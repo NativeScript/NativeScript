f76982b942535d9a25c10bbc87d9ee39b8689ecf
Panayot Cankov <panayot.cankov@telerik.com>
2017-05-15 17:53:16 +0300

    Add C function to obtain the time since the process started.

---
+++ ios/TNSWidgets/TNSProcess.h
@@ -0,0 +1,17 @@
+//
+//  TNSProcess.h
+//  TNSWidgets
+//
+//  Created by Panayot Cankov on 15/05/2017.
+//  Copyright © 2017 Telerik A D. All rights reserved.
+//
+
+#ifndef TNSProcess_h
+#define TNSProcess_h
+
+/**
+ * Get the milliseconds since the process started.
+ */
+double __tns_uptime();
+
+#endif /* TNSProcess_h */
