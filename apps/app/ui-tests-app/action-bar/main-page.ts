2b84e9049bc4b6ea824ac2b148b20efb6df6e8a6
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2017-06-05 15:36:12 +0300

    Init steps

--- apps/app/ui-tests-app/action-bar/main-page.ts
+++ apps/app/ui-tests-app/action-bar/main-page.ts
@@ -9,7 +9,7 @@ export function pageLoaded(args: EventData) {
 
     let wrapLayout = view.getViewById(page, "wrapLayoutWithExamples");
 
-    let examples: Map<string, string> = new Map<string, string>();
+    let examples: Map<string, string> = null;
     examples.set("actColor", "action-bar/color");
     examples.set("actBG", "action-bar/background");
     examples.set("actStyle", "action-bar/all");
@@ -23,6 +23,22 @@ export function pageLoaded(args: EventData) {
 
     let viewModel = new SubMainPageViewModel(wrapLayout, examples);
     page.bindingContext = viewModel;
+}
+
+export function loadExamples(examples: Map<string, string>) {
+    examples = new Map<string, string>();
+    examples.set("actColor", "action-bar/color");
+    examples.set("actBG", "action-bar/background");
+    examples.set("actStyle", "action-bar/all");
+    examples.set("actIcons", "action-bar/system-icons");
+    examples.set("actView", "action-bar/action-view");
+    examples.set("actionItemPosition", "action-bar/action-item-position");
+    examples.set("actBGCss", "action-bar/background-css");
+    examples.set("actTransparentBgCss", "action-bar/transparent-bg-css");
+    examples.set("modalHiddenActBar", "action-bar/modal-test-hidden-action-bar");
+    examples.set("modalShownActBar", "action-bar/modal-test-with-action-bar");
+
+    return examples;
 }
 
 export class SubMainPageViewModel extends MainPageViewModel {
