eda99bcdc9c7dffa3bd5bed93252ae8a37cdbd4c
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2017-06-05 11:06:00 +0300

    Extend test page issue-3354

--- apps/app/ui-tests-app/issues/issue-3354.ts
+++ apps/app/ui-tests-app/issues/issue-3354.ts
@@ -1,4 +1,6 @@
 export function clear(args) {
-    const tv = args.object.page.getViewById("tv");
-    tv.text= "";
+    const tv1 = args.object.page.getViewById("tv1");
+    tv1.text = "";
+    const tv2 = args.object.page.getViewById("tv2");
+    tv2.text = "";
 }
