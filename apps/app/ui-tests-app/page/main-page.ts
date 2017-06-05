90fbaa266b9bd08216ecaa74ce4b22de8fe15649
Svetoslav Tsenov <tsenov.svetoslav@gmail.com>
2017-06-06 00:46:54 +0300

    Expose TestPageMainViewModel

--- apps/app/ui-tests-app/page/main-page.ts
+++ apps/app/ui-tests-app/page/main-page.ts
@@ -1,5 +1,5 @@
 import { EventData } from "tns-core-modules/data/observable";
-import { MainPageViewModel } from "../mainPage";
+import { TestPageMainViewModel } from "../test-page-main-view-model";
 import { WrapLayout } from "tns-core-modules/ui/layouts/wrap-layout";
 import { Page } from "tns-core-modules/ui/page";
 
@@ -18,7 +18,7 @@ export function loadExamples() {
     return examples;
 }
 
-export class SubMainPageViewModel extends MainPageViewModel {
+export class SubMainPageViewModel extends TestPageMainViewModel {
     constructor(container: WrapLayout, examples: Map<string, string>) {
         super(container, examples);
     }
