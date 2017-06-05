6885c082ba7d4cf0f625f9710917da781801ab16
Svetoslav Tsenov <tsenov.svetoslav@gmail.com>
2017-06-06 01:45:19 +0300

    Merge

--- apps/app/ui-tests-app/perf/memory-leaks/main-page.ts
+++ apps/app/ui-tests-app/perf/memory-leaks/main-page.ts
@@ -1,5 +1,5 @@
 import { EventData } from "tns-core-modules/data/observable";
-import { MainPageViewModel } from "../../mainPage";
+import { TestPageMainViewModel } from "../../test-page-main-view-model";
 import { WrapLayout } from "tns-core-modules/ui/layouts/wrap-layout";
 import { Page } from "tns-core-modules/ui/page";
 import { isAndroid } from "tns-core-modules/platform";
@@ -18,7 +18,7 @@ export function pageLoaded(args: EventData) {
     page.bindingContext = viewModel;
 }
 
-export class SubMainPageViewModel extends MainPageViewModel {
+export class SubMainPageViewModel extends TestPageMainViewModel {
     constructor(container: WrapLayout, examples: Map<string, string>) {
         super(container, examples);
     }
