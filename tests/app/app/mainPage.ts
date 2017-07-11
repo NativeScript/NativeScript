19b5e66d858649d4648caf570391d8293be2a53c
vchimev <Vasil.Chimev@telerik.com>
2017-07-11 16:33:49 +0300

    Remove permission request from tests app

--- tests/app/app/mainPage.ts
+++ tests/app/app/mainPage.ts
@@ -33,27 +33,29 @@ function onNavigatedTo(args) {
     page.content = label;
     args.object.off(Page.navigatedToEvent, onNavigatedTo);
 
-    if (platform.isAndroid && parseInt(platform.device.sdkVersion) >= 23) {
-        let handler = (args: application.AndroidActivityRequestPermissionsEventData) => {
-            application.android.off(application.AndroidApplication.activityRequestPermissionsEvent, handler);
-            if (args.requestCode === 1234 && args.grantResults.length > 0 && args.grantResults[0] === android.content.pm.PackageManager.PERMISSION_GRANTED) {
-                runTests();
-            } else {
-                trace.write("Permission for write to external storage not granted!", trace.categories.Error, trace.messageType.error);
-            }
-        };
+    // Request permission to write test-results.xml file for API >= 23
+    // if (platform.isAndroid && parseInt(platform.device.sdkVersion) >= 23) {
+    //     let handler = (args: application.AndroidActivityRequestPermissionsEventData) => {
+    //         application.android.off(application.AndroidApplication.activityRequestPermissionsEvent, handler);
+    //         if (args.requestCode === 1234 && args.grantResults.length > 0 && args.grantResults[0] === android.content.pm.PackageManager.PERMISSION_GRANTED) {
+    //             runTests();
+    //         } else {
+    //             trace.write("Permission for write to external storage not granted!", trace.categories.Error, trace.messageType.error);
+    //         }
+    //     };
 
-        application.android.on(application.AndroidApplication.activityRequestPermissionsEvent, handler);
+    //     application.android.on(application.AndroidApplication.activityRequestPermissionsEvent, handler);
 
-        if ((<any>android.support.v4.content.ContextCompat).checkSelfPermission(application.android.currentContext, (<any>android).Manifest.permission.WRITE_EXTERNAL_STORAGE) !== android.content.pm.PackageManager.PERMISSION_GRANTED) {
-            (<any>android.support.v4.app.ActivityCompat).requestPermissions(application.android.currentContext, [(<any>android).Manifest.permission.WRITE_EXTERNAL_STORAGE], 1234);
-        } else {
-            runTests();
-        }
-    } else {
-        runTests();
-    }
+    //     if ((<any>android.support.v4.content.ContextCompat).checkSelfPermission(application.android.currentContext, (<any>android).Manifest.permission.WRITE_EXTERNAL_STORAGE) !== android.content.pm.PackageManager.PERMISSION_GRANTED) {
+    //         (<any>android.support.v4.app.ActivityCompat).requestPermissions(application.android.currentContext, [(<any>android).Manifest.permission.WRITE_EXTERNAL_STORAGE], 1234);
+    //     } else {
+    //         runTests();
+    //     }
+    // } else {
+    //     runTests();
+    // }
 
+    runTests();
 }
 export function createPage() {
     return page;
