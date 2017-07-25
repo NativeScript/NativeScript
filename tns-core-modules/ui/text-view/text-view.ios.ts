5a660a6eda3b3617a262dcd8c0eeea7c090e0aa2
Alexander Vakrilov <alexander.vakrilov@telerik.com>
2017-07-25 11:22:21 +0300

    Set text-field and text-view tintColor to textColor (iOS) (fixes #4357) EXTENDED (#4549)
    * set text-field and text-view tintColor to textColor (#4357)
    
    * Rest tint color

--- tns-core-modules/ui/text-view/text-view.ios.ts
+++ tns-core-modules/ui/text-view/text-view.ios.ts
@@ -135,7 +135,7 @@ export class TextView extends EditableTextBase implements TextViewDefinition {
             if (placeholderColor) {
                 this.nativeView.textColor = placeholderColor.ios;
             } else if (color) {
-                // Use semi-transparent vesion of color for back-compatibility 
+                // Use semi-transparent version of color for back-compatibility 
                 this.nativeView.textColor = color.ios.colorWithAlphaComponent(0.22);
             } else {
                 this.nativeView.textColor = UIColor.blackColor.colorWithAlphaComponent(0.22);
@@ -145,8 +145,10 @@ export class TextView extends EditableTextBase implements TextViewDefinition {
 
             if (color) {
                 this.nativeView.textColor = color.ios;
+                this.nativeView.tintColor = color.ios;
             } else {
-                this.nativeView.textColor = UIColor.blackColor;
+                this.nativeView.textColor = null;
+                this.nativeView.tintColor = null;
             }
         }
     }
