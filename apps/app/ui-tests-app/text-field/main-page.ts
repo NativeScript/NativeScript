3fd65cce9f3797a0de1a9ed17041bbef69c0da2d
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2017-08-28 23:29:56 +0300

    Include ui tests for focus/ blur events for text-view and text-field (#4765)
    * Include ui tests for focus/ blur events for text-view and text-field
    
    * Update main-page.ts

--- apps/app/ui-tests-app/text-field/main-page.ts
+++ apps/app/ui-tests-app/text-field/main-page.ts
@@ -14,6 +14,7 @@ export function loadExamples() {
     examples.set("secured-text-field", "text-field/secured-text-field-4135");
     examples.set("max-length", "text-field/max-length");
     examples.set("text-field-border", "text-field/text-field-border");
+    examples.set("focus-blur-events", "text-field/focus-blur-events");
 
     return examples;
 }
