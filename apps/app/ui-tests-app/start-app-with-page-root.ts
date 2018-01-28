2cddb6105368237b39cc58c89a69fd60ce9611de
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2018-01-28 15:51:12 +0200

    chore: update exmaples to use defualtPage property

--- apps/app/ui-tests-app/start-app-with-page-root.ts
+++ apps/app/ui-tests-app/start-app-with-page-root.ts
@@ -1,6 +1,6 @@
-﻿
+﻿require("~/ui-tests-app/app-base");
 import * as application from "tns-core-modules/application";
 
-console.log("####### ------ START APP MODULES USING PAGE AS ROOT ELEMENT ------ #######");
+console.log("####### ------ START APP MODULES USING PAGE AS A ROOT ELEMENT ------ #######");
 
 application.start({ moduleName: "ui-tests-app/main-page" });
