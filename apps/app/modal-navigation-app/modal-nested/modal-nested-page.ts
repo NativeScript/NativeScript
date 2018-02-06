3c91548cc2c3ab8b3ce49c3b3ee843abe58caef8
mdonev <manol.donev@gmail.com>
2018-02-06 19:03:13 +0200

    chore(apps): add modal-navigation-app for ui testing

---
+++ apps/app/modal-navigation-app/modal-nested/modal-nested-page.ts
@@ -0,0 +1,23 @@
+import { StackLayout } from "tns-core-modules/ui/layouts/stack-layout";
+import { NavigatedData } from "tns-core-modules/ui/page";
+import { View, EventData } from "tns-core-modules/ui/core/view";
+
+export function onNavigatingTo(args: NavigatedData) {
+    console.log("NESTED modal-frame onNavigatingTo");
+}
+
+export function onNavigatingFrom(args: NavigatedData) {
+    console.log("NESTED modal-frame onNavigatingFrom");
+}
+
+export function onNavigatedTo(args: NavigatedData) {
+    console.log("NESTED modal-frame onNavigatedTo");
+}
+
+export function onNavigatedFrom(args: NavigatedData) {
+    console.log("NESTED modal-frame onNavigatedFrom");
+}
+
+export function closeModal(args: EventData) {
+    (args.object as View).closeModal();
+}
