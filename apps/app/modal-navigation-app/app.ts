5c9547916fe5f3a759583a752ebb8ea5ffea7a9f
vchimev <Vasil.Chimev@gmail.com>
2018-02-12 17:10:10 +0200

    test(apps): remove modal-navigation-app
    The app has been moved to `e2e/modal-navigation`.

--- apps/app/modal-navigation-app/app.ts
+++
@@ -1,4 +0,0 @@
-import * as application from "tns-core-modules/application";
-
-application.run({ moduleName: "modal-navigation-app/app-root" });
-// application.run({ moduleName: "modal-navigation-app/tab-root"});
--- apps/app/modal-navigation-app/modal-tab/modal-tab-page.ts
+++
@@ -1,9 +0,0 @@
-import { ShownModallyData } from "tns-core-modules/ui/core/view";
-
-export function onShowingModally(args: ShownModallyData) {
-    console.log("modal tabview showingModally");
-}
-
-export function onShownModally(args: ShownModallyData) {
-    console.log("modal tabview shownModally");
-}
