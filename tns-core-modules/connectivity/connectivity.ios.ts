310edf70cf2e3d74c74a6e4001f188555e6782fb
Nikolay Tsonev <tsonevn@users.noreply.github.com>
2018-03-15 16:25:43 +0200

    add zonedCallback in connectivity module (#5531)

--- tns-core-modules/connectivity/connectivity.ios.ts
+++ tns-core-modules/connectivity/connectivity.ios.ts
@@ -72,7 +72,7 @@ let _connectionTypeChangedCallback: (newConnectionType: number) => void;
 export function startMonitoring(connectionTypeChangedCallback: (newConnectionType: number) => void): void {
     if (!_monitorReachabilityRef) {
         _monitorReachabilityRef = _createReachability();
-        _connectionTypeChangedCallback = connectionTypeChangedCallback;
+        _connectionTypeChangedCallback = <any>zonedCallback(connectionTypeChangedCallback);
         SCNetworkReachabilitySetCallback(_monitorReachabilityRef, _reachabilityCallbackFunctionRef, null);
         SCNetworkReachabilityScheduleWithRunLoop(_monitorReachabilityRef, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
     }
