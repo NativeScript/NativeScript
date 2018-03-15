9fddec517a09f455547e21b615603924ef1fef9e
Noumaan Shah <noumaans@users.noreply.github.com>
2018-03-15 11:47:08 -0700

    Update enums.d.ts (#5530)
    I believe aspectFill vs aspectFit definitions are reversed.

--- tns-core-modules/ui/enums/enums.d.ts
+++ tns-core-modules/ui/enums/enums.d.ts
@@ -192,13 +192,14 @@ export module Stretch {
 
     /**
      * The image is resized to fill in the destination dimensions while it preserves its native aspect ratio.
+     * If the aspect ratio of the destination rectangle differs from the image, the image is clipped to fill
+     * in the destination.
      */
     export var aspectFill: string;
 
     /**
      * The image is resized to fit the destination dimensions while it preserves
-     * its native aspect ratio. If the aspect ratio of the destination rectangle differs from the image,
-     * the image is clipped to fit in the destination
+     * its native aspect ratio.
      */
     export var aspectFit: string;
 
