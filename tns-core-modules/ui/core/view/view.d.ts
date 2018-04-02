3d936fb150a44af6f0a14698cc61924425ca733c
Martin Yankov <m.i.yankov@gmail.com>
2018-04-02 13:44:06 +0300

    chore(view): expose onBackPressed in d.ts

--- tns-core-modules/ui/core/view/view.d.ts
+++ tns-core-modules/ui/core/view/view.d.ts
@@ -575,6 +575,11 @@ export abstract class View extends ViewBase {
     public getActualSize(): Size;
 
     /**
+     * Derived classes can override this method to handle Android back button press. 
+     */
+    onBackPressed(): boolean;
+
+    /**
      * @private
      * A valid css string which will be applied for all nested UI components (based on css rules).
      */
@@ -678,11 +683,6 @@ export abstract class View extends ViewBase {
      * @private
      */
     _onLivesync(): boolean;
-
-    /**
-     * Derived classes can override this method to handle Android back button press. 
-     */
-    onBackPressed(): boolean;
     /**
      * @private
      */
