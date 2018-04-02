e9f0feb99509c73b12d8199e9339b6b63c9b15ef
Svetoslav <tsenov.svetoslav@gmail.com>
2018-04-02 22:45:48 +0300

    refactor: update remove grid layout element

--- apps/app/ui-tests-app/issues/issue-3714.ts
+++ apps/app/ui-tests-app/issues/issue-3714.ts
@@ -5,19 +5,18 @@ import { Label } from "tns-core-modules/ui/label";
 import { StackLayout } from "tns-core-modules/ui/layouts/stack-layout";
 import { GridLayout } from "tns-core-modules/ui/layouts/grid-layout";
 
-const getElementById = (obj, id) => {
-    return obj.getViewById(id);
-}
-
 export function toggle(args: EventData) {
     const page = <Page>((<any>args.object).page);
-    const toggleBtn = <Button>getElementById(page, "toggleUserInteraction");
+    
+    const getElementById = id => { return page.getViewById(id); }
+    
+    const toggleBtn = <Button>getElementById("toggleUserInteraction");
     const isEnabled = toggleBtn.text === "disable" ? true : false;
     toggleBtn.text = !isEnabled ? "disable" : "enable";
-    const gridLayout = (<GridLayout>(getElementById(page, "layoutid")));
-    (<Button>getElementById(page, "testBtn")).isUserInteractionEnabled = !isEnabled;
-    (<Button>getElementById(page, "testLabel")).isUserInteractionEnabled = !isEnabled;
-    (<StackLayout>getElementById(page, "testStackLayout")).isUserInteractionEnabled = !isEnabled;
+
+    (<Button>getElementById("testBtn")).isUserInteractionEnabled = !isEnabled;
+    (<Button>getElementById("testLabel")).isUserInteractionEnabled = !isEnabled;
+    (<StackLayout>getElementById("testStackLayout")).isUserInteractionEnabled = !isEnabled;
 }
 
 export function test() {
