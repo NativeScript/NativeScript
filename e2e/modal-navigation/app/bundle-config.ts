3ecab4d1360bd7b8a0e6856ad990f0d88f839e90
Vasil Chimev <Vasil.Chimev@gmail.com>
2018-05-28 14:46:16 +0300

    refactor: remove bundle-config
    It is no more as `nativescript-dev-webpack@0.12.0` registers default application modules.

--- e2e/modal-navigation/app/bundle-config.ts
+++
@@ -1,7 +0,0 @@
-if ((<any>global).TNS_WEBPACK) {
-    require("globals");
-    // Register application modules
-    // This will register each `layout` postfixed xml, css, js, ts, scss file in the app/ folder
-    const context = (<any>require).context("~/", true, /(layout)\.(xml|css|js|ts|scss|less|sass)$/);
-    global.registerWebpackModules(context);
-}
