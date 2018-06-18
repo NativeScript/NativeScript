ec367cbcf4dfc40d4e9b6132e4beef0627361825
Manol Donev <manol.donev@gmail.com>
2018-06-18 19:11:51 +0300

    fix(ios): translate transform breaks sequential animation set

--- tns-core-modules/ui/animation/animation.ios.ts
+++ tns-core-modules/ui/animation/animation.ios.ts
@@ -79,8 +79,8 @@ class AnimationDelegateImpl extends NSObject implements CAAnimationDelegate {
                 targetStyle[setLocal ? rotateProperty.name : rotateProperty.keyframe] = value;
                 break;
             case Properties.translate:
-                targetStyle[setLocal ? translateXProperty.name : translateXProperty.keyframe] = value;
-                targetStyle[setLocal ? translateYProperty.name : translateYProperty.keyframe] = value;
+                targetStyle[setLocal ? translateXProperty.name : translateXProperty.keyframe] = value.x;
+                targetStyle[setLocal ? translateYProperty.name : translateYProperty.keyframe] = value.y;
                 break;
             case Properties.scale:
                 targetStyle[setLocal ? scaleXProperty.name : scaleXProperty.keyframe] = value.x === 0 ? 0.001 : value.x;
