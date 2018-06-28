43a172d32123529b7eab282c5ac8c0f7f2c9bedd
Vasil Trifonov <v.trifonov@gmail.com>
2018-06-28 15:59:18 +0300

    updated android typings

--- tns-platform-declarations/android/common.d.ts
+++ tns-platform-declarations/android/common.d.ts
@@ -1,5 +1,3 @@
-/// <reference path="./android-support-v4.d.ts" />
-/// <reference path="./android-support-v7-appcompat.d.ts" />
-/// <reference path="./android-support-design.d.ts" />
-/// <reference path="./declarations.d.ts" />
+/// <reference path="./android-declarations.d.ts" />
+/// <reference path="./android-support.d.ts" />
 /// <reference path="./org.nativescript.widgets.d.ts" />
