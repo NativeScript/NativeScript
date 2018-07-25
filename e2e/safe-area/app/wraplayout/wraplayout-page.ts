a9e37012bc9295da480cda77d88bcfb9c257f6f7
Vasil Chimev <Vasil.Chimev@gmail.com>
2018-07-25 13:57:19 +0300

    test(safe-area): add wrap layout examples

---
+++ e2e/safe-area/app/wraplayout/wraplayout-page.ts
@@ -0,0 +1,8 @@
+import { View, EventData } from "tns-core-modules/ui/core/view/view";
+
+export function onNavigate(args: EventData) {
+    const view = args.object as View;
+    const route = view["route"];
+
+    view.page.frame.navigate(route);
+}
