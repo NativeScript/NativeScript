2f8fa6edde855aa85658e4723948d1a7fc1ef811
Martin Guillon <martin.guillon@akylas.fr>
2018-07-30 11:28:06 +0200

    use “this.view”

--- tns-core-modules/ui/gestures/gestures.ios.ts
+++ tns-core-modules/ui/gestures/gestures.ios.ts
@@ -353,22 +353,22 @@ class TouchGestureRecognizer extends UIGestureRecognizer {
 
     touchesBeganWithEvent(touches: NSSet<any>, event: any): void {
         this.executeCallback(TouchAction.down, touches, event);
-        (this.observer.target.nativeView as UIView).touchesBeganWithEvent(touches, event);
+        this.view.touchesBeganWithEvent(touches, event);
     }
 
     touchesMovedWithEvent(touches: NSSet<any>, event: any): void {
         this.executeCallback(TouchAction.move, touches, event);
-        (this.observer.target.nativeView as UIView).touchesMovedWithEvent(touches, event);
+        this.view.touchesMovedWithEvent(touches, event);
     }
 
     touchesEndedWithEvent(touches: NSSet<any>, event: any): void {
         this.executeCallback(TouchAction.up, touches, event);
-        (this.observer.target.nativeView as UIView).touchesEndedWithEvent(touches, event);
+        this.view.touchesEndedWithEvent(touches, event);
     }
 
     touchesCancelledWithEvent(touches: NSSet<any>, event: any): void {
         this.executeCallback(TouchAction.cancel, touches, event);
-        (this.observer.target.nativeView as UIView).touchesCancelledWithEvent(touches, event);
+        this.view.touchesCancelledWithEvent(touches, event);
     }
 
     private executeCallback(action: string, touches: NSSet<any>, event: any): void {
