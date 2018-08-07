e3fed0061326ccfbd56b7f568d51d0a5fa33bcd1
vakrilov <alexander.vakrilov@telerik.com>
2018-08-07 13:17:37 +0300

    test: list items relayout example

--- apps/app/ui-tests-app/list-view/main-page.ts
+++ apps/app/ui-tests-app/list-view/main-page.ts
@@ -19,6 +19,7 @@ export function loadExamples() {
     examples.set("scrolling-and-sizing", "list-view/scrolling-and-sizing");
     examples.set("row-height", "list-view/row-height");
     examples.set("width-percent", "list-view/width-percent");
+    examples.set("item-re-layout", "list-view/item-re-layout");
 
     return examples;
 }
