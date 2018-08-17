45ad5c13c7ec7f917ae354fd4fd87c7b85bd8d0d
Vasil Chimev <Vasil.Chimev@gmail.com>
2018-08-17 14:09:36 +0300

    test(safe-area): add RadListView example

--- e2e/safe-area/app/radlistview/radlistview-page.ts
+++ e2e/safe-area/app/radlistview/radlistview-page.ts
@@ -1,8 +1,6 @@
-import { Page } from "tns-core-modules/ui/page";
-
 import { ViewModel } from "~/listview/fragments/listview-fragment-view-model";
 
-export function onLoaded(args) {
-    const page = <Page>args.object;
+export function onPageLoaded(args) {
+    const page = args.object;
     page.bindingContext = new ViewModel();
 }
