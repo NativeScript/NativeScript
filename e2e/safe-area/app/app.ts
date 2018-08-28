bf070f105d3922bfa2bb231845bc3cdd991dfc9b
Vasil Chimev <Vasil.Chimev@gmail.com>
2018-08-28 15:31:20 +0300

    refactor: update safe area traces

--- e2e/safe-area/app/app.ts
+++ e2e/safe-area/app/app.ts
@@ -1,3 +1,7 @@
 import * as application from "tns-core-modules/application";
+import * as trace from "tns-core-modules/trace";
+
+trace.setCategories(trace.categories.Layout);
+trace.enable();
 
 application.run({ moduleName: "app-root" });
