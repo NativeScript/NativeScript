e53bdd4f473b6c0b9dcf9b1838dc8a840cc9f09f
Svetoslav <tsenov.svetoslav@gmail.com>
2018-09-16 19:41:30 +0300

    tests: include e2e test for "Android Back Btn Events" (#6269)
    * tests: include e2e test for "Android Back Btn Events"

--- e2e/modal-navigation/e2e/modal-frame.e2e-spec.ts
+++ e2e/modal-navigation/e2e/modal-frame.e2e-spec.ts
@@ -33,8 +33,7 @@ describe("modal-frame:", () => {
 
             afterEach(async function () {
                 if (this.currentTest.state === "failed") {
-                    await driver.logPageSource(this.currentTest.title);
-                    await driver.logScreenshot(this.currentTest.title);
+                    await driver.logTestArtifacts(this.currentTest.title);
                     await driver.resetApp();
                     await screen[root]();
                 }
