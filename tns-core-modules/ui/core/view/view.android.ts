405ccae80c232e1e0a715990a6ac07b2760af69b
Manol Donev <manoldonev@users.noreply.github.com>
2018-09-18 11:25:31 +0300

    fix(android): parallel navigations should not be triggered (#6275)

--- tns-core-modules/ui/core/view/view.android.ts
+++ tns-core-modules/ui/core/view/view.android.ts
@@ -497,7 +497,7 @@ export class View extends ViewCommon {
 
     public getLocationRelativeTo(otherView: ViewCommon): Point {
         if (!this.nativeViewProtected || !this.nativeViewProtected.getWindowToken() ||
-            !otherView.nativeViewProtected || !otherView.nativeViewProtected.getWindowToken() ||
+            !otherView || !otherView.nativeViewProtected || !otherView.nativeViewProtected.getWindowToken() ||
             this.nativeViewProtected.getWindowToken() !== otherView.nativeViewProtected.getWindowToken()) {
             return undefined;
         }
