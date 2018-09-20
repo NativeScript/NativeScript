3ed3e4356432f0a8fb6a3e06402c1fc35043654d
vakrilov <alexander.vakrilov@telerik.com>
2018-09-20 17:29:19 +0300

    test:3d-animations test page added

---
+++ e2e/animation/app/3d-rotate/page.ts
@@ -0,0 +1,44 @@
+import { EventData, Page } from "tns-core-modules/ui/page";
+import { View } from "tns-core-modules/ui/core/view";
+
+let view: View;
+
+export function pageLoaded(args: EventData) {
+    const page = <Page>args.object;
+    view = page.getViewById<View>("view");
+}
+
+export function onAnimateX(args: EventData) {
+    view.animate({
+        rotate: { x: 360, y: 0, z: 0 },
+        duration: 3000
+    });
+}
+
+export function onAnimateY(args: EventData) {
+    view.animate({
+        rotate: { x: 0, y: 360, z: 0 },
+        duration: 3000
+    });
+}
+
+export function onAnimateZ(args: EventData) {
+    view.animate({
+        rotate: { x: 0, y: 0, z: 360 },
+        duration: 3000
+    });
+}
+
+export function onAnimateXYZ(args: EventData) {
+    view.animate({
+        rotate: { x: 360, y: 360, z: 360 },
+        duration: 3000
+    });
+}
+
+
+export function onReset(args: EventData) {
+    view.rotate = 0;
+    view.rotateX = 0;
+    view.rotateY = 0;
+}
