cf59ca9d7ea21904dd4fbb9a2d03374fe63e05f1
Manol Donev <manoldonev@users.noreply.github.com>
2018-11-02 21:08:17 +0200

    remove double comment

--- tns-core-modules/ui/frame/frame.android.ts
+++ tns-core-modules/ui/frame/frame.android.ts
@@ -859,11 +859,9 @@ class FragmentCallbacksImplementation implements AndroidFragmentCallbacks {
         if (traceEnabled()) {
             traceWrite(`${fragment}.onDestroy()`, traceCategories.NativeLifecycle);
         }
-        
+
         superFunc.call(fragment);
 
-        // fixes 'java.lang.IllegalStateException: The specified child already has a parent. You must call removeView() on the child's parent first'.	
-        // on app resume in nested frame scenarios with support library version greater than 26.0.0	
         const entry = this.entry;
         if (!entry) {
             traceError(`${fragment}.onDestroy: entry is null or undefined`);
