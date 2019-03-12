35c224e648246f58ae946fd31b3eadbed8648f95
Manol Donev <manol.donev@gmail.com>
2019-03-12 14:19:14 +0200

    fix unwrap

--- tns-core-modules/data/observable/observable.ts
+++ tns-core-modules/data/observable/observable.ts
@@ -13,7 +13,7 @@ export class WrappedValue implements WrappedValueDefinition {
     }
 
     public static unwrap(value: any) {
-        return (value && "wrapped" in value) ? value.wrapped : value;
+        return (value instanceof WrappedValue) ? value.wrapped : value;
     }
 
     public static wrap(value: any) {
