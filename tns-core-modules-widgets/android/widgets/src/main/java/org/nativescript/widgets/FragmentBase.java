b0f53caa16960c594e1f5a01cd3e34179500c3ac
ADjenkov <a.djenkov90@gmail.com>
2019-03-21 10:04:59 +0200

    feat(tns-core-modules-widgets): migrate to AndroidX namespaces

--- tns-core-modules-widgets/android/widgets/src/main/java/org/nativescript/widgets/FragmentBase.java
+++ tns-core-modules-widgets/android/widgets/src/main/java/org/nativescript/widgets/FragmentBase.java
@@ -1,7 +1,7 @@
 package org.nativescript.widgets;
 
 import android.animation.Animator;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment; 
 
 public abstract class FragmentBase extends Fragment {
 
