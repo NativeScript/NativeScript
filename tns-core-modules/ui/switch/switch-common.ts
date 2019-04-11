8aa74240749417127bbcfda3dd0804a435cf2ae6
Martin Yankov <m.i.yankov@gmail.com>
2019-04-11 18:45:14 +0300

    feat(ios-switch): add property for off state background color

--- tns-core-modules/ui/switch/switch-common.ts
+++ tns-core-modules/ui/switch/switch-common.ts
@@ -1,14 +1,27 @@
 ﻿import { Switch as SwitchDefinition } from ".";
 import { View, Property, booleanConverter, CSSType } from "../core/view";
+import { Color } from "../../color";
 
 export * from "../core/view";
 
 @CSSType("Switch")
 export class SwitchBase extends View implements SwitchDefinition {
     public checked: boolean;
+    public offBackgroundColor: Color;
+
+    _onCheckedPropertyChanged(newValue: boolean) {
+        //
+    }
 }
 
 SwitchBase.prototype.recycleNativeView = "auto";
 
-export const checkedProperty = new Property<SwitchBase, boolean>({ name: "checked", defaultValue: false, valueConverter: booleanConverter });
+function onCheckedPropertyChanged(switchBase: SwitchBase, oldValue: boolean, newValue: boolean) {
+    switchBase._onCheckedPropertyChanged(newValue);
+}
+
+export const checkedProperty = new Property<SwitchBase, boolean>({ name: "checked", defaultValue: false, valueConverter: booleanConverter, valueChanged: onCheckedPropertyChanged });
 checkedProperty.register(SwitchBase);
+
+export const offBackgroundColorProperty = new Property<SwitchBase, Color>({ name: "offBackgroundColor", equalityComparer: Color.equals, valueConverter: (v) => new Color(v) });
+offBackgroundColorProperty.register(SwitchBase);
