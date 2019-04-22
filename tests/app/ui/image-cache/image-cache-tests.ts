60bfe3bd9c18403b93f7df83c16d0a32503cbb1e
lukecurran <luke@pepper.me>
2019-04-22 10:56:14 -0500

    Image cache module fix

--- tests/app/ui/image-cache/image-cache-tests.ts
+++ tests/app/ui/image-cache/image-cache-tests.ts
@@ -1,25 +1,23 @@
 ﻿// >> image-cache-require
 import * as imageCacheModule from "tns-core-modules/ui/image-cache";
 import * as imageSource from "tns-core-modules/image-source";
-import * as fs from "tns-core-modules/file-system";
 // << image-cache-require
 
-export function test_DummyTestForSnippetOnly() {
+export let test_DummyTestForSnippetOnly = function() {
     // >> image-cache-request-images
-    var cache = new imageCacheModule.Cache();
-    cache.placeholder = imageSource.fromFile(fs.path.join(__dirname, "res/no-image.png"));
+    const cache = new imageCacheModule.Cache();
     cache.maxRequests = 5;
-    
+
     // Enable download while not scrolling
     cache.enableDownload();
-    
-    var imgSouce: imageSource.ImageSource;
-    var url = "https://github.com/NativeScript.png";
+
+    let imgSource: imageSource.ImageSource;
+    const url = "https://github.com/NativeScript.png";
     // Try to read the image from the cache
-    var image = cache.get(url);
+    const image = cache.get(url);
     if (image) {
         // If present -- use it.
-        imgSouce = imageSource.fromNativeSource(image);
+        imgSource = imageSource.fromNativeSource(image);
     }
     else {
         // If not present -- request its download.
@@ -28,7 +26,7 @@ export function test_DummyTestForSnippetOnly() {
             url: url,
             completed: (image: any, key: string) => {
                 if (url === key) {
-                    imgSouce = imageSource.fromNativeSource(image);
+                    imgSource = imageSource.fromNativeSource(image);
                 }
             }
         });
@@ -37,4 +35,39 @@ export function test_DummyTestForSnippetOnly() {
     // Disable download while scrolling
     cache.disableDownload();
     // << image-cache-request-images
+}
+
+export let test_ImageCache_NothingAtProvidedUrl = function() {
+    const cache = new imageCacheModule.Cache();
+    cache.maxRequests = 5;
+
+    // Enable download while not scrolling
+    cache.enableDownload();
+
+    let imgSource: imageSource.ImageSource;
+    const url = "https://github.com/NativeScript-NoImage.png";
+    // Try to read the image from the cache
+    const image = cache.get(url);
+    if (image) {
+        // If present -- use it.
+        imgSource = imageSource.fromNativeSource(image);
+    }
+    else {
+        // If not present -- request its download.
+        cache.push({
+            key: url,
+            url: url,
+            completed: (image: any, key: string) => {
+                if (url === key) {
+                    imgSource = imageSource.fromNativeSource(image);
+                }
+            },
+            error: (key: string) => {
+                console.log("No image for key: ", key);
+            }
+        });
+    }
+
+    // Disable download while scrolling
+    cache.disableDownload();
 }
