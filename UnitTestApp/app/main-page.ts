9bd86a6f061363fc3b7cfcef79b60f66c556a0d1
Vasil Chimev <vasil.chimev@gmail.com>
2019-05-27 14:16:27 +0300

    refactor: main-page module

--- UnitTestApp/app/main-page.ts
+++ UnitTestApp/app/main-page.ts
@@ -1,31 +1,30 @@
-/*
-In NativeScript, a file with the same name as an XML file is known as
-a code-behind file. The code-behind is a great place to place your view
-logic, and to set up your page’s data binding.
-*/
-
-import { EventData } from "tns-core-modules/data/observable";
 import { Page } from "tns-core-modules/ui/page";
-import { HelloWorldModel } from "./main-view-model";
+import * as trace from "tns-core-modules/trace";
+// import * as tests from "../testRunner";
 
-// Event handler for Page "navigatingTo" event attached in main-page.xml
-export function navigatingTo(args: EventData) {
-    /*
-    This gets a reference this page’s <Page> UI component. You can
-    view the API reference of the Page to see what’s available at
-    https://docs.nativescript.org/api-reference/classes/_ui_page_.page.html
-    */
-    const page = <Page>args.object;
+let executeTests = true;
 
-    /*
-    A page’s bindingContext is an object that should be used to perform
-    data binding between XML markup and TypeScript code. Properties
-    on the bindingContext can be accessed using the {{ }} syntax in XML.
-    In this example, the {{ message }} and {{ onTap }} bindings are resolved
-    against the object returned by createViewModel().
+trace.enable();
+trace.addCategories(trace.categories.Test + "," + trace.categories.Error);
 
-    You can learn more about data binding in NativeScript at
-    https://docs.nativescript.org/core-concepts/data-binding.
-    */
-    page.bindingContext = new HelloWorldModel();
+// When debugging
+// trace.setCategories(trace.categories.concat(
+//    trace.categories.Test,
+//    trace.categories.Navigation,
+//    trace.categories.Transition,
+//    trace.categories.NativeLifecycle,
+//    trace.categories.ViewHierarchy,
+//    trace.categories.VisualTreeEvents
+// ));
+
+function runTests() {
+    // setTimeout(() => tests.runAll(""), 10);
+}
+
+export function onNavigatedTo(args) {
+    args.object.off(Page.loadedEvent, onNavigatedTo);
+    if (executeTests) {
+        executeTests = false;
+        runTests();
+    }
 }
