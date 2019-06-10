2469e6f527b71c96738366972dbe270f4327592f
Vasil Chimev <vasil.chimev@gmail.com>
2019-06-10 17:56:52 +0300

    ui-tests-app: enable examples

--- apps/app/ui-tests-app/layouts/main-page.ts
+++ apps/app/ui-tests-app/layouts/main-page.ts
@@ -10,20 +10,20 @@ export function pageLoaded(args: EventData) {
 }
 
 export function loadExamples() {
-    const examples = new Map<string, string>();    
-    examples.set("absolute", "layouts/absolute");
-    examples.set("dock", "layouts/dock");
-    examples.set("grid", "layouts/grid");
-    examples.set("stack", "layouts/stack");
-    examples.set("wrap", "layouts/wrap");
-    examples.set("pabsolute", "layouts-percent/absolute");
-    examples.set("pdock", "layouts-percent/dock");
-    examples.set("pgrid", "layouts-percent/grid");
-    examples.set("pstack", "layouts-percent/stack");
-    examples.set("pwrap", "layouts-percent/wrap");
-    examples.set("passThroughParent", "layouts/passThroughParent");
-    examples.set("stacklayout-6059", "layouts/stacklayout-6059");
-    examples.set("safe-area", "layouts/safe-area");
+    const examples = new Map<string, string>();
+    examples.set("absolute", "layouts/absolute-page");
+    examples.set("dock", "layouts/dock-page");
+    examples.set("grid", "layouts/grid-page");
+    examples.set("stack", "layouts/stack-page");
+    examples.set("wrap", "layouts/wrap-page");
+    examples.set("pabsolute", "layouts-percent/absolute-page");
+    examples.set("pdock", "layouts-percent/dock-page");
+    examples.set("pgrid", "layouts-percent/grid-page");
+    examples.set("pstack", "layouts-percent/stack-page");
+    examples.set("pwrap", "layouts-percent/wrap-page");
+    examples.set("passThroughParent", "layouts/passThroughParent-page");
+    examples.set("stacklayout-6059", "layouts/stacklayout-6059-page");
+    examples.set("safe-area", "layouts/safe-area-page");
 
     return examples;
 }
