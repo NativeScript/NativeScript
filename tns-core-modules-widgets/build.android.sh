957da6cddc351d6106370ad2ff1185ab1d0e901a
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2019-06-14 19:37:21 +0300

    update build scripts

--- tns-core-modules-widgets/build.android.sh
+++ tns-core-modules-widgets/build.android.sh
@@ -18,17 +18,22 @@ cd android
 cd ..
 cp android/widgets/build/outputs/aar/widgets-release.aar dist/package/platforms/android/widgets-release.aar
 
-echo "Copy NPM artefacts"
-cp .npmignore LICENSE README.md package.json dist/package
-if [ "$1" ]
+if [ "$PACKAGE_VERSION" ]
 then
-  echo "Suffix package.json's version with tag: $1"
-  sed -i.bak 's/\(\"version\"\:[[:space:]]*\"[^\"]*\)\"/\1-'$1'"/g' ./dist/package/package.json
+  echo "Suffix package.json's version with tag: $PACKAGE_VERSION"
+  sed -i.bak 's/\(\"version\"\:[[:space:]]*\"[^\"]*\)\"/\1-'$PACKAGE_VERSION'"/g' ./dist/package/package.json
 fi
 
-echo "NPM pack"
-cd dist/package
-PACKAGE="$(npm pack)"
-cd ../..
-mv dist/package/$PACKAGE dist/$PACKAGE
-echo "Output: dist/$PACKAGE"
+if [ "$SKIP_PACK" ]
+then
+  echo "SKIP pack" 
+else
+  echo "Copy NPM artefacts"
+  cp .npmignore LICENSE README.md package.json dist/package
+  echo "NPM pack"
+  cd dist/package
+  PACKAGE="$(npm pack)"
+  cd ../..
+  mv dist/package/$PACKAGE dist/$PACKAGE
+  echo "Output: dist/$PACKAGE"
+fi
--- tns-core-modules-widgets/build.ios.sh
+++ tns-core-modules-widgets/build.ios.sh
@@ -3,6 +3,22 @@
 echo "Set exit on simple errors"
 set -e
 
+# for i in "$@"
+# do
+# case $i in
+#     --package_version=*)
+#     PACKAGE_VERSION="${i#*=}"
+#     shift # past argument=value
+#     ;;
+#     --skip-pack=*)
+#     SKIP_PACK="true"
+#     shift # past argument=value
+#     ;;
+# esac
+# done
+# echo "PACKAGE_VERSION  = ${PACKAGE_VERSION}"
+# echo "PUBLISH = ${PUBLISH}"
+
 echo "Use dumb terminal"
 export TERM=dumb
 
@@ -22,18 +38,22 @@ echo "Copy ios/TNSWidgets/build/*.framework dist/package/platforms/ios"
 cp -R ios/TNSWidgets/build/*.framework dist/package/platforms/ios
 cp ios/TNSWidgets/build/*.framework.dSYM.zip dist/package/platforms/ios
 
-echo "Copy NPM artefacts"
-cp .npmignore LICENSE README.md package.json dist/package
-if [ "$1" ]
+if [ "$PACKAGE_VERSION" ]
 then
-  echo "Suffix package.json's version with tag: $1"
-  sed -i.bak 's/\(\"version\"\:[[:space:]]*\"[^\"]*\)\"/\1-'$1'"/g' ./dist/package/package.json
+  echo "Suffix package.json's version with tag: $PACKAGE_VERSION"
+  sed -i.bak 's/\(\"version\"\:[[:space:]]*\"[^\"]*\)\"/\1-'$PACKAGE_VERSION'"/g' ./dist/package/package.json
 fi
 
-echo "NPM pack"
-cd dist/package
-PACKAGE="$(npm pack)"
-cd ../..
-mv dist/package/$PACKAGE dist/$PACKAGE
-echo "Output: dist/$PACKAGE"
-
+if [ "$SKIP_PACK" ]
+then
+  echo "SKIP pack" 
+else
+  echo "Copy NPM artefacts"
+  cp .npmignore LICENSE README.md package.json dist/package
+  echo "NPM pack"
+  cd dist/package
+  PACKAGE="$(npm pack)"
+  cd ../..
+  mv dist/package/$PACKAGE dist/$PACKAGE
+  echo "Output: dist/$PACKAGE"
+fi
