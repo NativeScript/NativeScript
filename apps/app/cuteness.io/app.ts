83e48b397d46aaec675a5ef7bc9557b6a3dc6ded
Vasil Chimev <vasil.chimev@gmail.com>
2019-06-21 15:36:51 +0300

    refactor(apps): delete cuteness.io from apps
    Moved to `e2e` folder.

--- apps/app/cuteness.io/app.ts
+++
@@ -1,6 +0,0 @@
-﻿import * as application from "tns-core-modules/application";
-
-// Start the application
-global.registerModule("cuteness.io/app.css", () => require("~/cuteness.io/app.css"));
-application.setCssFileName("cuteness.io/app.css");
-application.run({ moduleName: "cuteness.io/app-root" });
