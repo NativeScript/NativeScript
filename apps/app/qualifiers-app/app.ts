c45f8ada5cb344b1ab98b63810ed9f7f12a42cb9
vakrilov <alexander.vakrilov@telerik.com>
2019-06-26 14:56:19 +0300

    chore: move qualifiers app to e2e

--- apps/app/qualifiers-app/app.ts
+++
@@ -1,5 +0,0 @@
-import * as application from "tns-core-modules/application";
-
-application.setCssFileName("qualifiers-app/app-root.css");
-
-application.run({ moduleName: "qualifiers-app/app-root" });
