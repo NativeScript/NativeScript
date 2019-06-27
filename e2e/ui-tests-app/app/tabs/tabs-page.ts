9fab8f6a560f0a25d72878887991ab519335a0fd
Manol Donev <manol.donev@gmail.com>
2019-06-27 15:53:30 +0300

    chore: update bottomnav & tabs examples

--- e2e/ui-tests-app/app/tabs/tabs-page.ts
+++ e2e/ui-tests-app/app/tabs/tabs-page.ts
@@ -1,19 +1,17 @@
 import { EventData } from "tns-core-modules/data/observable";
-import { SubMainPageViewModel } from "../sub-main-page-view-model";
-import { WrapLayout } from "tns-core-modules/ui/layouts/wrap-layout";
 import { Page } from "tns-core-modules/ui/page";
-import { BottomNavigation } from "tns-core-modules/ui/bottom-navigation";
+import { Tabs } from "tns-core-modules/ui/tabs";
 
 export function goToFirst(args: EventData) {
     const page = <Page>(<any>args.object).page;
-    const bottomNav = <BottomNavigation>page.getViewById("bottomNav");
+    const tabsNav = <Tabs>page.getViewById("tabsNav");
 
-    bottomNav.selectedIndex = 0;
+    tabsNav.selectedIndex = 0;
 }
 
 export function goToSecond(args: EventData) {
   const page = <Page>(<any>args.object).page;
-  const bottomNav = <BottomNavigation>page.getViewById("bottomNav");
+  const tabsNav = <Tabs>page.getViewById("tabsNav");
 
-  bottomNav.selectedIndex = 1;
+  tabsNav.selectedIndex = 1;
 }
