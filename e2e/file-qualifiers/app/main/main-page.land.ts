6c435a7bc85180e3f045df7d74f4a53f9f460b9d
vakrilov <alexander.vakrilov@telerik.com>
2019-07-02 15:20:01 +0300

    chore: update project

--- e2e/file-qualifiers/app/main/main-page.land.ts
+++ e2e/file-qualifiers/app/main/main-page.land.ts
@@ -1,7 +1,15 @@
+import { topmost } from "tns-core-modules/ui/frame";
+
 export function onNavigatedTo(args) {
     console.log("---> [LANDSCAPE] onNavigatedTo");
 }
 
+export function tap(args) {
+    console.log("---> [DEFAULT] tap");
+}
+
 export function navigate(args) {
-    console.log("---> [LANDSCAPE] navigate");
+    console.log("---> [DEFAULT] navigate");
+
+    topmost().navigate("other/other-page");
 }
--- e2e/file-qualifiers/app/main/main-page.ts
+++ e2e/file-qualifiers/app/main/main-page.ts
@@ -1,7 +1,15 @@
+import { topmost } from "tns-core-modules/ui/frame";
+
 export function onNavigatedTo(args) {
     console.log("---> [DEFAULT] onNavigatedTo");
 }
 
+export function tap(args) {
+    console.log("---> [DEFAULT] tap");
+}
+
 export function navigate(args) {
     console.log("---> [DEFAULT] navigate");
+
+    topmost().navigate("other/other-page");
 }
