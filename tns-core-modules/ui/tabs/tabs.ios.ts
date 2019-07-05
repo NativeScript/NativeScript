fd1983f45e536c185ac5eb3801a106ba7c65b65b
Vasil Chimev <vasil.chimev@gmail.com>
2019-07-05 11:32:55 +0300

    feat(ios-tabs): tab bar item appearance
    Implements https://github.com/NativeScript/NativeScript/issues/7436 and https://github.com/NativeScript/nativescript-angular/issues/1884.

--- tns-core-modules/ui/tabs/tabs.ios.ts
+++ tns-core-modules/ui/tabs/tabs.ios.ts
@@ -557,6 +557,8 @@ export class Tabs extends TabsBase {
     private _delegate: UIPageViewControllerDelegateImpl;
     // private _moreNavigationControllerDelegate: UINavigationControllerDelegateImpl;
     private _iconsCache = {};
+    private _hasImage;
+    private _hasTitle;
 
     constructor() {
         super();
