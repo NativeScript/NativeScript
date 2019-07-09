8943226765c9de8be388b13daa639f35788bfd19
Martin Yankov <m.i.yankov@gmail.com>
2019-07-09 11:39:45 +0300

    refactor: move material typings to core modules

--- tns-core-modules/references.d.ts
+++ tns-core-modules/references.d.ts
@@ -1,2 +1,3 @@
 /// <reference path="../tns-platform-declarations/ios.d.ts" />
 /// <reference path="../tns-platform-declarations/android.d.ts" />
+/// <reference path="platforms/ios/typings/objc!MaterialComponents.d.ts" />
