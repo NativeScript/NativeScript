9b7c7c0d20d2ef3bf6a1ea8b2a32b99cf99fe6b4
vakrilov <alexander.vakrilov@telerik.com>
2019-07-11 12:43:09 +0300

    chore: remove unneeded console registration

--- tns-core-modules/globals/globals.ts
+++ tns-core-modules/globals/globals.ts
@@ -6,10 +6,3 @@ import "./polyfills/xhr";
 import "./polyfills/fetch";
 
 import "./decorators";
-
-// This is probably not needed any more
-if ((<any>global).__snapshot || (<any>global).__snapshotEnabled) {
-    // Object.assign call will fire an error when trying to write to a read-only property of an object, such as 'console'
-    const consoleModule = require("console").Console;
-    global.console = global.console || new consoleModule();
-}
