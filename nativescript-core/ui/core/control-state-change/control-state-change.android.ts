4437b082624dfe41e6fe8303f0028e6ea1a45b5f
vakrilov <alexander.vakrilov@telerik.com>
2019-08-30 16:42:07 +0300

    chore: add anroid module for consistency

---
+++ nativescript-core/ui/core/control-state-change/control-state-change.android.ts
@@ -0,0 +1,10 @@
+﻿/* tslint:disable:no-unused-variable */
+import { ControlStateChangeListener as ControlStateChangeListenerDefinition } from "./control-state-change";
+
+export class ControlStateChangeListener implements ControlStateChangeListenerDefinition {
+    constructor(control: any /* UIControl */, callback: (state: string) => void) {
+        throw new Error("ControlStateChangeListener is not implemented for Android");
+    }
+    public start() { }
+    public stop() { }
+}
