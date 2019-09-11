f15d594c1826f2514298b86c265f11650c9721e4
Vasil Chimev <vasil.chimev@gmail.com>
2019-09-11 18:21:08 +0300

    fix(dark-mode): tslint errors

--- tns-core-modules/color/compatibility-colors.ios.ts
+++ tns-core-modules/color/compatibility-colors.ios.ts
@@ -2,11 +2,11 @@ import { ios } from "../utils/utils";
 
 const majorVersion = ios.MajorVersion;
 
-export const activityIndicatorViewStyle = () => { return majorVersion <= 12 ? UIActivityIndicatorViewStyle.Gray : UIActivityIndicatorViewStyle.Medium };
+export const activityIndicatorViewStyle = () => majorVersion <= 12 ? UIActivityIndicatorViewStyle.Gray : UIActivityIndicatorViewStyle.Medium;
 
 // UI Element Colors
-export const backgroundColor = () => { return majorVersion <= 12 ? UIColor.whiteColor : UIColor.systemBackgroundColor };
-export const labelColor = () => { return majorVersion <= 12 ? UIColor.blackColor : UIColor.labelColor };
+export const backgroundColor = () => majorVersion <= 12 ? UIColor.whiteColor : UIColor.systemBackgroundColor;
+export const labelColor = () => majorVersion <= 12 ? UIColor.blackColor : UIColor.labelColor;
 
 // Standard Colors
-export const blueColor = () => { return majorVersion <= 12 ? UIColor.blueColor : UIColor.systemBlueColor };
+export const blueColor = () => majorVersion <= 12 ? UIColor.blueColor : UIColor.systemBlueColor;
