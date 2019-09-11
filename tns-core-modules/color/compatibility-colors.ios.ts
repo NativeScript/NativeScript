47df0a623c8c57a657acf7b588680ab536dc3fe3
Vasil Chimev <vasil.chimev@gmail.com>
2019-09-11 17:06:12 +0300

    fix(dark-mode): tabs colors

--- tns-core-modules/color/compatibility-colors.ios.ts
+++ tns-core-modules/color/compatibility-colors.ios.ts
@@ -3,4 +3,10 @@ import { ios } from "../utils/utils";
 const majorVersion = ios.MajorVersion;
 
 export const activityIndicatorViewStyle = () => { return majorVersion <= 12 ? UIActivityIndicatorViewStyle.Gray : UIActivityIndicatorViewStyle.Medium };
+
+// UI Element Colors
 export const backgroundColor = () => { return majorVersion <= 12 ? UIColor.whiteColor : UIColor.systemBackgroundColor };
+export const labelColor = () => { return majorVersion <= 12 ? UIColor.blackColor : UIColor.labelColor };
+
+// Standard Colors
+export const blueColor = () => { return majorVersion <= 12 ? UIColor.blueColor : UIColor.systemBlueColor };
