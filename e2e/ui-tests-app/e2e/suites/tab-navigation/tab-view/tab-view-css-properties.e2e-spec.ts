c2579a5298fd3a51542414f91e5710050209a3cf
Manol Donev <manol.donev@gmail.com>
2019-10-09 18:29:21 +0300

    fix test

--- e2e/ui-tests-app/e2e/suites/tab-navigation/tab-view/tab-view-css-properties.e2e-spec.ts
+++ e2e/ui-tests-app/e2e/suites/tab-navigation/tab-view/tab-view-css-properties.e2e-spec.ts
@@ -59,11 +59,11 @@ describe(`${suite}-${spec}-suite`, async function () {
             if (driver.isIOS && imageName.includes("android")) {
                 this.skip();
             }
-            if (driver.platformName === Platform.ANDROID
-                && (sample.sample.toLowerCase() === "all" || sample.sample.toLowerCase() === "reset")) {
+            let scenarioBtn = await driver.waitForElement(sample.sample);
+            if (!scenarioBtn) {
                 await driver.scroll(Direction.up, 400, 200, 300, 200);
+                scenarioBtn = await driver.waitForElement(sample.sample);
             }
-            const scenarioBtn = await driver.waitForElement(sample.sample);
             await scenarioBtn.click();
             imageName = setImageName(suite, spec, imageName);
             await driver.imageHelper.compareScreen({ imageName: imageName, timeOutSeconds: 5, tolerance: 0, toleranceType: ImageOptions.pixel });
