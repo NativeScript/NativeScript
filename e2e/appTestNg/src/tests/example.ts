d356d506b556da08e04bad58daa316b75b920e50
Vasil Chimev <vasil.chimev@gmail.com>
2019-11-14 05:04:50 +0200

    tns test init

---
+++ e2e/appTestNg/src/tests/example.ts
@@ -0,0 +1,9 @@
+// A sample Mocha test
+describe('Array', function () {
+	describe('#indexOf()', function () {
+		it('should return -1 when the value is not present', function () {
+			assert.equal(-1, [1, 2, 3].indexOf(5));
+			assert.equal(-1, [1, 2, 3].indexOf(0));
+		});
+	});
+});
