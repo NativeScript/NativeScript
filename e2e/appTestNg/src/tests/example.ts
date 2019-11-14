7b319626aa51f5189fca9ee8ec6a9914d33d2dd5
Vasil Chimev <vasil.chimev@gmail.com>
2019-11-14 10:07:14 +0200

    appTestNg

--- e2e/appTestNg/src/tests/example.ts
+++ e2e/appTestNg/src/tests/example.ts
@@ -1,9 +1,37 @@
-// A sample Mocha test
-describe('Array', function () {
-	describe('#indexOf()', function () {
-		it('should return -1 when the value is not present', function () {
-			assert.equal(-1, [1, 2, 3].indexOf(5));
-			assert.equal(-1, [1, 2, 3].indexOf(0));
+import { Component, ElementRef } from "@angular/core";
+import { nsTestBedAfterEach, nsTestBedBeforeEach, nsTestBedRender } from "nativescript-angular/testing";
+
+@Component({
+	template: `
+    <StackLayout>
+        <Label text="Label"></Label>
+    </StackLayout>`
+})
+export class StackLayoutComponent {
+	constructor(public elementRef: ElementRef) { }
+}
+
+console.log("---> test example");
+
+describe('Describe', function () {
+
+	console.log("---> test describe");
+
+	describe('StackLayoutComponent', function () {
+
+		this.beforeEach(nsTestBedBeforeEach(
+			[StackLayoutComponent]
+		))
+		this.afterEach(nsTestBedAfterEach());
+
+		it('Render', function () {
+
+			return nsTestBedRender(StackLayoutComponent).then((fixture) => {
+				const componentRef = fixture.componentRef;
+				const componentRoot = componentRef.instance.elementRef.nativeElement;
+				console.log("---> tests componentRef", componentRef);
+				console.log("---> tests componentRoot", componentRoot);
+			});
 		});
 	});
 });
---
+++ e2e/appTestNg/src/tests/test-main.ts
@@ -0,0 +1,4 @@
+import "nativescript-angular/zone-js/testing.mocha";
+import {nsTestBedInit} from "nativescript-angular/testing";
+console.log("---> test main");
+nsTestBedInit();
