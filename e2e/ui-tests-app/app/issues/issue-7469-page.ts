85920129b7c540903826704ab1b2c2fd93553c7d
Eduardo Speroni <edusperoni@gmail.com>
2019-12-11 17:36:54 -0300

    update example with more test cases

--- e2e/ui-tests-app/app/issues/issue-7469-page.ts
+++ e2e/ui-tests-app/app/issues/issue-7469-page.ts
@@ -3,6 +3,7 @@ import { Page, Color } from "tns-core-modules/ui/page";
 import { Button } from "tns-core-modules/ui/button";
 import { StackLayout } from "tns-core-modules/ui/layouts/stack-layout";
 import { removeCallback, start, stop, addCallback } from "tns-core-modules/fps-meter";
+import { Label } from "tns-core-modules/ui/label/label";
 
 let callbackId;
 let fpsLabel: any;
@@ -23,24 +24,10 @@ export function stopFPSMeter() {
     stop();
 }
 
-export function toggle(args: EventData) {
-    const page = <Page>((<any>args.object).page);
-
-    const getElementById = id => page.getViewById(id);
-
-    const toggleBtn = <Button>getElementById("toggleUserInteraction");
-    const isEnabled = toggleBtn.text === "disable" ? true : false;
-    toggleBtn.text = !isEnabled ? "disable" : "enable";
-
-    (<Button>getElementById("testBtn")).isUserInteractionEnabled = !isEnabled;
-    (<Button>getElementById("testLabel")).isUserInteractionEnabled = !isEnabled;
-    (<StackLayout>getElementById("testStackLayout")).isUserInteractionEnabled = !isEnabled;
-}
-
 let timeouts = [];
 let intervals = [];
 
-let label;
+let reusableItem;
 let loaded = false;
 let isIn1 = false;
 
@@ -51,10 +38,10 @@ export function pageLoaded(args) {
         // stopFPSMeter();
         timeouts.forEach((v) => clearTimeout(v));
         intervals.forEach((v) => clearInterval(v));
-        label._tearDownUI(true);
+        reusableItem._tearDownUI(true);
     }
     loaded = true;
-    label = args.object.getViewById("removelabel");
+    reusableItem = args.object.getViewById("reusableItem");
     fpsLabel = args.object.getViewById("fpslabel");
     const stack1: StackLayout = args.object.getViewById("stack1");
     const stack2: StackLayout = args.object.getViewById("stack2");
@@ -86,23 +73,55 @@ export function pageUnloaded(args) {
 }
 
 export function makeReusable(args: EventData) {
+    console.log("loaded:", args.object);
     // console.log("making reusable");
     (args.object as any).reusable = true;
 }
 
+export function onReusableUnloaded(args: EventData) {
+    console.log("unloaded:", args.object);
+}
+var testLabel: Label;
+
 export function test(args: any) {
     const page = args.object.page;
-    label = page.getViewById("removelabel");
+    reusableItem = page.getViewById("reusableItem");
     const stack1: StackLayout = page.getViewById("stack1");
     const stack2: StackLayout = page.getViewById("stack2");
-    label.parent.removeChild(label);
-    // console.log(label.nativeView);
+    if (!testLabel) {
+        testLabel = new Label();
+        testLabel.text = "This label is not reusable and is dynamic";
+        testLabel.on("loaded", () => { console.log("LODADED testLabel"); });
+        testLabel.on("unloaded", () => { console.log("UNLODADED testLabel"); });
+    }
+    reusableItem.parent.removeChild(reusableItem);
+    if (!reusableItem._suspendNativeUpdatesCount) {
+        console.log("reusableItem SHOULD BE UNLOADED");
+    }
+    if (!testLabel._suspendNativeUpdatesCount) {
+        console.log("testLabel SHOULD BE UNLOADED");
+    }
+    if (!testLabel.parent) {
+        reusableItem.addChild(testLabel);
+    }
+    if (!testLabel.nativeView) {
+        console.log("testLabel NATIVE VIEW SHOULD BE CREATED");
+    }
+    if (!testLabel._suspendNativeUpdatesCount) {
+        console.log("testLabel SHOULD BE UNLOADED");
+    }
     if (isIn1) {
         isIn1 = false;
-        stack2.addChild(label);
+        stack2.addChild(reusableItem);
     } else {
         isIn1 = true;
-        stack1.addChild(label);
+        stack1.addChild(reusableItem);
+    }
+    if (reusableItem._suspendNativeUpdatesCount) {
+        console.log("reusableItem SHOULD BE LOADED AND RECEIVING UPDATES");
+    }
+    if (testLabel._suspendNativeUpdatesCount) {
+        console.log("testLabel SHOULD BE LOADED AND RECEIVING UPDATES");
     }
     // console.log("onTap");
     // alert("onTap");
