77edeb9364ed9dc33fe37fc936094f4006a3073a
Kirill Goncharov <kdgoncharov@gmail.com>
2019-12-18 19:40:01 +0300

    feat(html-view): Additional properties for HtmlView component
    - Allow to set text color with `color` CSS property.
    - Allow to set link color with `link-color` CSS property.
    - Allow to set font attributes with `font-family` and `font-size` CSS properties.
    - Make text selectable on Android by default (for consistency with IOS).
    - Remove extra padding on IOS.

--- nativescript-core/ui/html-view/html-view-common.ts
+++ nativescript-core/ui/html-view/html-view-common.ts
@@ -1,5 +1,7 @@
-﻿import { HtmlView as HtmlViewDefinition } from ".";
+﻿import { Color } from "../../color";
+import { Style, CssProperty } from "../core/properties";
 import { View, Property, CSSType } from "../core/view";
+import { HtmlView as HtmlViewDefinition } from ".";
 
 export * from "../core/view";
 
@@ -13,3 +15,11 @@ HtmlViewBase.prototype.recycleNativeView = "auto";
 // TODO: Can we use Label.ios optimization for affectsLayout???
 export const htmlProperty = new Property<HtmlViewBase, string>({ name: "html", defaultValue: "", affectsLayout: true });
 htmlProperty.register(HtmlViewBase);
+
+export const linkColorProperty = new CssProperty<Style, Color>({
+    name: "linkColor",
+    cssName: "link-color",
+    equalityComparer: Color.equals,
+    valueConverter: (value) => new Color(value),
+});
+linkColorProperty.register(Style);
