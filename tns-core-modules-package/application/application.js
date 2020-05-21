aa646ed560ab81283af2efeb7ff0fbb0b469518c
Vasko <v.trifonov@gmail.com>
2020-05-21 21:11:55 +0300

    fix: '__exportStar undefined'

---
+++ tns-core-modules-package/application/application.js
@@ -0,0 +1,4 @@
+// use precompiled js file to make sure we require the ts-helpers to register __exportStar function
+Object.defineProperty(exports, "__esModule", { value: true });
+require("@nativescript/core/globals/ts-helpers");
+__exportStar(require("@nativescript/core/application/application"), exports);
