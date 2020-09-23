2cece368e7e047fc152f649eda9c210fe05beb12
Martin Guillon <martin.guillon@akylas.fr>
2020-09-23 21:11:40 +0200

    fix wrong import

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,6 +1,6 @@
 /// <reference path="./global-types.d.ts" />
-// Init globals first (use require to ensure it's always at the top)
-import '../globals';
+// Init globals first (use import to ensure it's always at the top)
+import './globals';
 
 export { iOSApplication, AndroidApplication } from './application';
 export type { ApplicationEventData, LaunchEventData, OrientationChangedEventData, UnhandledErrorEventData, DiscardedErrorEventData, CssChangedEventData, LoadAppCSSEventData, AndroidActivityEventData, AndroidActivityBundleEventData, AndroidActivityRequestPermissionsEventData, AndroidActivityResultEventData, AndroidActivityNewIntentEventData, AndroidActivityBackPressedEventData, SystemAppearanceChangedEventData } from './application';
