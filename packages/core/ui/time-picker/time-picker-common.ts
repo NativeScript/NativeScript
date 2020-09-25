21d56cf2a2c06dca6f791335f866a45d29b47c2e
asharghi <a.sharghi.mail@gmail.com>
2020-09-25 12:29:09 +0200

    fix(ios): preferredDatePickerStyle property

--- packages/core/ui/time-picker/time-picker-common.ts
+++ packages/core/ui/time-picker/time-picker-common.ts
@@ -98,6 +98,7 @@ export abstract class TimePickerBase extends View implements TimePickerDefinitio
 	public maxHour: number;
 	public minMinute: number;
 	public maxMinute: number;
+	public iosPreferredDatePickerStyle: number;
 }
 
 TimePickerBase.prototype.recycleNativeView = 'auto';
@@ -204,3 +205,9 @@ export const timeProperty = new Property<TimePickerBase, Date>({
 	},
 });
 timeProperty.register(TimePickerBase);
+
+export const iosPreferredDatePickerStyleProperty = new Property<TimePickerBase, number>({
+	name: 'iosPreferredDatePickerStyle',
+	defaultValue: 0
+});
+iosPreferredDatePickerStyleProperty.register(TimePickerBase);
