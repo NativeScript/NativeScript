d51d4405c03e8c23e88c26719fa188accce856d8
Tiago Alves <tralves@gmail.com>
2020-10-28 12:59:28 +0000

    Adds the textShadow css property;

--- packages/core/ui/styling/style/index.ts
+++ packages/core/ui/styling/style/index.ts
@@ -10,7 +10,7 @@ import { Observable } from '../../../data/observable';
 
 import { FlexDirection, FlexWrap, JustifyContent, AlignItems, AlignContent, Order, FlexGrow, FlexShrink, FlexWrapBefore, AlignSelf } from '../../layouts/flexbox-layout';
 import { Trace } from '../../../trace';
-import { TextAlignment, TextDecoration, TextTransform, WhiteSpace } from '../../text-base';
+import { TextAlignment, TextDecoration, TextTransform, WhiteSpace, TextShadow } from '../../text-base';
 
 export interface CommonLayoutParams {
 	width: number;
@@ -154,6 +154,7 @@ export class Style extends Observable implements StyleDefinition {
 	public textAlignment: TextAlignment;
 	public textDecoration: TextDecoration;
 	public textTransform: TextTransform;
+	public textShadow: TextShadow;
 	public whiteSpace: WhiteSpace;
 
 	public minWidth: Length;
