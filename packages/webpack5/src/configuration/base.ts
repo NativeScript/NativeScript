9dd5471d6d36e776cff1b2492fc1f5876b5a547f
Martin Guillon <martin.guillon@akylas.fr>
2020-11-14 15:13:09 +0100

    chore: workiing on base

--- packages/webpack5/src/configuration/base.ts
+++ packages/webpack5/src/configuration/base.ts
@@ -1,7 +1,10 @@
 import { Configuration } from 'webpack';
+import Config from 'webpack-chain';
 
 // todo: add base configuration that's shared across all flavors
 export default function (env): Configuration {
+	const config = new Config()
+	config.entry('')
 	return {
 		entry: {
 			'bundle.js': 'bundle.js',
