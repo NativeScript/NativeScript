bea55474df958ee5b64e2a7e29787a4a1d6349bc
Igor Randjelovic <rigor789@gmail.com>
2020-11-17 14:49:36 +0100

    chore: more base configuration

--- packages/webpack5/src/configuration/javascript.ts
+++ packages/webpack5/src/configuration/javascript.ts
@@ -2,9 +2,16 @@ import base from './base';
 import { IWebpackEnv } from '@nativescript/webpack';
 import Config from 'webpack-chain';
 
-// todo: add base configuration for core
+// todo: add base configuration for core with javascript
 export default function (env: IWebpackEnv): Config {
 	const config = base(env);
+
+	// set up xml
+	config.module
+		.rule('xml')
+		.test(/\.xml$/)
+		.use('xml-loader')
+		.loader('xml-loader');
 
 	return config;
 }
