c1d240d66fa0b78276c05cd4e7e07cbc7041eb5a
Igor Randjelovic <rigor789@gmail.com>
2020-11-18 14:10:44 +0100

    feat: implement basic public api

--- packages/webpack5/src/configuration/index.ts
+++ packages/webpack5/src/configuration/index.ts
@@ -7,36 +7,12 @@ import svelte from './svelte';
 import typescript from './typescript';
 import vue from './vue';
 
-// export chain configs
-// todo: rename if needed
-export { base as __base, angular as __angular, javascript as __javascript, react as __react, svelte as __svelte, typescript as __typescript, vue as __vue };
-
-// export final configs
-export const baseConfig = (env: IWebpackEnv) => base(env).toConfig();
-
-export const angularConfig = (env: IWebpackEnv) => angular(env).toConfig();
-export const javascriptConfig = (env: IWebpackEnv) => javascript(env).toConfig();
-export const reactConfig = (env: IWebpackEnv) => react(env).toConfig();
-export const svelteConfig = (env: IWebpackEnv) => svelte(env).toConfig();
-export const typescriptConfig = (env: IWebpackEnv) => typescript(env).toConfig();
-export const vueConfig = (env: IWebpackEnv) => vue(env).toConfig();
-
-export interface IWebpackEnv {
-	[name: string]: any;
-
-	appPath?: string;
-	appResourcesPath?: string;
-
-	android?: boolean;
-	ios?: boolean;
-
-	production?: boolean;
-	report?: boolean;
-	hmr?: boolean;
-	// todo: add others
-}
-
-export enum WebpackPlatform {
-	'ios',
-	'android',
-}
+export const configs = {
+	base,
+	angular,
+	javascript,
+	react,
+	svelte,
+	typescript,
+	vue,
+};
---
+++ packages/webpack5/src/helpers/temp.ts
@@ -0,0 +1,53 @@
+import { existsSync } from 'fs';
+import { getPackageJson } from './projectHelpers';
+import { resolve } from 'path';
+
+// todo: get rid of these or reduce them to their simplest form
+// no need to do magical string replacements, loops etc...
+
+/**
+ * Function to ensure the app directory exists
+ *
+ * @param appDirectory
+ */
+function verifyEntryModuleDirectory(appDirectory: string) {
+	if (!appDirectory) {
+		throw new Error('Path to app directory is not specified. Unable to find entry module.');
+	}
+
+	if (!existsSync(appDirectory)) {
+		throw new Error(`The specified path to app directory ${appDirectory} does not exist. Unable to find entry module.`);
+	}
+}
+
+function getPackageJsonEntry(appDirectory) {
+	const packageJsonSource = getPackageJson(appDirectory);
+	const entry = packageJsonSource.main;
+
+	if (!entry) {
+		throw new Error(`${appDirectory}/package.json must contain a 'main' attribute!`);
+	}
+
+	return entry.replace(/\.js$/i, '');
+}
+
+export function getEntryModule(appDirectory: string, platform: 'android' | 'ios') {
+	verifyEntryModuleDirectory(appDirectory);
+
+	const entry = getPackageJsonEntry(appDirectory);
+
+	const tsEntryPath = resolve(appDirectory, `${entry}.ts`);
+	const jsEntryPath = resolve(appDirectory, `${entry}.js`);
+	let entryExists = existsSync(tsEntryPath) || existsSync(jsEntryPath);
+	if (!entryExists && platform) {
+		const platformTsEntryPath = resolve(appDirectory, `${entry}.${platform}.ts`);
+		const platformJsEntryPath = resolve(appDirectory, `${entry}.${platform}.js`);
+		entryExists = existsSync(platformTsEntryPath) || existsSync(platformJsEntryPath);
+	}
+
+	if (!entryExists) {
+		throw new Error(`The entry module ${entry} specified in ` + `${appDirectory}/package.json doesn't exist!`);
+	}
+
+	return entry;
+}
