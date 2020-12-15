1894944b41010ce09b6a151d75a2d0cdf23d118f
Martin Guillon <martin.guillon@akylas.fr>
2020-12-15 11:37:29 +0100

    fix(android): dont create a borderDrawable if not necessary.
    That action is slow and should be prevented as much as possible

--- packages/core/ui/styling/background.android.ts
+++ packages/core/ui/styling/background.android.ts
@@ -44,8 +44,11 @@ export namespace ad {
 			const constantState = drawable.getConstantState();
 			androidView._cachedDrawable = constantState || drawable;
 		}
-
-		if (isSetColorFilterOnlyWidget(nativeView) && drawable && !background.hasBorderWidth() && !background.hasBorderRadius() && !background.clipPath && !background.image && background.color) {
+		const onlyColor = !background.hasBorderWidth() && !background.hasBorderRadius() && !background.clipPath && !background.image && background.color;
+		if (drawable instanceof  android.graphics.drawable.ColorDrawable && onlyColor) {
+			drawable.setColor(background.color.android);
+			drawable.invalidateSelf();
+		} else if (isSetColorFilterOnlyWidget(nativeView) && drawable && onlyColor) {
 			if (drawable instanceof org.nativescript.widgets.BorderDrawable && androidView._cachedDrawable) {
 				if (!(androidView._cachedDrawable instanceof android.graphics.drawable.Drawable.ConstantState)) {
 					return;
