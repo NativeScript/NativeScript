99c5f6b42c59b4ca187999f4642293bad2f22bfc
William Juan <williamjuan027@gmail.com>
2021-01-27 18:18:25 +0700

    feat(demo): move box shadow demo to a separate page

---
+++ apps/toolbox/src/box-shadow.ts
@@ -0,0 +1,127 @@
+import { Observable, StackLayout } from '@nativescript/core';
+import { EventData, Page } from '@nativescript/core';
+
+export function navigatingTo(args: EventData) {
+	const page = <Page>args.object;
+	page.bindingContext = new BoxShadowModel();
+}
+
+export class BoxShadowModel extends Observable {
+	private _selectedComponentType: string;
+	private _selectedBackgroundType: string;
+	private _selectedBorderType: string;
+	private _boxShadow: string;
+
+	background: string;
+	borderWidth: number;
+	borderRadius: number;
+	appliedBoxShadow: string;
+
+	constructor() {
+		super();
+	}
+
+	get selectedComponentType(): string {
+		return this._selectedComponentType;
+	}
+
+	set selectedComponentType(value: string) {
+		if (this._selectedComponentType !== value) {
+			this._selectedComponentType = value;
+			this.notifyPropertyChange('selectedComponentType', value);
+		}
+	}
+
+	get selectedBackgroundType(): string {
+		return this._selectedBackgroundType;
+	}
+
+	set selectedBackgroundType(value: string) {
+		if (this._selectedBackgroundType !== value) {
+			this._selectedBackgroundType = value;
+			this.notifyPropertyChange('selectedBackgroundType', value);
+			switch (value) {
+				case 'solid':
+					this.background = '#65ADF1';
+					break;
+				case 'gradient':
+					this.background = 'linear-gradient(to top, #65ADF1, white)';
+					break;
+				case 'transparent':
+					this.background = 'transparent';
+					break;
+				default:
+					break;
+			}
+			this.notifyPropertyChange('background', this.background);
+		}
+	}
+
+	get selectedBorderType(): string {
+		return this._selectedBorderType;
+	}
+
+	set selectedBorderType(value: string) {
+		this._selectedBorderType = value;
+		this.notifyPropertyChange('selectedBorderType', value);
+		switch (value) {
+			case 'solid':
+				this.borderWidth = this.borderWidth ? 0 : 2;
+				break;
+			case 'rounded':
+				this.borderRadius = this.borderRadius ? 0 : 10;
+				break;
+			case 'none':
+				this.borderRadius = 0;
+				this.borderWidth = 0;
+				break;
+			default:
+				break;
+		}
+		this.notifyPropertyChange('borderRadius', this.borderRadius);
+		this.notifyPropertyChange('borderWidth', this.borderWidth);
+	}
+
+	selectComponentType(args): void {
+		this.selectedComponentType = args.object.componentType;
+	}
+
+	selectBackgroundType(args): void {
+		this.selectedBackgroundType = args.object.backgroundType;
+	}
+
+	selectBorderType(args): void {
+		this.selectedBorderType = args.object.borderType;
+	}
+
+	selectAnimationType(args): void {
+		this.notifyPropertyChange('animationType', args.object.animationType);
+	}
+
+	applyBoxShadow(): void {
+		if (!this._boxShadow) {
+			this._boxShadow = '';
+		}
+		this.appliedBoxShadow = this._boxShadow;
+		this.notifyPropertyChange('appliedBoxShadow', this.appliedBoxShadow);
+
+		// TODO: this is a workaround to apply shadow immediately,
+		// since the box-shadow logic is currently inside background.ts
+		this.notifyPropertyChange('background', '');
+		this.notifyPropertyChange('background', this.background);
+	}
+
+	textChange(args): void {
+		this._boxShadow = args.object.text;
+	}
+
+	toggleAnimation(args) {
+		console.log('toggling animation');
+		const layout = args.object as StackLayout;
+		if (!layout.className) {
+			layout.className = 'sample-animation';
+		} else {
+			layout.className = undefined;
+		}
+	}
+}
