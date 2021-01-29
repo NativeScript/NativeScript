1e91faaf1acf71dd403951c7ceb3a0baafe92ee5
Eduardo Speroni <edusperoni@users.noreply.github.com>
2021-01-29 16:11:52 -0300

    feat(core): reusable views (#9163)

--- packages/core/ui/core/view-base/index.d.ts
+++ packages/core/ui/core/view-base/index.d.ts
@@ -237,6 +237,12 @@ export abstract class ViewBase extends Observable {
 	public bindingContext: any;
 
 	/**
+     * Gets or sets if the view is reusable.
+     * Reusable views are not automatically destroyed when removed from the View tree.
+     */
+    public reusable: boolean;
+
+	/**
 	 * Gets the name of the constructor function for this instance. E.g. for a Button class this will return "Button".
 	 */
 	public typeName: string;
@@ -364,6 +370,13 @@ export abstract class ViewBase extends Observable {
 	 * This method should *not* be overridden by derived views.
 	 */
 	_tearDownUI(force?: boolean): void;
+
+	/**
+     * Tears down the UI of a reusable node by making it no longer reusable.
+     * @see _tearDownUI
+     * @param forceDestroyChildren Force destroy the children (even if they are reusable)
+     */
+    destroyNode(forceDestroyChildren?: boolean): void;
 
 	/**
 	 * Creates a native view.
