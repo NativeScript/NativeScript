1e6a904b30ec4702da2c4286df2d79adf1b57238
Martin Guillon <martin.guillon@akylas.fr>
2021-02-04 18:15:19 +0100

    fix: correctly sanitize project name (#9193)

--- packages/webpack5/src/platforms/ios.ts
+++ packages/webpack5/src/platforms/ios.ts
@@ -3,8 +3,14 @@ import { basename } from "path";
 import { INativeScriptPlatform } from "../helpers/platform";
 import { getProjectRootPath } from "../helpers/project";
 
+function sanitizeName(appName: string): string {
+	const sanitizedName = appName.split("").filter((c) =>
+		/[a-zA-Z0-9]/.test(c)
+	).join("");
+	return sanitizedName;
+}
 function getDistPath() {
-	const appName = basename(getProjectRootPath());
+	const appName = sanitizeName(basename(getProjectRootPath()));
 	return `platforms/ios/${appName}/app`;
 }
 
