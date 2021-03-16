7de3f4a4c88f288e3347e92067a0adc8ced6b9aa
Nathan Walker <walkerrunpdx@gmail.com>
2021-03-15 20:44:44 -0700

    fix: move BottomNavigation and Tabs to @nativescript-community
    Cocoapods no longer required with @nativescript/core out of the box and users are no longer taxed with MaterialTabs even if they didn't use those components.
    
    BREAKING CHANGES
    
    If using `BottomNavigation`, just install `@nativescript-community/ui-material-bottom-navigation` and update your imports to use it. API is exactly the same.
    
    If using `Tabs`, just install `@nativescript-community/ui-material-tabs` and update your imports to use it. API is exactly the same.

--- apps/ui/src/bottom-navigation/fancy-fonts-page.ts
+++
@@ -1,9 +0,0 @@
-import { EventData, Page, BottomNavigation } from '@nativescript/core';
-
-export const selectTab = (args: EventData) => {
-	const page = <Page>(<any>args.object).page;
-	const bottomNav = <BottomNavigation>page.getViewById('bottomNavigation');
-
-	const index = (<any>args.object).tag;
-	bottomNav.selectedIndex = index;
-};
