55bbbc3684268087ab12e3e5322227710d0b678c
Martin Guillon <martin.guillon@akylas.fr>
2021-03-18 14:00:48 +0100

    chore: webpack tsc error

--- packages/webpack/jasmine-config/reporter.ts
+++ packages/webpack/jasmine-config/reporter.ts
@@ -1,4 +1,4 @@
 import { SpecReporter } from 'jasmine-spec-reporter';
 
 jasmine.getEnv().clearReporters();
-jasmine.getEnv().addReporter(new SpecReporter());
+jasmine.getEnv().addReporter(new SpecReporter() as any);
