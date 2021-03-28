0b32d5a88d1873cdd7e566824477bdd3420b1f9e
Igor Randjelovic <rigor789@gmail.com>
2021-03-28 23:04:15 +0200

    fix: hmr runtime for core/ts

--- packages/webpack5/src/configuration/typescript.ts
+++ packages/webpack5/src/configuration/typescript.ts
@@ -1,13 +1,13 @@
 import Config from 'webpack-chain';
 
+import { getEntryDirPath, getEntryPath } from "../helpers/platform";
 import { addVirtualEntry } from '../helpers/virtualModules';
-import { getEntryDirPath } from "../helpers/platform";
 import { env as _env, IWebpackEnv } from '../index';
 import base from './base';
 
 export default function (config: Config, env: IWebpackEnv = _env): Config {
 	base(config, env);
-
+	const entryPath = getEntryPath();
 	const filterRE = '/.(xml|js|ts|s?css)$/';
 	const virtualEntryPath = addVirtualEntry(
 		config,
@@ -38,6 +38,7 @@ export default function (config: Config, env: IWebpackEnv = _env): Config {
 		.test(/\.(js|ts)$/)
 		.exclude
 		.add(/node_modules/)
+		.add(entryPath)
 		.end()
 		.use('nativescript-hot-loader')
 		.loader('nativescript-hot-loader')
