0cce23f1093704a92dc310231aac6c0e53b6a54c
Janos Hrubos <33330538+janoshrubos@users.noreply.github.com>
2021-05-06 14:43:24 +0200

    feat: typedoc api reference

--- tools/scripts/build-docs.sh
+++ tools/scripts/build-docs.sh
@@ -3,7 +3,6 @@ set -e
 
 ENV="${ENV:-dev}"
 DIST_DIR="bin/dist"
-TARGET_DIR="$DIST_DIR/snippets"
 PACKAGE_VERSION="${PACKAGE_VERSION:-0.0.0}"
 
 archive_dist_dir() {
@@ -13,44 +12,27 @@ archive_dist_dir() {
 
 npm_install() {
     # Don't install modules twice.
-
     MARKER_FILE="./node_modules/installed"
     if [ ! -f "$MARKER_FILE" ] ; then
+				# Fixes perm issue while installing
+				npm i -g npm@^6.13.6
         npm install
         npm install @types/handlebars@4.0.33
         touch "$MARKER_FILE"
     fi
 }
 
-extract_snippets() {
-    BIN="./node_modules/markdown-snippet-injector/extract.js"
-
-    npm install markdown-snippet-injector
-
-    for SNIPPET_DIR in {tests/app,apps/app,nativescript-core} ; do
-        echo "Extracting snippets from: $SNIPPET_DIR"
-        node "$BIN" --root="$SNIPPET_DIR" --target="$TARGET_DIR" \
-            --sourceext=".js|.ts|.xml|.html|.css"
-    done
-
-    archive_dist_dir "snippets"
-}
-
 extract_apiref() {
     APIREF_DIR="$DIST_DIR/api-reference"
     rm -rf "$APIREF_DIR"
 
-    npm_install
-    npm run typedoc
+#    npm_install
+    npx typedoc --tsconfig tools/scripts/tsconfig.typedoc.json
 
-    mv "$DIST_DIR/apiref" "$APIREF_DIR"
+    mv "dist/apiref" "$APIREF_DIR"
     archive_dist_dir "api-reference"
 }
 
-rm -rf "$TARGET_DIR"
-mkdir -p "$TARGET_DIR"
-
 if [ "${BASH_SOURCE[0]}" == "$0" ] ; then
-    extract_snippets
     extract_apiref
 fi
