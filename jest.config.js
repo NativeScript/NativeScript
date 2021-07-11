83631ad46ec23394e6d1ddab708594056b855f43
dhruvaop <71749153+dhruvaop@users.noreply.github.com>
2021-07-12 03:35:19 +0530

    Update jest.config.js
    changed the root dir

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-	projects: ['<rootDir>/packages/core'],
+  preset: '../../jest.preset.js',
+  coverageDirectory: '../../coverage/apps/products',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
+    },
+  },
+  displayName: 'products',
 };
