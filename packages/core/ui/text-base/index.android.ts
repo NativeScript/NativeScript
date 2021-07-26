38a61cca601887dc2d26fb3c82df9ebb8f57fcc4
Martin Guillon <martin.guillon@akylas.fr>
2021-07-26 16:23:27 +0200

    fix: more text base fixes for plugins using `nativeTextViewProtected`

--- packages/core/ui/text-base/index.android.ts
+++ packages/core/ui/text-base/index.android.ts
@@ -345,10 +345,10 @@ export class TextBase extends TextBaseCommon {
 	}
 
 	[lineHeightProperty.getDefault](): number {
-		return this.nativeView.getLineSpacingExtra() / layout.getDisplayDensity();
+		return this.nativeTextViewProtected.getLineSpacingExtra() / layout.getDisplayDensity();
 	}
 	[lineHeightProperty.setNative](value: number) {
-		this.nativeView.setLineSpacing(value * layout.getDisplayDensity(), 1);
+		this.nativeTextViewProtected.setLineSpacing(value * layout.getDisplayDensity(), 1);
 	}
 
 	[fontInternalProperty.getDefault](): android.graphics.Typeface {
@@ -395,7 +395,7 @@ export class TextBase extends TextBaseCommon {
 
 	[textShadowProperty.setNative](value: CSSShadow) {
 		// prettier-ignore
-		this.nativeViewProtected.setShadowLayer(
+		this.nativeTextViewProtected.setShadowLayer(
 			Length.toDevicePixels(value.blurRadius, java.lang.Float.MIN_VALUE),
 			Length.toDevicePixels(value.offsetX, 0),
 			Length.toDevicePixels(value.offsetY, 0),
@@ -437,7 +437,7 @@ export class TextBase extends TextBaseCommon {
 	[paddingLeftProperty.setNative](value: CoreTypes.LengthType) {
 		org.nativescript.widgets.ViewHelper.setPaddingLeft(this.nativeTextViewProtected, Length.toDevicePixels(value, 0) + Length.toDevicePixels(this.style.borderLeftWidth, 0));
 	}
-	
+
 	[accessibilityIdentifierProperty.setNative](value: string): void {
 		// we override the default setter to apply it on nativeTextViewProtected
 		const id = Utils.ad.resources.getId(':id/nativescript_accessibility_id');
@@ -471,10 +471,10 @@ export class TextBase extends TextBaseCommon {
 		if (this._tappable !== tappable) {
 			this._tappable = tappable;
 			if (this._tappable) {
-				this.nativeViewProtected.setMovementMethod(android.text.method.LinkMovementMethod.getInstance());
-				this.nativeViewProtected.setHighlightColor(null);
+				this.nativeTextViewProtected.setMovementMethod(android.text.method.LinkMovementMethod.getInstance());
+				this.nativeTextViewProtected.setHighlightColor(null);
 			} else {
-				this.nativeViewProtected.setMovementMethod(this._defaultMovementMethod);
+				this.nativeTextViewProtected.setMovementMethod(this._defaultMovementMethod);
 			}
 		}
 	}
