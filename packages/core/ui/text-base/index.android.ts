200d49fce7d1dd2b7abf50fe77111d4823ee4d99
Juan de Dios Martínez Vallejo <vallemardeveloper@gmail.com>
2021-09-24 21:27:12 +0200

    refactor

--- packages/core/ui/text-base/index.android.ts
+++ packages/core/ui/text-base/index.android.ts
@@ -286,15 +286,14 @@ export class TextBase extends TextBaseCommon {
 	[textAlignmentProperty.setNative](value: CoreTypes.TextAlignmentType) {
 		const verticalGravity = this.nativeTextViewProtected.getGravity() & android.view.Gravity.VERTICAL_GRAVITY_MASK;
 		switch (value) {
-			case 'initial':
-			case 'left':
-				this.nativeTextViewProtected.setGravity(android.view.Gravity.START | verticalGravity);
-				break;
 			case 'center':
 				this.nativeTextViewProtected.setGravity(android.view.Gravity.CENTER_HORIZONTAL | verticalGravity);
 				break;
 			case 'right':
 				this.nativeTextViewProtected.setGravity(android.view.Gravity.END | verticalGravity);
+				break;
+			default: // initial | left | justify
+				this.nativeTextViewProtected.setGravity(android.view.Gravity.START | verticalGravity);
 				break;
 		}
 		if (android.os.Build.VERSION.SDK_INT >= 25) {
