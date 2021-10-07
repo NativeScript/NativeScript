51828f802f5892b08bc2ab2ca8545d28b0f33ce4
Igor Randjelovic <rigor789@gmail.com>
2021-10-07 12:18:45 +0200

    chore(migrate): apps/toolbox to webpack5

--- apps/toolbox/nativescript.config.ts
+++ apps/toolbox/nativescript.config.ts
@@ -3,10 +3,8 @@ import { NativeScriptConfig } from '@nativescript/core';
 export default {
 	id: 'org.nativescript.ToolBox',
 	appResourcesPath: '../../tools/assets/App_Resources',
-	webpackConfigPath: 'webpack.custom.config.js',
 	android: {
 		v8Flags: '--expose_gc',
-		markingMode: 'none',
 		suppressCallJSMethodExceptions: false,
 	},
 } as NativeScriptConfig;
