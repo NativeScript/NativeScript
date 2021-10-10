ab3a288a6b512e4db4c5c2326c85177f7c769dfc
Igor Randjelovic <rigor789@gmail.com>
2021-10-10 13:57:02 +0200

    chore(migrate): apps/ui to webpack5

--- apps/automated/nativescript.config.ts
+++ apps/automated/nativescript.config.ts
@@ -2,6 +2,7 @@ import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
 	id: 'org.nativescript.UnitTestApp',
+	appPath: 'src',
 	appResourcesPath: '../../tools/assets/App_Resources',
 	android: {
 		v8Flags: '--expose_gc',
--- apps/toolbox/nativescript.config.ts
+++ apps/toolbox/nativescript.config.ts
@@ -2,6 +2,7 @@ import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
 	id: 'org.nativescript.ToolBox',
+	appPath: 'src',
 	appResourcesPath: '../../tools/assets/App_Resources',
 	android: {
 		v8Flags: '--expose_gc',
