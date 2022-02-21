282ac57b6c7a9ea750a7cdf4c0f42199f3fc3a5c
Igor Randjelovic <rigor789@gmail.com>
2022-02-21 23:28:06 +0100

    feat(android): update ui-mobile-base to gradle7 (#9778)

--- packages/ui-mobile-base/android/widgets/src/main/java/org/nativescript/widgets/ContentLayout.java
+++ packages/ui-mobile-base/android/widgets/src/main/java/org/nativescript/widgets/ContentLayout.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.nativescript.widgets;
 
@@ -18,38 +18,38 @@ public class ContentLayout extends LayoutBase {
 
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        CommonLayoutParams.adjustChildrenLayoutParams(this, widthMeasureSpec, heightMeasureSpec);
+		CommonLayoutParams.adjustChildrenLayoutParams(this, widthMeasureSpec, heightMeasureSpec);
 
-        int measureWidth = 0;
-        int measureHeight = 0;
+		int measureWidth = 0;
+		int measureHeight = 0;
 
-        for (int i = 0, count = this.getChildCount(); i < count; i++) {
-            View child = this.getChildAt(i);
-            if (child.getVisibility() == View.GONE) {
-                continue;
-            }
+		for (int i = 0, count = this.getChildCount(); i < count; i++) {
+			View child = this.getChildAt(i);
+			if (child.getVisibility() == View.GONE) {
+				continue;
+			}
 
-        	CommonLayoutParams.measureChild(child, widthMeasureSpec, heightMeasureSpec);
-            final int childMeasuredWidth = CommonLayoutParams.getDesiredWidth(child);
-            final int childMeasuredHeight = CommonLayoutParams.getDesiredHeight(child);
-            
-            measureWidth = Math.max(measureWidth, childMeasuredWidth);
-            measureHeight = Math.max(measureHeight, childMeasuredHeight);
-        }
-        
-        // Add in our padding
-        measureWidth += this.getPaddingLeft() + this.getPaddingRight();
-        measureHeight += this.getPaddingTop() + this.getPaddingBottom();
+			CommonLayoutParams.measureChild(child, widthMeasureSpec, heightMeasureSpec);
+			final int childMeasuredWidth = CommonLayoutParams.getDesiredWidth(child);
+			final int childMeasuredHeight = CommonLayoutParams.getDesiredHeight(child);
 
-        // Check against our minimum sizes
-        measureWidth = Math.max(measureWidth, this.getSuggestedMinimumWidth());
-        measureHeight = Math.max(measureHeight, this.getSuggestedMinimumHeight());
+			measureWidth = Math.max(measureWidth, childMeasuredWidth);
+			measureHeight = Math.max(measureHeight, childMeasuredHeight);
+		}
 
-        int widthSizeAndState = resolveSizeAndState(measureWidth, widthMeasureSpec, 0);
-        int heightSizeAndState = resolveSizeAndState(measureHeight, heightMeasureSpec, 0);
+		// Add in our padding
+		measureWidth += this.getPaddingLeft() + this.getPaddingRight();
+		measureHeight += this.getPaddingTop() + this.getPaddingBottom();
 
-        this.setMeasuredDimension(widthSizeAndState, heightSizeAndState);
-    }
+		// Check against our minimum sizes
+		measureWidth = Math.max(measureWidth, this.getSuggestedMinimumWidth());
+		measureHeight = Math.max(measureHeight, this.getSuggestedMinimumHeight());
+
+		int widthSizeAndState = resolveSizeAndState(measureWidth, widthMeasureSpec, 0);
+		int heightSizeAndState = resolveSizeAndState(measureHeight, heightMeasureSpec, 0);
+
+		this.setMeasuredDimension(widthSizeAndState, heightSizeAndState);
+	}
 
 	@Override
 	protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
@@ -57,20 +57,20 @@ public class ContentLayout extends LayoutBase {
 		int paddingRight = this.getPaddingRight();
 		int paddingTop = this.getPaddingTop();
 		int paddingBottom = this.getPaddingBottom();
-		
+
 		int childLeft = paddingLeft;
 		int childTop = paddingTop;
-		
+
 		int childRight = right - left - (paddingLeft + paddingRight);
 		int childBottom = bottom - top - (paddingRight + paddingBottom);
 
 		for (int i = 0, count = this.getChildCount(); i < count; i++) {
-		    View child = this.getChildAt(i);
-		    if (child.getVisibility() == View.GONE) {
-		        continue;
-		    }
-		
-		    CommonLayoutParams.layoutChild(child, childLeft, childTop, childRight, childBottom);
+			View child = this.getChildAt(i);
+			if (child.getVisibility() == View.GONE) {
+				continue;
+			}
+
+			CommonLayoutParams.layoutChild(child, childLeft, childTop, childRight, childBottom);
 		}
 
 		CommonLayoutParams.restoreOriginalParams(this);
--- packages/ui-mobile-base/android/widgets/src/main/java/org/nativescript/widgets/TabViewPager.java
+++ packages/ui-mobile-base/android/widgets/src/main/java/org/nativescript/widgets/TabViewPager.java
@@ -1,62 +1,64 @@
 package org.nativescript.widgets;
 
 import android.content.Context;
-import androidx.viewpager.widget.ViewPager;
 import android.util.AttributeSet;
-import android.view.MotionEvent;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.ViewPager;
 
 // See this thread for more information https://stackoverflow.com/questions/9650265
 public class TabViewPager extends ViewPager {
-    private boolean swipePageEnabled = true;
-    private boolean animationEnabled = true;
+	private boolean swipePageEnabled = true;
+	private boolean animationEnabled = true;
 
-    public TabViewPager(Context context) {
-        super(context);
-    }
+	public TabViewPager(Context context) {
+		super(context);
+	}
 
-    public TabViewPager(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
+	public TabViewPager(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
 
-    public void setSwipePageEnabled(boolean enabled) {
-        this.swipePageEnabled = enabled;
-    }
+	public void setSwipePageEnabled(boolean enabled) {
+		this.swipePageEnabled = enabled;
+	}
 
-    public void setAnimationEnabled(boolean enabled) {
-        this.animationEnabled = enabled;
-    }
+	public void setAnimationEnabled(boolean enabled) {
+		this.animationEnabled = enabled;
+	}
 
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent event) {
-        if (this.swipePageEnabled) {
-            return super.onInterceptTouchEvent(event);
-        }
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent event) {
+		if (this.swipePageEnabled) {
+			return super.onInterceptTouchEvent(event);
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if (this.swipePageEnabled) {
-            return super.onTouchEvent(event);
-        }
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (this.swipePageEnabled) {
+			return super.onTouchEvent(event);
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    @Override
-    public boolean executeKeyEvent(KeyEvent event) {
-        if (this.swipePageEnabled) {
-            return super.executeKeyEvent(event);
-        }
+	@Override
+	public boolean executeKeyEvent(@NonNull KeyEvent event) {
+		if (this.swipePageEnabled) {
+			return super.executeKeyEvent(event);
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    @Override
-    public void setCurrentItem(int item) {
-        boolean smoothScroll = this.animationEnabled;
-        super.setCurrentItem(item, smoothScroll);
-    }
+	@Override
+	public void setCurrentItem(int item) {
+		boolean smoothScroll = this.animationEnabled;
+		super.setCurrentItem(item, smoothScroll);
+	}
 }
