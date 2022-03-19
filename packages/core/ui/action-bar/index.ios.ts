b6f95d416e83ec7ace0fbd4e64b54962038d32d3
Dimitris - Rafail Katsampas <katsampasdr@gmail.com>
2022-03-19 23:48:34 +0200

    fix(iOS15): Navigation button is not using custom icon.

--- packages/core/ui/action-bar/index.ios.ts
+++ packages/core/ui/action-bar/index.ios.ts
@@ -253,14 +253,10 @@ export class ActionBar extends ActionBarBase {
 		// TODO: This could cause issue when canceling BackEdge gesture - we will change the backIndicator to
 		// show the one from the old page but the new page will still be visible (because we canceled EdgeBackSwipe gesutre)
 		// Consider moving this to new method and call it from - navigationControllerDidShowViewControllerAnimated.
-		if (img) {
-			const image = img.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal);
-			navigationBar.backIndicatorImage = image;
-			navigationBar.backIndicatorTransitionMaskImage = image;
-		} else {
-			navigationBar.backIndicatorImage = null;
-			navigationBar.backIndicatorTransitionMaskImage = null;
-		}
+		const image = img ? img.imageWithRenderingMode(1 /* AlwaysOriginal */) : null;
+		const appearance = this._getAppearance(navigationBar);
+		appearance.setBackIndicatorImageTransitionMaskImage(image, image);
+		this._updateAppearance(navigationBar, appearance);
 
 		// Set back button visibility
 		if (this.navigationButton) {
