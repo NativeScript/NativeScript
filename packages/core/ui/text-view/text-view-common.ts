2e98d92d021bc13dbeffadb6afbd40a0fe299802
farfromrefuge <martin.guillon@akylas.fr>
2022-04-30 19:50:05 +0200

    feat(core): maxLines support for all text components (#9884)

--- packages/core/ui/text-view/text-view-common.ts
+++ packages/core/ui/text-view/text-view-common.ts
@@ -1,14 +1,7 @@
 import { TextView as TextViewDefinition } from '.';
 import { EditableTextBase } from '../editable-text-base';
-import { Property } from '../core/properties';
 
 export class TextViewBase extends EditableTextBase implements TextViewDefinition {
 	public static returnPressEvent = 'returnPress';
 	public maxLines: number;
 }
-
-export const maxLinesProperty = new Property<EditableTextBase, number>({
-	name: 'maxLines',
-	valueConverter: parseInt,
-});
-maxLinesProperty.register(EditableTextBase);
