80e6b8130a85fcd36d6e3bde581ae7d5e4bd1612
Nathan Walker <walkerrunpdx@gmail.com>
2022-06-04 10:07:59 -0700

    feat(types-minimal): minimal types for only the latest Android and iOS sdks (#9927)

---
+++ packages/types-minimal/src/lib/ios/ios.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="runtime.d.ts" />
+/// <reference path="objc-x86_64/objc!AVFoundation.d.ts" />
+/// <reference path="objc-x86_64/objc!Foundation.d.ts" />
+/// <reference path="objc-x86_64/objc!ObjectiveC.d.ts" />
+/// <reference path="objc-x86_64/objc!UIKit.d.ts" />
