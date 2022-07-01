f5f96c559859bc97111b2e23874588370c8086f5
Nathan Walker <walkerrunpdx@gmail.com>
2022-07-01 14:40:25 -0700

    feat(types-minimal): include AVFAudio and ARKit

--- packages/types-minimal/src/lib/ios/ios.d.ts
+++ packages/types-minimal/src/lib/ios/ios.d.ts
@@ -1,4 +1,6 @@
 /// <reference path="runtime.d.ts" />
+/// <reference path="objc-x86_64/objc!ARKit.d.ts" />
+/// <reference path="objc-x86_64/objc!AVFAudio.d.ts" />
 /// <reference path="objc-x86_64/objc!AVFoundation.d.ts" />
 /// <reference path="objc-x86_64/objc!Foundation.d.ts" />
 /// <reference path="objc-x86_64/objc!ObjectiveC.d.ts" />
