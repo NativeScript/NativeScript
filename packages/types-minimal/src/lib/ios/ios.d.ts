1786d659d93071321771dde0826d2ebfb598b789
Nathan Walker <walkerrunpdx@gmail.com>
2022-07-09 08:52:20 -0700

    feat(types-minimal): PDFKit and Vision

--- packages/types-minimal/src/lib/ios/ios.d.ts
+++ packages/types-minimal/src/lib/ios/ios.d.ts
@@ -4,4 +4,6 @@
 /// <reference path="objc-x86_64/objc!AVFoundation.d.ts" />
 /// <reference path="objc-x86_64/objc!Foundation.d.ts" />
 /// <reference path="objc-x86_64/objc!ObjectiveC.d.ts" />
+/// <reference path="objc-x86_64/objc!PDFKit.d.ts" />
 /// <reference path="objc-x86_64/objc!UIKit.d.ts" />
+/// <reference path="objc-x86_64/objc!Vision.d.ts" />
