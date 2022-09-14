e4b5cdf7df86be9d0ea2730438e134bdaf1c3a1a
Dimitris-Rafail Katsampas <katsampasdr@gmail.com>
2022-09-14 08:17:50 +0300

    fix(ios): properties lineHeight and letterSpacing did not apply to spans (#10025)

--- packages/core/platforms/ios/src/UIView+NativeScript.m
+++ packages/core/platforms/ios/src/UIView+NativeScript.m
@@ -75,9 +75,7 @@
     }
 }
 
--(void)nativeScriptSetFormattedTextDecorationAndTransform:(NSDictionary*)details {
-    CGFloat letterSpacing = [[details valueForKey:@"letterSpacing"] doubleValue];
-    CGFloat lineHeight = [[details valueForKey:@"lineHeight"] doubleValue];
+-(void)nativeScriptSetFormattedTextDecorationAndTransform:(NSDictionary*)details letterSpacing:(CGFloat)letterSpacing lineHeight:(CGFloat)lineHeight {
     NSMutableAttributedString *attrText = [NativeScriptUtils createMutableStringWithDetails:details];
     if (letterSpacing != 0) {
         NSNumber *kern = [NSNumber numberWithDouble:letterSpacing * ((UITextView*)self).font.pointSize];
@@ -89,7 +87,7 @@
     
     if (lineHeight > 0) {
         NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
-        paragraphStyle.minimumLineHeight = lineHeight;
+        paragraphStyle.lineSpacing = lineHeight;
         // make sure a possible previously set text alignment setting is not lost when line height is specified
         if ([self isKindOfClass:[UIButton class]]) {
             paragraphStyle.alignment = ((UIButton*)self).titleLabel.textAlignment;
