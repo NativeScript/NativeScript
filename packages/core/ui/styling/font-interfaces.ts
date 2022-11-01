61d6ed605ab8edc0c2bba07ebee1f7d6110ed573
Martin Guillon <dev@akylas.fr>
2022-11-01 08:43:33 +0100

    chore: fixed font typings
    TODO: needs refactoring as there are a lot of duplicated typings

--- packages/core/ui/styling/font-interfaces.ts
+++ packages/core/ui/styling/font-interfaces.ts
@@ -1,6 +1,6 @@
 ﻿export type FontStyle = 'normal' | 'italic';
 
-export type FontWeight = '100' | '200' | '300' | 'normal' | '400' | '500' | '600' | 'bold' | '700' | '800' | '900';
+export type FontWeight = '100' | '200' | '300' | 'normal' | '400' | '500' | '600' | 'bold' | '700' | '800' | '900' | number;
 
 export interface ParsedFont {
 	fontStyle?: FontStyle;
