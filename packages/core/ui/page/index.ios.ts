NativeScript/NativeScript

commit 6148955335f164aa160e719a98ad811b4d006560

fix(ios): frame navigatingTo event (#10096)

packages/core/ui/page/index.ios.ts
@@ -118,13 +118,6 @@ class UIViewControllerImpl extends UIViewController {
 		}
 
 		const frame = this.navigationController ? (<any>this.navigationController).owner : null;
-		const newEntry = this[ENTRY];
-
-		// Don't raise event if currentPage was showing modal page.
-		if (!owner._presentedViewController && newEntry && (!frame || frame.currentPage !== owner)) {
-			const isBack = isBackNavigationTo(owner, newEntry);
-			owner.onNavigatingTo(newEntry.entry.context, isBack, newEntry.entry.bindingContext);
-		}
 
 		if (frame) {
 			if (!owner.parent) {

packages/core/ui/frame/index.ios.ts
@@ -307,7 +307,7 @@ export class Frame extends FrameBase {
 	}
 
 	public _onNavigatingTo(backstackEntry: BackstackEntry, isBack: boolean) {
-		//
+		super._onNavigatingTo(backstackEntry, isBack);
 	}
 }
 

commit 8e5bcdd5d85bc2dd14d0ccd366ffdaec2912c0b6

fix(ios): revert regression from PR #10096
https://github.com/NativeScript/NativeScript/pull/10096

The changes in `ui/page/index.ios.ts` where not meant to be there.

packages/core/ui/page/index.ios.ts
@@ -118,7 +118,14 @@ class UIViewControllerImpl extends UIViewController {
 		}
 
 		const frame = this.navigationController ? (<any>this.navigationController).owner : null;
+		const newEntry = this[ENTRY];
 
+		// Don't raise event if currentPage was showing modal page.
+		if (!owner._presentedViewController && newEntry && (!frame || frame.currentPage !== owner)) {
+			const isBack = isBackNavigationTo(owner, newEntry);
+			owner.onNavigatingTo(newEntry.entry.context, isBack, newEntry.entry.bindingContext);
+		}
+		
 		if (frame) {
 			if (!owner.parent) {
 				owner._frame = frame;