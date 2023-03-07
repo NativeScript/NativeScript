eaa1708715337318c5b20f542785ec5953e46f9b
ammarahm-ed <ammarahmed6506@gmail.com>
2023-03-07 23:19:17 +0500

    do not bind window to global by default

--- packages/core/ui/core/dom/index.ts
+++ packages/core/ui/core/dom/index.ts
@@ -1,5 +1,3 @@
-export { HTMLLabelElement } from './src/nodes/html-label-element/HTMLLabelELement';
 import GlobalWindow from './src/window/Window';
 
 export const Window = GlobalWindow;
-new Window().bindToGlobal();
