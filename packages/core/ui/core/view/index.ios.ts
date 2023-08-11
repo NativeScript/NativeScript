d3a0f2c9a6711cead60bc66e3e416a69f634bd92
Nathan Walker <walkerrunpdx@gmail.com>
2023-08-11 07:09:32 -0700

    chore: TypeScript 5.1 with Nx 16.6 migrations (#10338)

--- packages/core/ui/core/view/index.ios.ts
+++ packages/core/ui/core/view/index.ios.ts
@@ -31,7 +31,9 @@ const PFLAG_LAYOUT_REQUIRED = 1 << 2;
 const majorVersion = iOSNativeHelper.MajorVersion;
 
 export class View extends ViewCommon implements ViewDefinition {
+	// @ts-ignore
 	nativeViewProtected: UIView;
+	// @ts-ignore
 	viewController: UIViewController;
 	private _popoverPresentationDelegate: IOSHelper.UIPopoverPresentationControllerDelegateImp;
 	private _adaptivePresentationDelegate: IOSHelper.UIAdaptivePresentationControllerDelegateImp;
@@ -992,8 +994,6 @@ class UIViewControllerTransitioningDelegateImpl extends NSObject implements UIVi
 }
 
 export class ContainerView extends View {
-	public iosOverflowSafeArea: boolean;
-
 	constructor() {
 		super();
 		this.iosOverflowSafeArea = true;
@@ -1001,6 +1001,7 @@ export class ContainerView extends View {
 }
 
 export class CustomLayoutView extends ContainerView {
+	// @ts-ignore
 	nativeViewProtected: UIView;
 
 	createNativeView() {
