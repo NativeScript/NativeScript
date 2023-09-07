7475f9b092e5e17bc18cc755b019bbaef2c8c010
Juan de Dios Martínez Vallejo <vallemardeveloper@gmail.com>
2023-09-07 12:39:00 +0200

    format

--- packages/core/utils/index.android.ts
+++ packages/core/utils/index.android.ts
@@ -180,7 +180,7 @@ export function dismissSoftInput(nativeView?: any): void {
 export function dismissKeyboard() {
 	dismissSoftInput();
     const modalDialog = (topmost()?._modalParent ?? topmost())?.modal?._dialogFragment?.getDialog();
-    const view = (modalDialog ?? AndroidUtils.getCurrentActivity());
+    const view = modalDialog ?? AndroidUtils.getCurrentActivity();
 	if (view) {
 		const focus = view.getCurrentFocus();
 
