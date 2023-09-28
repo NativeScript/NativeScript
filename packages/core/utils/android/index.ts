38551b426821a8ec711b98b196b4ba0c0a55bac9
Nathan Walker <walkerrunpdx@gmail.com>
2023-09-28 11:52:51 -0700

    chore: cleanup

--- packages/core/utils/android/index.ts
+++ packages/core/utils/android/index.ts
@@ -68,7 +68,7 @@ export function dismissSoftInput(nativeView?: android.view.View): void {
 		}
 		windowToken = nativeView.getWindowToken();
 	} else if (getCurrentActivity() instanceof androidx.appcompat.app.AppCompatActivity) {
-		const modalDialog = (topmost()?._modalParent ?? topmost())?.modal?._dialogFragment?.getDialog();
+		const modalDialog = (topmost()?._modalParent ?? (topmost()?.modal as any))?._dialogFragment?.getDialog();
 		const window = (modalDialog ?? getCurrentActivity()).getWindow();
 		const decorView = window.getDecorView();
 		if (decorView) {
