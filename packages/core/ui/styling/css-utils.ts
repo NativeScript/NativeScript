4866208dac8c0f68c5eed7322a56d7061418aad9
Nathan Walker <walkerrunpdx@gmail.com>
2023-12-18 09:08:34 -0800

    fix: tests

--- packages/core/ui/styling/css-utils.ts
+++ packages/core/ui/styling/css-utils.ts
@@ -4,7 +4,7 @@ import { Length } from './style-properties';
 
 export function cleanupImportantFlags(value: unknown, propertyName: string) {
 	if (typeof value !== 'string') {
-		return value;
+		return '' + value;
 	}
 
 	const index = value?.indexOf('!important');
