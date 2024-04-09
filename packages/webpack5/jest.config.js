5396ce11903db00c5e234bbadac085320e8ee155
fafromrefug <dev@akylas.fr>
2024-04-09 11:31:59 +0200

    chore: webpack plugin build fix

--- packages/webpack5/jest.config.js
+++ packages/webpack5/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
 	moduleNameMapper: {
-		'^@nativescript/webpack$': '<rootDir>/src'
+		'^@akylas/nativescript-webpack$': '<rootDir>/src'
 	},
 	setupFiles: [
 		'<rootDir>/scripts/jest.setup.ts'
