39e72dab6860cde109512a278d498f8ed7d6bb9a
Dimitris - Rafail Katsampas <katsampasdr@gmail.com>
2024-04-14 18:14:12 +0000

    fix: Added null-check for pseudo-class lookupSort calls

--- packages/core/ui/styling/css-selector.ts
+++ packages/core/ui/styling/css-selector.ts
@@ -429,10 +429,10 @@ export class FunctionalPseudoClassSelector extends PseudoClassSelector {
 			sequence.trackChanges(node, map);
 		}
 	}
-
 	public lookupSort(sorter: LookupSorter, base?: SelectorCore): void {
+		const baseSelector = base || this;
 		for (const sequence of this.selectorSequences) {
-			sequence.lookupSort(sorter, base);
+			sequence.lookupSort(sorter, baseSelector);
 		}
 	}
 }
