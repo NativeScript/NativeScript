1c3b5c8ed394f30201ef30fcf8fe69bc73f33cdb
fafromrefug <dev@akylas.fr>
2024-06-04 15:16:37 +0200

    fix: Application `once` method fix (was not removed after first event)

--- packages/core/application/application-common.ts
+++ packages/core/application/application-common.ts
@@ -153,7 +153,7 @@ export class ApplicationCommon {
 	// Application events go through the global events.
 	on: ApplicationEvents['on'] = globalEvents.on.bind(globalEvents);
 	off: ApplicationEvents['off'] = globalEvents.off.bind(globalEvents);
-	once: ApplicationEvents['once'] = globalEvents.on.bind(globalEvents);
+	once: ApplicationEvents['once'] = globalEvents.once.bind(globalEvents);
 	notify: ApplicationEvents['notify'] = globalEvents.notify.bind(globalEvents);
 	hasListeners: ApplicationEvents['hasListeners'] = globalEvents.hasListeners.bind(globalEvents);
 
