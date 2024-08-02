2814fe37f7df571cb05abf726ecb7acb46601675
Dimitris - Rafail Katsampas <dimitris.katsampas@fuelics.com>
2024-08-03 01:30:47 +0300

    fix: Updated color regex validity to accept 4-digit hex

--- packages/core/color/color-common.ts
+++ packages/core/color/color-common.ts
@@ -4,7 +4,7 @@ import * as knownColors from './known-colors';
 import { Color } from '.';
 
 const SHARP = '#';
-const HEX_REGEX = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)|(^#[0-9A-F]{8}$)/i;
+const HEX_REGEX = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)|(^#[0-9A-F]{8}$)|(^#[0-9A-F]{4}$)/i;
 
 export class ColorBase implements definition.Color {
 	private _argb: number;
@@ -578,7 +578,7 @@ function hsvToRgb(h1, s1, v1) {
 	const s = s1 / 100;
 	const v = v1 / 100;
 
-	var i = Math.floor(h),
+	const i = Math.floor(h),
 		f = h - i,
 		p = v * (1 - s),
 		q = v * (1 - f * s),
