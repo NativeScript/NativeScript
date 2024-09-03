050601232ac4f424e9d3ba6b711f3ada4afe253b
Igor Randjelovic <rigor789@gmail.com>
2024-09-03 17:59:03 +0200

    fix(layout): prevent negative width/height values (#10616)

--- packages/core/ui/core/view/view-helper/view-helper-common.ts
+++ packages/core/ui/core/view/view-helper/view-helper-common.ts
@@ -82,6 +82,9 @@ export class ViewHelper {
 			default:
 				childTop = top + effectiveMarginTop;
 				childHeight = bottom - top - (effectiveMarginTop + effectiveMarginBottom);
+				if (childHeight < 0) {
+					childHeight = 0;
+				}
 				break;
 		}
 
@@ -112,6 +115,9 @@ export class ViewHelper {
 			default:
 				childLeft = left + effectiveMarginLeft;
 				childWidth = right - left - (effectiveMarginLeft + effectiveMarginRight);
+				if (childWidth < 0) {
+					childWidth = 0;
+				}
 				break;
 		}
 
