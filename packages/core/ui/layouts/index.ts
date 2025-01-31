f06894e1d8cf0f1934134cb000dffa67ecd3a82a
Dimitris - Rafail Katsampas <katsampasdr@gmail.com>
2025-02-01 00:13:26 +0200

    fix: Added missing import

--- packages/core/ui/layouts/index.ts
+++ packages/core/ui/layouts/index.ts
@@ -2,7 +2,7 @@ export { AbsoluteLayout } from './absolute-layout';
 export { DockLayout } from './dock-layout';
 export { FlexboxLayout } from './flexbox-layout';
 export { GridLayout, GridUnitType, ItemSpec } from './grid-layout';
-export { RootLayout, getRootLayout } from './root-layout';
+export { RootLayout, getRootLayout, getRootLayoutById } from './root-layout';
 export type { RootLayoutOptions, ShadeCoverOptions } from './root-layout';
 export { StackLayout } from './stack-layout';
 export { WrapLayout } from './wrap-layout';
