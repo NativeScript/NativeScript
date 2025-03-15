f608028f57916c9ed3aabdb57807a6b380c92b73
Arthur <45236453+ArthurMJ96@users.noreply.github.com>
2025-03-15 08:41:58 +0100

    feat(typescript-utils): add EndsWith utility type

--- packages/core/utils/typescript-utils.d.ts
+++ packages/core/utils/typescript-utils.d.ts
@@ -4,3 +4,12 @@
  * // returns: { eventName: string; object?: Observable }
  */
 export type Optional<T, K extends keyof T> = Omit<T, K> & { [P in K]?: T[P] };
+
+/**
+ * Determines if a string type ends with a specified suffix.
+ * @example type IsChangeEvent = EndsWith<"propertyNameChange", "Change", true, false>
+ * // returns: true
+ * @example type IsChangeEvent = EndsWith<"someEvent", "Change", true, false>
+ * // returns: false
+ */
+export type EndsWith<T extends string, S extends string, PassType = true, FailType = false> = T extends `${infer _}${S}` ? PassType : FailType;
