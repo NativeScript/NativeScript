d18fb092ad03e4ff772bd7ffd352fd9aac49922b
Juan de Dios Martínez Vallejo <vallemardeveloper@gmail.com>
2025-03-23 18:07:47 +0100

    feat(rspack): replace css by postcss and bump/remove dependencies (#10724)

--- packages/rspack/src/loaders/css2json-loader/index.ts
+++ packages/rspack/src/loaders/css2json-loader/index.ts
@@ -1,4 +1,4 @@
-import { parse, Import, Stylesheet } from 'css';
+import postcss, { Root, Rule, Declaration, AtRule } from 'postcss';
 import { urlToRequest } from 'loader-utils';
 import { dedent } from 'ts-dedent';
 
@@ -11,21 +11,22 @@ export default function loader(content: string, map: any) {
 	const inline = !!options.useForImports;
 	const requirePrefix = inline ? inlineLoader : '';
 
-	const ast = parse(content);
+	const ast = postcss.parse(content);
 
 	// todo: revise if this is necessary
 	// todo: perhaps use postCSS and just build imports into a single file?
 	let dependencies = [];
+
 	getAndRemoveImportRules(ast)
 		.map(extractUrlFromRule)
 		.map(createRequireUri)
-		.forEach(({ uri, requireURI }) => {
+		.forEach(({ requireURI }) => {
 			dependencies.push(`require("${requirePrefix}${requireURI}")`);
 		});
 
-	const str = JSON.stringify(ast, (k, v) => (k === 'position' ? undefined : v));
+	const cssCompatibleAST = transformPostcssASTtoCSS(ast);
 
-	// map.mappings = map.mappings.replace(/;{2,}/, '')
+	const str = JSON.stringify(cssCompatibleAST);
 
 	const code = dedent`
 	/* CSS2JSON */
@@ -33,41 +34,29 @@ export default function loader(content: string, map: any) {
 	const ___CSS2JSON_LOADER_EXPORT___ = ${str}
 	export default ___CSS2JSON_LOADER_EXPORT___
 	`;
-	this.callback(
-		null,
-		code, //`${dependencies.join('\n')}module.exports = ${str};`,
-		map,
-	);
+
+	this.callback(null, code, map);
 }
 
-function getImportRules(ast: Stylesheet): Import[] {
-	if (!ast || (<any>ast).type !== 'stylesheet' || !ast.stylesheet) {
-		return [];
-	}
-	return <Import[]>(
-		ast.stylesheet.rules.filter(
-			(rule) => rule.type === 'import' && (<any>rule).import,
-		)
-	);
-}
-
-function getAndRemoveImportRules(ast: Stylesheet): Import[] {
-	const imports = getImportRules(ast);
-	ast.stylesheet.rules = ast.stylesheet.rules.filter(
-		(rule) => rule.type !== 'import',
-	);
-
+/**
+ * Extract @import and remove them from the AST
+ */
+function getAndRemoveImportRules(ast: Root): AtRule[] {
+	const imports = ast.nodes.filter(
+		(node) => node.type === 'atrule' && node.name === 'import',
+	) as AtRule[];
+	imports.forEach((rule) => rule.remove());
 	return imports;
 }
 
 /**
  * Extracts the url from import rule (ex. `url("./platform.css")`)
  */
-function extractUrlFromRule(importRule: Import): string {
-	const urlValue = importRule.import;
+function extractUrlFromRule(importRule: AtRule): string {
+	const params = importRule.params;
 
-	const unpackedUrlMatch = urlValue.match(unpackUrlPattern);
-	const unpackedValue = unpackedUrlMatch ? unpackedUrlMatch[1] : urlValue;
+	const unpackedUrlMatch = params.match(unpackUrlPattern);
+	const unpackedValue = unpackedUrlMatch ? unpackedUrlMatch[1] : params;
 
 	const quotesMatch = unpackedValue.match(betweenQuotesPattern);
 	return quotesMatch ? quotesMatch[2] : unpackedValue;
@@ -75,7 +64,40 @@ function extractUrlFromRule(importRule: Import): string {
 
 function createRequireUri(uri): { uri: string; requireURI: string } {
 	return {
-		uri: uri,
+		uri,
 		requireURI: urlToRequest(uri),
+	};
+}
+
+/**
+ * TRANSFORMING THE POSTCSS AST TO THE ORIGINAL CSS AST
+ */
+function transformPostcssASTtoCSS(ast: Root) {
+	return {
+		type: 'stylesheet',
+		stylesheet: {
+			rules: ast.nodes
+				.filter((node) => node.type === 'rule') // Solo reglas CSS, no comentarios ni otros
+				.map(transformRule),
+			parsingErrors: [],
+		},
+	};
+}
+
+function transformRule(rule: Rule) {
+	return {
+		type: 'rule',
+		selectors: rule.selectors,
+		declarations: rule.nodes
+			.filter((node) => node.type === 'decl')
+			.map(transformDeclaration),
+	};
+}
+
+function transformDeclaration(decl: Declaration) {
+	return {
+		type: 'declaration',
+		property: decl.prop,
+		value: decl.value,
 	};
 }
