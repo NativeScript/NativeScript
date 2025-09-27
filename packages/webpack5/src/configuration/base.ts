eff8a5c893c6c7a1cd1e33480d71a431a7bcc3b6
Nathan Walker <walkerrunpdx@gmail.com>
2025-09-27 08:41:33 -0700

    fix: windows source map paths
    Co-authored-by: Jason Cassidy  <47318351+jcassidyav@users.noreply.github.com>

--- packages/webpack5/src/configuration/base.ts
+++ packages/webpack5/src/configuration/base.ts
@@ -181,6 +181,7 @@ export default function (config: Config, env: IWebpackEnv = _env): Config {
 		class FixSourceMapUrlPlugin {
 			apply(compiler) {
 				compiler.hooks.emit.tap('FixSourceMapUrlPlugin', (compilation) => {
+          const leadingCharacter = process.platform === "win32" ? "/":"";
 					Object.keys(compilation.assets).forEach((filename) => {
 						if (filename.endsWith('.mjs') || filename.endsWith('.js')) {
 							const asset = compilation.assets[filename];
@@ -189,7 +190,7 @@ export default function (config: Config, env: IWebpackEnv = _env): Config {
 							// Replace sourceMappingURL to use file:// protocol pointing to actual location
 							source = source.replace(
 								/\/\/# sourceMappingURL=(.+\.map)/g,
-								`//# sourceMappingURL=file://${outputPath}/$1`,
+								`//# sourceMappingURL=file://${leadingCharacter}${outputPath}/$1`,
 							);
 
 							compilation.assets[filename] = {
