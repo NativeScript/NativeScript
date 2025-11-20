f3b33330e8675db66ca13f5c6f5199a20c053b3a
Nathan Walker <walkerrunpdx@gmail.com>
2025-11-20 15:00:53 -0800

    fix(image-source): url loading under different conditions (#10955)

--- packages/core/http/http-shared.ts
+++ packages/core/http/http-shared.ts
@@ -1,21 +1,7 @@
-// Shared types and interfaces for http and image-source modules.
-// Only put platform-agnostic types/interfaces here.
-
 // Example: (add more as needed)
-// TODO: look at removing this after circulars are completely resolved.
+// Note: Circular dep help between http and image-source.
+// Interfaces can be moved around further in future to help avoid.
 export interface ImageSourceLike {
 	toBase64String(format: string, quality?: number): string;
 	// ... add other shared methods/properties as needed
-}
-
-// Circular dependency resolution handling (http <--> image-source)
-let _getImage: (arg: any) => Promise<ImageSourceLike>;
-export function getImageRequest(arg: any): Promise<ImageSourceLike> {
-	if (_getImage) {
-		return _getImage(arg);
-	}
-	return Promise.reject(new Error('No getImage request handler set.'));
-}
-export function setGetImageRequest(fn: (arg: any) => Promise<ImageSourceLike>) {
-	_getImage = fn;
 }
