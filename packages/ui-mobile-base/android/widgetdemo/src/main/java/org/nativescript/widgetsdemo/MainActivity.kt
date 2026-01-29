a267c36dbc59f6de2ca07b729c383517d604134f
Osei Fortune <fortune.osei@yahoo.com>
2026-01-29 18:52:34 -0400

    chore(ui-demo): actionbar

--- packages/ui-mobile-base/android/widgetdemo/src/main/java/org/nativescript/widgetsdemo/MainActivity.kt
+++ packages/ui-mobile-base/android/widgetdemo/src/main/java/org/nativescript/widgetsdemo/MainActivity.kt
@@ -3,16 +3,21 @@ package org.nativescript.widgetsdemo
 import android.app.Dialog
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import android.util.TypedValue
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.graphics.drawable.toDrawable
+import androidx.core.view.postDelayed
 import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.DialogFragment
 import org.nativescript.widgets.CommonLayoutParams
 import org.nativescript.widgets.ContentLayout
 import org.nativescript.widgets.GridLayout
+import org.nativescript.widgets.GridUnitType
 import org.nativescript.widgets.LayoutBase
 import org.nativescript.widgets.StackLayout
 import org.nativescript.widgets.Utils
@@ -35,7 +40,6 @@ class MainActivity : AppCompatActivity() {
 					ViewGroup.LayoutParams.MATCH_PARENT
 				)
 			)
-			page.overflowEdge = LayoutBase.OverflowEdgeBottom
 
 			params.widthPercent = 100f
 			params.heightPercent = 100f
@@ -74,15 +78,57 @@ class MainActivity : AppCompatActivity() {
 
 		Utils.enableEdgeToEdge(this)
 		val frame = ContentLayout(this)
+
+	//	frame.overflowEdge = LayoutBase.OverflowEdgeNone
+
 		val page = GridLayout(this)
-		page.layoutParams = CommonLayoutParams(
+
+
+		val pageParams = CommonLayoutParams(
 			ViewGroup.LayoutParams(
 				ViewGroup.LayoutParams.MATCH_PARENT,
 				ViewGroup.LayoutParams.MATCH_PARENT
 			)
 		)
+		page.layoutParams = pageParams
+
+		page.addRow(
+			1, GridUnitType.auto
+		)
+
+		page.addRow(
+			1, GridUnitType.star
+		)
+
+
+		val height = TypedValue().let { tv ->
+			if (theme.resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
+				TypedValue.complexToDimensionPixelSize(tv.data, resources.displayMetrics)
+			} else {
+				ViewGroup.LayoutParams.WRAP_CONTENT
+			}
+		}
+
+		val ab = Toolbar(this).apply {
+			layoutParams = ViewGroup.LayoutParams(
+				ViewGroup.LayoutParams.MATCH_PARENT,
+				height
+			)
+			setBackgroundColor(Color.BLACK)   // REQUIRED or it's invisible
+			setTitleTextColor(Color.WHITE)    // REQUIRED to see the title
+		}
+
+
+		page.addView(ab)
+
+		setSupportActionBar(ab)
+
+		supportActionBar?.setDisplayShowTitleEnabled(true)
+		supportActionBar?.title = "???"
+
+
 		page.setBackgroundColor(Color.MAGENTA)
-		page.overflowEdge = LayoutBase.OverflowEdgeAllButTop
+		page.overflowEdge = LayoutBase.OverflowEdgeIgnore
 //		page.setInsetListener {
 //			val insets = it.asIntBuffer()
 //			insets.put(0,0)
@@ -94,7 +140,6 @@ class MainActivity : AppCompatActivity() {
 //		}
 
 		frame.setBackgroundColor(Color.BLUE)
-		//frame.overflowEdge = LayoutBase.OverflowEdgeDontApply
 		frame.layoutParams = CommonLayoutParams(
 			ViewGroup.LayoutParams(
 				ViewGroup.LayoutParams.MATCH_PARENT,
@@ -104,12 +149,14 @@ class MainActivity : AppCompatActivity() {
 
 		val svp = StackLayout(this)
 //		svp.setBackgroundColor(Color.YELLOW)
-		svp.layoutParams = CommonLayoutParams(
+		val svParams = CommonLayoutParams(
 			ViewGroup.LayoutParams(
 				ViewGroup.LayoutParams.MATCH_PARENT,
 				ViewGroup.LayoutParams.MATCH_PARENT
 			)
 		)
+		svParams.row = 1
+		svp.layoutParams = svParams
 //		svp.setInsetListener {
 //			val insets = it.asIntBuffer()
 //			insets.put(0,0)
@@ -119,7 +166,6 @@ class MainActivity : AppCompatActivity() {
 //			// insets.put(7, 1)
 //		}
 
-		//	svp.overflowEdge = StackLayout.OverflowEdgeAllButBottom
 		val scrollView = NestedScrollView(this)
 		scrollView.layoutParams = CommonLayoutParams(
 			ViewGroup.LayoutParams(
@@ -131,14 +177,14 @@ class MainActivity : AppCompatActivity() {
 
 		val container = StackLayout(this)
 		container.setBackgroundColor(Color.RED)
-		//container.overflowEdge = StackLayout.OverflowEdgeNone
-
 		val text = TextView(this)
 		text.text = getString(R.string.ipsum)
 		text.textSize = 40f
 		text.setTextColor(Color.WHITE)
 		container.addView(text)
 		scrollView.addView(container)
+
+
 		page.addView(svp)
 		val btn = Button(this)
 		btn.text = "Toggle Overflow"
@@ -148,6 +194,7 @@ class MainActivity : AppCompatActivity() {
 				ViewGroup.LayoutParams.WRAP_CONTENT
 			)
 		)
+		params.row = 1
 		params.width = 300
 		params.height = 300
 		params.width
@@ -196,5 +243,10 @@ class MainActivity : AppCompatActivity() {
 		page.addView(btn)
 		frame.addView(page)
 		setContentView(frame)
+
+		frame.postDelayed(3000) {
+			page.overflowEdge = LayoutBase.OverflowEdgeNone
+		}
+
 	}
 }
