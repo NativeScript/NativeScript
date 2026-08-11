NativeScript/NativeScript

commit 3b6522344f41f007130ce46e2fe117dfb5bd7b41

feat(ios): option to delay the launch event until the app becomes active (#9906)

packages/core/application/application.ios.ts
@@ -9,6 +9,7 @@ import { ios as iosUtils, dataSerialize } from '../utils/native-helper';
 import { ApplicationCommon, initializeSdkVersionClass, SceneEvents } from './application-common';
 import { ApplicationEventData, SceneEventData } from './application-interfaces';
 import { Observable } from '../data/observable';
+import type { iOSApplication as IiOSApplication } from './application';
 import { Trace } from '../trace';
 import {
 	AccessibilityServiceEnabledPropName,
@@ -256,10 +257,11 @@ class SceneDelegate extends UIResponder implements UIWindowSceneDelegate {
 // ensure available globally
 global.SceneDelegate = SceneDelegate;
 
-export class iOSApplication extends ApplicationCommon {
+export class iOSApplication extends ApplicationCommon implements IiOSApplication {
 	private _delegate: UIApplicationDelegate;
 	private _delegateHandlers = new Map<string, Array<Function>>();
 	private _rootView: View;
+	private launchEventCalled = false;
 	private _sceneDelegate: UIWindowSceneDelegate;
 	private _windowSceneMap = new Map<UIScene, UIWindow>();
 	private _primaryScene: UIWindowScene | null = null;
@@ -270,6 +272,8 @@ export class iOSApplication extends ApplicationCommon {
 	displayedOnce = false;
 	displayedLinkTarget: CADisplayLinkTarget;
 	displayedLink: CADisplayLink;
+
+	shouldDelayLaunchEvent = false;
 
 	/**
 	 * @internal - should not be constructed by the user.
@@ -585,6 +589,7 @@ export class iOSApplication extends ApplicationCommon {
 	}
 
 	private notifyAppStarted(notification?: NSNotification) {
+		this.launchEventCalled = true;
 		const root = this.notifyLaunch({
 			ios: notification?.userInfo?.objectForKey('UIApplicationLaunchOptionsLocalNotificationKey') ?? null,
 		});
@@ -693,7 +698,10 @@ export class iOSApplication extends ApplicationCommon {
 				this.window.backgroundColor = SDK_VERSION <= 12 || !UIColor.systemBackgroundColor ? UIColor.whiteColor : UIColor.systemBackgroundColor;
 			}
 
-			this.notifyAppStarted(notification);
+			this.launchEventCalled = false;
+			if (!this.shouldDelayLaunchEvent) {
+				this.notifyAppStarted();
+			}
 		} else {
 			// Scene-based app - window creation will happen in scene delegate
 		}
@@ -701,6 +709,9 @@ export class iOSApplication extends ApplicationCommon {
 
 	@profile
 	private didBecomeActive(notification: NSNotification) {
+		if (!this.launchEventCalled) {
+			this.notifyAppStarted(notification);
+		}
 		const additionalData = {
 			ios: UIApplication.sharedApplication,
 		};

packages/core/application/application.d.ts
@@ -265,6 +265,12 @@ export class iOSApplication extends ApplicationCommon {
 	 * Get the current one or set a custom one.
 	 */
 	sceneDelegate: UIWindowSceneDelegate;
+
+	/**
+	 * Flag to be set when the launch event should be delayed until the application has become active.
+	 * This is useful when you want to process notifications or data in the background without creating the UI.
+	 */
+	shouldDelayLaunchEvent: boolean;
 }
 
 export const VALID_FONT_SCALES: number[];

commit 92b3d03c20c70aad3c8461d22efd51dabf06f76d

fix(ios): ensure notification event is passed to app start when not delaying launch event
regressed in: https://github.com/NativeScript/NativeScript/pull/9906/changes

packages/core/application/application.ios.ts
@@ -702,7 +702,7 @@ export class iOSApplication extends ApplicationCommon implements IiOSApplication
 
 			this.launchEventCalled = false;
 			if (!this.shouldDelayLaunchEvent) {
-				this.notifyAppStarted();
+				this.notifyAppStarted(notification);
 			}
 		} else {
 			// Scene-based app - window creation will happen in scene delegate